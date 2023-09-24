025a2322a99febc47595692eecdac27ebf817643
nmakarov <311735@niuitmo.ru>
2023-09-24 16:37:34 +0300

    deleting entity, some configs, README

--- src/main/java/faang/school/urlshortenerservice/repository/URLRepository.java
+++
@@ -1,7 +0,0 @@
-package faang.school.urlshortenerservice.repository;
-
-import faang.school.urlshortenerservice.model.URLEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface URLRepository extends JpaRepository<URLEntity, Long> {
-}
--- src/main/java/faang/school/urlshortenerservice/service/URLService.java
+++
@@ -1,7 +0,0 @@
-package faang.school.urlshortenerservice.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class URLService {
-}
