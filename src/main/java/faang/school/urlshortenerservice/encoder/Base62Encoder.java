e0718e4a9773d25e414bf57820eb3cb0d60a7c3f
Sergey Gerasimov <czar997@yandex.ru>
2023-09-26 18:40:02 +0300

    добавлен класс HashGenerator

---
+++ src/main/java/faang/school/urlshortenerservice/encoder/Base62Encoder.java
@@ -0,0 +1,14 @@
+package faang.school.urlshortenerservice.encoder;
+
+import faang.school.urlshortenerservice.Hash;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Base62Encoder {
+    public List<Hash> encode(List<Long> numbers) {
+
+        return null;
+    }
+}
---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,18 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.Hash;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class HashRepository{
+
+    public List<Long> getUniqueNumbers(int uniqueNumbers) {
+        return null;
+    }
+
+    public void save(List<Hash> hashes) {
+
+    }
+}
