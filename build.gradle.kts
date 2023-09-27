beff96e797cbf65b0bac3eba96a359b94f66a174
Sergey Gerasimov <czar997@yandex.ru>
2023-09-27 15:11:42 +0300

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     /**
      * Database
      */
-//    implementation("org.liquibase:liquibase-core")
+    implementation("org.liquibase:liquibase-core")
     implementation("redis.clients:jedis:4.3.2")
     runtimeOnly("org.postgresql:postgresql")
 
