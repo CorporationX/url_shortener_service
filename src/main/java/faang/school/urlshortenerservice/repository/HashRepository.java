d6c4ec3f44b4f7b299eaa1a09d6f57d1da9f74ca
Viktor Ivanov <avacode78@gmail.com>
2023-09-28 20:21:41 +0700

    done schema

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,43 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class HashRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<Long> getUniqueNumbers(long uniqueNumbers) {
+        String sql = "select nextval('unique_number_sequence') from unique_number_sequence(1, ?)";
+        return jdbcTemplate.queryForList(sql, Long.class, uniqueNumbers);
+    }
+
+    public void save(List<Hash> hashes) {
+        String sql = "insert into hash(hash) values (?)";
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, hashes.get(i).getHash());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return hashes.size();
+            }
+        });
+    }
+
+    public List<Hash> getHashBatch(long uniqueNumbers) {
+        String sql = "delete from hash where ctid IN (SELECT ctid FROM hash LIMIT ?) RETURNING *";
+        return jdbcTemplate.queryForList(
+                sql, Hash.class, uniqueNumbers);
+    }
+}
