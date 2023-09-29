2bade49e417c51d3333157db951b9f7b3372ea5b
kxnvg <nikita.armeec.88@mail.ru>
2023-09-29 15:45:55 +0300

    Создан HashRepository для работы с таблицами по генерации хэшей для url

---
+++ src/main/java/faang/school/urlshortenerservice/generator/HashGenerator.java
@@ -0,0 +1,10 @@
+package faang.school.urlshortenerservice.generator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HashGenerator {
+
+    public void generateBatch() {
+    }
+}
---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,50 @@
+package faang.school.urlshortenerservice.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class HashRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final String GET_UNIQUE_NUMBERS_SQL = """
+            SELECT nextval('unique_number_sequence') FROM unique_number_sequence(1, ?)
+            """;
+    private final String SAVE_HASHES_SQL = """
+            INSERT INTO hash(hash) VALUES(?)
+            
+            """;
+    private final String GET_HASHES_SQL = """
+            DELETE FROM hash WHERE hash IN (SELECT hash FROM hash LIMIT ?) RETURNING *
+            
+            """;
+
+    public List<Long> getUniqueNumbers(long uniqueNumber) {
+        return jdbcTemplate.queryForList(GET_UNIQUE_NUMBERS_SQL, Long.class, uniqueNumber);
+    }
+
+    public void save(List<String> hashes) {
+        jdbcTemplate.batchUpdate(SAVE_HASHES_SQL, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, hashes.get(i));
+            }
+
+            @Override
+            public int getBatchSize() {
+                return hashes.size();
+            }
+        });
+    }
+
+    public List<String> getHashBatch(long uniqueNumber) {
+        return jdbcTemplate.queryForList(GET_HASHES_SQL, String.class, uniqueNumber);
+    }
+}
