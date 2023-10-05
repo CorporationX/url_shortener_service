91018368d596587c5bc447a8f055642d2825811f
Matvey <matveynikishaev@gmail.com>
2023-10-05 16:56:48 +0300

    Добавил кеширование

---
+++ src/main/java/faang/school/urlshortenerservice/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package faang.school.urlshortenerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+}
--- src/main/java/faang/school/urlshortenerservice/service/UrlService.java
+++ src/main/java/faang/school/urlshortenerservice/service/UrlService.java
@@ -5,6 +5,8 @@ import faang.school.urlshortenerservice.exception.UrlNotFoundException;
 import faang.school.urlshortenerservice.repository.HashRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +16,15 @@ public class UrlService {
     private final HashCache hashCache;
     private final HashRepository hashRepository;
 
+    @CachePut(value = "#url.getLongUrl()") // Не понимаю, что нужно сюда написать(
     public String createShortLink(UrlDto url) {
         var hash = hashCache.getHash();
         return hashRepository.saveUrlAndHash(url.getLongUrl(), hash);
     }
 
+    @Cacheable(value = "originUrl", key = "#url")
     public String getOriginUrl(String url) {
+        log.info("Getting origin URL for: " + url);
         var originUrl = hashRepository.getOriginalUrl(url).orElseThrow(
                 () -> new UrlNotFoundException("404", "Original URL not found for the given short URL")
         );
