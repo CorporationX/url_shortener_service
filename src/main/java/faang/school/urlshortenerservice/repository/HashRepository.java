3f7b5433970abf35168fefe1adc5e78b0904988a
Sergey Gerasimov <czar997@yandex.ru>
2023-10-07 18:01:54 +0300

    Генерация хэша

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -1,4 +1,4 @@
-package faang.school.urlshortenerservice;
+package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.entity.Hash;
 import lombok.RequiredArgsConstructor;
