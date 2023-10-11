6c7a1b61b70b15eb24a3c78869b0820b60aa1999
adil <adil.200191@gmail.com>
2023-10-11 21:14:43 +0600

    fix bugs

---
+++ src/main/java/faang/school/urlshortenerservice/service/HashCache.java
@@ -0,0 +1,11 @@
+package faang.school.urlshortenerservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HashCache {
+
+    public String getHash(String url) {
+        return "";
+    }
+}
