6804196d098a3d2566eab67269ec206aea89e302
Azat <azat.takhautdinov@gmail.com>
2023-10-19 19:33:14 +0500

    Сделал HashRepository и метод для получения уникальных значении, также сделал сущность Hash

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,17 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.model.Hash;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends JpaRepository<Hash, Long> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT NEXTVAL('unique_numbers_seq') FROM GENERATE_SERIES(1, :n)
+            """)
+    List<Long> getUniqueNumbers(long n);
+}
