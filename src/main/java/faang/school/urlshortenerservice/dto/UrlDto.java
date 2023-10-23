79d264a0343dafbe5a17fabe3c250f0feef004a0
Anton <Agressive.cat@yandex.ru>
2023-10-23 10:51:58 +0300

    added unit tests

--- src/main/java/faang/school/urlshortenerservice/dto/UrlDto.java
+++ src/main/java/faang/school/urlshortenerservice/dto/UrlDto.java
@@ -2,7 +2,6 @@ package faang.school.urlshortenerservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -10,7 +9,6 @@ import org.springframework.validation.annotation.Validated;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Validated
 public class UrlDto {
 
