23b79f3581b26cc007bbff4c6fbd7302d69d1357
Azat <azat.takhautdinov@gmail.com>
2023-10-26 19:03:31 +0500

    Кэширование свободных хэшей

---
+++ src/main/java/faang/school/urlshortenerservice/service/HashCache.java
@@ -0,0 +1,54 @@
+package faang.school.urlshortenerservice.service;
+
+import faang.school.urlshortenerservice.model.Hash;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class HashCache {
+    @Value("${queue-size}")
+    private int queueSize = 1;
+    @Value("${percent}")
+    private int fillPercent;
+    private final HashGenerator hashGenerator;
+    private final AtomicBoolean filling = new AtomicBoolean(false);
+    public final Queue<String> hashes = new ArrayBlockingQueue<>(queueSize);
+
+    @PostConstruct
+    public void init() {
+        List<Hash> generatedHashes = hashGenerator.getHashes(queueSize).join();
+        for (Hash hash : generatedHashes) {
+            if (hashes.offer(hash.getHash())) {
+                log.warn("The element added in queue: {}", hash);
+            } else {
+                log.warn("Queue is full");
+            }
+        }
+    }
+
+    public String getHash() {
+        if (hashes.size() / (queueSize / 100.0) < fillPercent) {
+            if (filling.compareAndSet(false, true)) {
+                fillHashes();
+            }
+        }
+        return hashes.poll();
+    }
+
+    private void fillHashes() {
+        hashGenerator.getHashes(queueSize)
+                .thenAccept(c -> hashes.addAll(c))
+                .thenRun(() -> filling.set(false));
+    }
+}
