b8297cddc62b484f21418ae92253ce7201f91e35
Azat <azat.takhautdinov@gmail.com>
2023-10-28 21:40:05 +0500

    Исправил REST ошибку

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -4,7 +4,7 @@ import faang.school.urlshortenerservice.dto.UrlDto;
 import faang.school.urlshortenerservice.service.UrlService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UrlController {
     private final UrlService urlService;
 
-    @PostMapping("/")
+    @GetMapping("/")
     public ResponseEntity<String> getUrl(@RequestBody UrlDto urlDto) {
         return ResponseEntity
                 .ok(urlService.getShortUrl(urlDto));
