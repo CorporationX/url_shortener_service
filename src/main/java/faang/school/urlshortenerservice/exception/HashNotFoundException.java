f5eac60aef36807189b37c7fb792c01794a1ef67
RasulMurtuzaliev <ras8705@mail.ru>
2024-01-23 20:39:50 +0500

    add Exceptions and Dto

---
+++ src/main/java/faang/school/urlshortenerservice/exception/HashNotFoundException.java
@@ -0,0 +1,7 @@
+package faang.school.urlshortenerservice.exception;
+
+public class HashNotFoundException extends BusinessException {
+    public HashNotFoundException(String code, String message) {
+        super(code, message);
+    }
+}
