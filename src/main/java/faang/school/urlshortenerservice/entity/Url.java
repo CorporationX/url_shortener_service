d0e173154171ec2e5acee988e988851014235f4f
InnMish <inn.mishustina@gmail.com>
2024-02-19 17:41:54 +0300

    Revert "Создание короткой и оригинальной ссылки"
    This reverts commit 369033ca478af68b1be272fce75ea4cabfb25451.

--- src/main/java/faang/school/urlshortenerservice/entity/Url.java
+++ src/main/java/faang/school/urlshortenerservice/entity/Url.java
@@ -2,6 +2,8 @@ package faang.school.urlshortenerservice.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -22,6 +24,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Url {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String hash;
 
     @Column(name = "url", nullable = false)
--- src/main/java/faang/school/urlshortenerservice/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package faang.school.urlshortenerservice.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
