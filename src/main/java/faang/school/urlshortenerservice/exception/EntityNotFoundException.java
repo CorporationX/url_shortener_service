49347d25b3234cc330a6f78903fab3a4bea011cb
Kamil Gasanov <gasanovkamil516@gmail.com>
2024-02-20 12:11:45 +0300

    added exception handler

--- src/main/java/faang/school/urlshortenerservice/exception/EntityNotFoundException.java
+++ src/main/java/faang/school/urlshortenerservice/exception/EntityNotFoundException.java
@@ -3,7 +3,6 @@ package faang.school.urlshortenerservice.exception;
 import org.webjars.NotFoundException;
 
 public class EntityNotFoundException extends NotFoundException {
-
     public EntityNotFoundException(String message) {
         super(message);
     }
