077004b8d5f462f6dc5a38eb190120526a2f623b
Kamil Gasanov <gasanovkamil516@gmail.com>
2024-02-20 08:49:56 +0300

    added HashCache

---
+++ src/main/java/faang/school/urlshortenerservice/service/HashCache.java
@@ -0,0 +1,42 @@
+package faang.school.urlshortenerservice.service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Component
+@RequiredArgsConstructor
+public class HashCache {
+    private final HashGenerator hashGenerator;
+
+    @Value("${hash.capacity}")
+    private int capacity = 10001;
+
+    @Value("${hash.fill_percent}")
+    private int fillPercent;
+
+    private final AtomicBoolean filling = new AtomicBoolean(false);
+
+    private final Queue<String> hashes = new ArrayBlockingQueue<>(capacity);
+
+    @PostConstruct
+    public void init() {
+       hashes.addAll(hashGenerator.getHashes(capacity));
+    }
+
+    public String getHash() {
+        if (hashes.size() * 100.0 / capacity  < fillPercent) {
+            if (filling.compareAndSet(false, true)) {
+                hashGenerator.getHashAsync(capacity)
+                        .thenAccept(hashes::addAll)
+                        .thenRun(() -> filling.set(false));
+            }
+        }
+        return hashes.poll();
+    }
+}
--- src/main/java/faang/school/urlshortenerservice/service/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/service/HashGenerator.java
@@ -5,10 +5,12 @@ import faang.school.urlshortenerservice.repository.HashRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
@@ -16,14 +18,29 @@ public class HashGenerator {
     private final HashRepository hashRepository;
     private final Base62Encoder base62Encoder;
 
-    @Value("${hash_generator.max_range}")
+    @Value("${hash.generator.max_range}")
     private int maxRange;
 
-    @Async("taskExecutor")
+    @Scheduled(cron = "${hash.generator.cron}")
     @Transactional
     public void generateBatch() {
         List<Long> numbers = hashRepository.getUniqueNumbers(maxRange);
         List<Hash> hashBatch = base62Encoder.encode(numbers).stream().map(Hash::new).toList();
         hashRepository.saveAll(hashBatch);
+    }
+
+    @Transactional
+    public List<String> getHashes(int amount) {
+        List<String> hashes = hashRepository.getHashBatch(amount);
+        if (hashes.size() < amount) {
+            generateBatch();
+            hashes.addAll(hashRepository.getHashBatch(amount - hashes.size()));
+        }
+        return hashes;
+    }
+
+    @Async("taskExecutor")
+    public CompletableFuture<List<String>> getHashAsync(int amount) {
+        return CompletableFuture.completedFuture(getHashes(amount));
     }
 }
