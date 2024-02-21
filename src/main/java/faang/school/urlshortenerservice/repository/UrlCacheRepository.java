40e462318e77487c9f0a77c9f5e73e36a3db2278
Kamil Gasanov <gasanovkamil516@gmail.com>
2024-02-21 19:02:17 +0300

    fix

--- src/main/java/faang/school/urlshortenerservice/repository/UrlCacheRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlCacheRepository.java
@@ -4,13 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.concurrent.TimeUnit;
+
 @Repository
 @RequiredArgsConstructor
 public class UrlCacheRepository {
     private final StringRedisTemplate redisTemplate;
 
     public void save(String hash, String longUrl) {
-        redisTemplate.opsForValue().set(hash, longUrl);
+        redisTemplate.opsForValue().set(hash, longUrl, 1, TimeUnit.DAYS);
     }
 
     public String get(String hash) {
