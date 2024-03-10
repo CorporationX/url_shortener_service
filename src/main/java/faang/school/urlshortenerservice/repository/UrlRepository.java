270e926e1164cb9f2f0b1df13f66041d246ba54c
Ackerman995 <rauanture@gmail.com>
2024-03-10 15:24:34 +0500

    add CleanerScheduler

--- src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
@@ -1,10 +1,18 @@
 package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.entity.Url;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UrlRepository extends CrudRepository<Url, Long> {
     Url findByHash(String hash);
+
+    @Query(nativeQuery = true, value = """
+            DELETE FROM url WHERE createdAt < NOW() - INTERVAL '1 year' RETURNING hash
+            """)
+    List<String> deleteOlderThanYear();
 }
