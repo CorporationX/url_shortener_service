2c94fb24800cf59e21023f7fd0193a5a60e83fc8
phoen1x <106927036+vladmeniailov@users.noreply.github.com>
2024-03-18 22:41:50 +0300

    Add HashRepository with methods

--- src/main/java/faang/school/urlshortenerservice/entity/Url.java
+++ src/main/java/faang/school/urlshortenerservice/entity/Url.java
@@ -2,10 +2,7 @@ package faang.school.urlshortenerservice.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
