9f017f244c92a498512931632b1b5f5ed3d9d40d
phoen1x <106927036+vladmeniailov@users.noreply.github.com>
2024-03-18 20:49:55 +0300

    Add DB structure

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Url.java
@@ -0,0 +1,28 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "url")
+public class Url {
+    @Id
+    @Column(length = 6, nullable = false)
+    private String hash;
+    @Column(nullable = false)
+    private String url;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
