27eb53769576f2e713aac0135d8b7c5748d0c469
fd_shell <dariuskpp@gmail.com>
2024-03-19 21:49:21 +0300

    Create hashRepository

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,12 @@
+package faang.school.urlshortenerservice.repository;
+
+import java.util.List;
+
+public interface HashRepository {
+    List<Long> getUniqueNumbers(int count);
+
+    void save(List<String> hashes);
+
+    List<String> getHashBatch();
+
+}
