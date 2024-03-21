d351338fb87b076729194580e46b0d2638a3f785
fd_shell <dariuskpp@gmail.com>
2024-03-21 15:37:13 +0300

    Create hashGenerator

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -3,6 +3,8 @@ package faang.school.urlshortenerservice.repository;
 import java.util.List;
 
 public interface HashRepository {
+
+
     List<Long> getUniqueNumbers(int count);
 
     void save(List<String> hashes);
