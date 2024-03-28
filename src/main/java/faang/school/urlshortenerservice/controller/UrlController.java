e5499662d0df2fe6c7fd1fa673d9293d5b52cc7f
Edisolt Khatsiev <Edisolt Khatsiev>
2024-03-28 18:57:38 +0300

    done

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -1,21 +1,28 @@
 package faang.school.urlshortenerservice.controller;
 
+import faang.school.urlshortenerservice.entity.Url;
 import faang.school.urlshortenerservice.service.UrlService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/hash")
+@RequestMapping
 public class UrlController {
     private final UrlService urlService;
 
+    @GetMapping("/{hash}")
     @ResponseStatus(HttpStatus.FOUND)
-    @GetMapping
-    public void redirect() {
+    public RedirectView redirectToOriginalUrl(@PathVariable String hash) {
+        String url = urlService.getUrl(hash);
+        return new RedirectView(url);
     }
 }
--- src/main/java/faang/school/urlshortenerservice/service/UrlService.java
+++ src/main/java/faang/school/urlshortenerservice/service/UrlService.java
@@ -1,9 +1,23 @@
 package faang.school.urlshortenerservice.service;
 
+import faang.school.urlshortenerservice.repository.UrlCacheRepository;
+import faang.school.urlshortenerservice.repository.UrlRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UrlService {
+    private final UrlRepository urlRepository;
+    private final UrlCacheRepository urlCacheRepository;
+
+    public String getUrl(String hash) {
+        return urlCacheRepository
+                .get(hash)
+                .orElseGet(() -> urlRepository.findById(hash)
+                        .orElseThrow(() -> new EntityNotFoundException(String.format("Url %s not found for hash", hash)))
+                )
+                .toString();
+    }
 }
