07c237d20be5c92a2e12f3c047d9f94412add5a8
Emperor_Samedi <mexanik13970@gmail.com>
2024-03-28 23:59:36 +0300

    add Url add UrlRepository

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Url.java
@@ -0,0 +1,29 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "url")
+public class Url {
+    @Id
+    @Column(name = "hash", length = 6)
+    private String hash;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
