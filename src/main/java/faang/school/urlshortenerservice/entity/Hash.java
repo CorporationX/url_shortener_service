fb7dba14357544a63bd39aa7ce5ba12d4aeb73ed
Edisolt Khatsiev <Edisolt Khatsiev>
2024-03-28 07:03:40 +0300

    done

--- src/main/java/faang/school/urlshortenerservice/entity/Hash.java
+++ src/main/java/faang/school/urlshortenerservice/entity/Hash.java
@@ -6,11 +6,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "hash")
 public class Hash {
