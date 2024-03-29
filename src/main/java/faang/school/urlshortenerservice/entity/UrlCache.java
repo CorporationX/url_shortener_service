a7085ae86a4c63253ad2860773c84ce4553cc7b9
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-03-29 11:37:51 +0300

    fix

--- src/main/java/faang/school/urlshortenerservice/entity/UrlCache.java
+++ src/main/java/faang/school/urlshortenerservice/entity/UrlCache.java
@@ -4,15 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
-import java.util.concurrent.TimeUnit;
+import java.io.Serializable;
 
 @AllArgsConstructor
 @Getter
-@RedisHash(value = "UrlCache",timeToLive = 86400)
-public class UrlCache {
+@RedisHash(value = "UrlCache", timeToLive = 86400)
+public class UrlCache implements Serializable {
     @Id
     private String hash;
     @Indexed
