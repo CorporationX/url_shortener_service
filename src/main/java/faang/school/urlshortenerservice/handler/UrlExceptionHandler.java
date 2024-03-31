5ce68988df5de3aeaa0f3fa8fa89991eb96f2885
Alexander Bulgakov <asbulgakov90@gmail.com>
2024-03-31 18:26:39 +0300

    Complete without tests feature/BJS2-3566

---
+++ src/main/java/faang/school/urlshortenerservice/handler/UrlExceptionHandler.java
@@ -0,0 +1,39 @@
+package faang.school.urlshortenerservice.handler;
+
+import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Component
+@RestControllerAdvice
+@Slf4j
+public class UrlExceptionHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(ValidationException ex) {
+        String message = ex.getMessage();
+        log.error("EntityNotFoundException, {}", message, ex);
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleRuntimeException(RuntimeException ex) {
+        String message = ex.getMessage();
+        log.error("RuntimeException, {}", message, ex);
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, message);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleGlobalException(Exception ex) {
+        String message = ex.getMessage();
+        log.error("GlobalException, {}", message, ex);
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, message);
+    }
+}
