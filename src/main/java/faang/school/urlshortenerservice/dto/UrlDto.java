d1a4a9ad8efc81ba559c8b317eba0888a27d311a
Ruslan Latyshev <latyshevruslan@ya.ru>
2024-03-31 18:48:36 +0300

    fix after review

--- src/main/java/faang/school/urlshortenerservice/dto/UrlDto.java
+++ src/main/java/faang/school/urlshortenerservice/dto/UrlDto.java
@@ -1,6 +1,5 @@
 package faang.school.urlshortenerservice.dto;
 
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +11,6 @@ import org.hibernate.validator.constraints.URL;
 @NoArgsConstructor
 @Builder
 public class UrlDto {
-    //@Pattern(regexp = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()!@:%_\\+.~#?&\\/\\/=]*)")
     @URL
     private String url;
 }
