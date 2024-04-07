d6fab536a644fe75521e982619fd359302f0d2de
Edisolt Khatsiev <Edisolt Khatsiev>
2024-04-07 12:59:28 +0300

    deleted unnecessary @Url custom annotation. added @URL from org.hibernate

--- src/main/java/faang/school/urlshortenerservice/dto/UrlDto.java
+++ src/main/java/faang/school/urlshortenerservice/dto/UrlDto.java
@@ -1,13 +1,11 @@
 package faang.school.urlshortenerservice.dto;
 
-import faang.school.urlshortenerservice.validator.url.UrlConstraint;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 import java.time.LocalDateTime;
 
@@ -18,7 +16,7 @@ import java.time.LocalDateTime;
 public class UrlDto {
     @Size(max = 6)
     private String hash;
-    @UrlConstraint
+    @URL
     private String url;
     private LocalDateTime createdAt;
 }
