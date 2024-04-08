01ba459ad520af4290e2cb07d3405a04bc2c87f4
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-04-08 12:45:00 +0300

    fix2

--- src/main/java/faang/school/urlshortenerservice/dto/RequestUrl.java
+++ src/main/java/faang/school/urlshortenerservice/dto/RequestUrl.java
@@ -1,10 +1,12 @@
 package faang.school.urlshortenerservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
 
 @Data
 public class RequestUrl {
-    @URL
+    @NotBlank(message = "Url cannot be blank")
+    @URL(message = "Url mast be valid")
     private String url;
 }
