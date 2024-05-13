80accaf932fa91fa4619b940e0fdba80181cb68d
Bukevsky <bykaplay@gmail.com>
2024-05-13 09:56:47 +0300

    created migration

--- src/main/resources/db/changelog/changeset/V001__url_shortener_service_create_structure.sql
+++ src/main/resources/db/changelog/changeset/V001__url_shortener_service_create_structure.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS url (
     hash varchar(6) PRIMARY KEY NOT NULL,
     url varchar NOT NULL,
-    created_at timestamptz DEFAULT current_timestamp
+    created_at TIMESTAMPTZ DEFAULT current_timestamp
 );
 
 CREATE TABLE IF NOT EXISTS hash (
