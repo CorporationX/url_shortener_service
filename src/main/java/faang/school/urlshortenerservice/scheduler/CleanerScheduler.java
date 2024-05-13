735acff762dd3f3635e87630b54ddbd80812cffa
kirillgutorov <gutorov.k@mail.ru>
2024-05-13 21:00:09 +0700

    Scheduler

--- src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
+++ src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
@@ -1,17 +1,11 @@
 package faang.school.urlshortenerservice.scheduler;
 
-import faang.school.urlshortenerservice.model.Hash;
-import faang.school.urlshortenerservice.model.Url;
-import faang.school.urlshortenerservice.repository.HashRepository;
-import faang.school.urlshortenerservice.repository.UrlRepository;
 import faang.school.urlshortenerservice.service.UrlShortenerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -21,10 +15,10 @@ public class CleanerScheduler {
 
     private final UrlShortenerService urlShortenerService;
 
-//    @Scheduled(cron = "${scheduler.cleaner}")
+    //    @Scheduled(cron = "${scheduler.cleaner}")
     @Scheduled(fixedDelay = 10000)
     @Async("executor")
-    public void scheduledClean(){
+    public void scheduledClean() {
         log.info("Scheduled url cleaner started");
         urlShortenerService.cleanAsync();
         log.info("Scheduled url cleaner finished");
