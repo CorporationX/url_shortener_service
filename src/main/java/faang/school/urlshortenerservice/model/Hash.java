b50b80f2b2955a62cee7602e478dae5f026490a6
Viktor Dyukov <snailrake@mail.ru>
2024-05-16 13:27:14 +0400

    feat: add hash repository

---
+++ src/main/java/faang/school/urlshortenerservice/model/Hash.java
@@ -0,0 +1,20 @@
+package faang.school.urlshortenerservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "hash")
+public class Hash {
+
+    @Id
+    @Column(name = "hash")
+    private String hash;
+}
