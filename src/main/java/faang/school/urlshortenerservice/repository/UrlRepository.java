4b719cf14dcd1da19b866e574f2d0e94f882f6b3
DadyaVolodya <pl4stmass@yandex.ru>
2024-05-18 14:46:27 +0300

    developing scheduler

--- src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
@@ -1,4 +1,13 @@
 package faang.school.urlshortenerservice.repository;
 
-public interface UrlRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url,Long> {
+    Set<String> findOldUrls();
+
+    void deleteOldUrls(Set<String> hashes);
 }
--- src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
+++ src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
@@ -1,4 +1,28 @@
 package faang.school.urlshortenerservice.scheduler;
 
+import faang.school.urlshortenerservice.repository.HashRepository;
+import faang.school.urlshortenerservice.repository.UrlRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
 public class CleanerScheduler {
+
+    private final UrlRepository urlRepository;
+    private final HashRepository hashRepository;
+
+    @Transactional
+    @Scheduled(cron = "${expression.cron:0 0 0 * * *}")
+    public void cleanOldUrls(){
+        Set<String> hashes = urlRepository.findOldUrls();
+        if(!hashes.isEmpty()){
+            urlRepository.deleteOldUrls(hashes);
+            hashRepository.saveHashes(hashes);
+        }
+    }
 }
