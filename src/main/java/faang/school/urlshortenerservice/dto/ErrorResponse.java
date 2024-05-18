19f0a36ec02736ea787da6c1b5d75baf6aaa34a7
Ikhsanov Nail <ikhsanov.nail@outlook.com>
2024-05-18 18:49:14 +0500

    BJS2-6691 Exception Handler

---
+++ src/main/java/faang/school/urlshortenerservice/dto/ErrorResponse.java
@@ -0,0 +1,16 @@
+package faang.school.urlshortenerservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private HttpStatus status;
+    private String message;
+
+}
---
+++ src/main/java/faang/school/urlshortenerservice/handler/UrlExceptionHandler.java
@@ -0,0 +1,57 @@
+package faang.school.urlshortenerservice.handler;
+
+import faang.school.urlshortenerservice.dto.ErrorResponse;
+import faang.school.urlshortenerservice.exception.DataValidationException;
+import faang.school.urlshortenerservice.exception.SystemException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class UrlExceptionHandler {
+
+    @ExceptionHandler(SystemException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleInternalServerError(Exception exception) {
+        log.error("Internal server error: {}", exception.getMessage());
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+    }
+
+    @ExceptionHandler(DataValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDataValidationException(DataValidationException exception) {
+        log.error("Data validation error: {}", exception.getMessage());
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.error("MethodArgumentNotValidException: {}", exception.getMessage());
+        return exception.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(
+                        error -> ((FieldError) error).getField(),
+                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), "")
+                ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> handleAllExceptions(Exception exception) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        body.put("message", exception.getMessage() != null ? exception.getMessage() : "Unknown error");
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
