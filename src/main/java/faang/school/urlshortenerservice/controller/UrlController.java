5608fe197356c490ea9b90dd37fd4a3860918e05
DadyaVolodya <pl4stmass@yandex.ru>
2024-05-21 15:24:37 +0300

    add get logic

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -4,6 +4,9 @@ import faang.school.urlshortenerservice.dto.UrlDto;
 import faang.school.urlshortenerservice.service.UrlService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +21,16 @@ public class UrlController {
     @PostMapping("/url")
     public RedirectView shortenUrl(@Valid @RequestBody UrlDto urlDto) {
         String shortUrl = urlService.createShortenUrl(urlDto.getLongUrl());
-        return new RedirectView(shortUrl);
+        RedirectView redirectView = new RedirectView(shortUrl);
+        redirectView.setStatusCode(HttpStatus.FOUND);
+        return redirectView;
+    }
+
+    @GetMapping("{hash}")
+    public RedirectView createLongUrl(@PathVariable String hash){
+        String longUrl = urlService.getLongUrl(hash);
+        RedirectView redirectView = new RedirectView(longUrl);
+        redirectView.setStatusCode(HttpStatus.FOUND);
+        return redirectView;
     }
 }
