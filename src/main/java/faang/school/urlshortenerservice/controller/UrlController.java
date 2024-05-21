02c7c5853bc9994120d0304c228e4a7aad91e74b
three-moon <avfedorenko3@yandex.com>
2024-05-22 01:18:40 +0300

    Creating an original link

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -4,10 +4,14 @@ import faang.school.urlshortenerservice.dto.UrlDto;
 import faang.school.urlshortenerservice.service.UrlService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping
@@ -18,5 +22,12 @@ public class UrlController {
     @PostMapping("url")
     public UrlDto create(@RequestBody @Valid UrlDto urlDto) {
         return urlService.create(urlDto);
+    }
+
+    @GetMapping("{hash}")
+    public RedirectView get(@PathVariable String hash, RedirectAttributes attributes) {
+        attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
+        attributes.addAttribute("attribute", "redirectWithRedirectView");
+        return new RedirectView(urlService.get(hash).getUrl());
     }
 }
