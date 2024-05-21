34b8a5aa39ae10205a9e4a6c64acaece2dd9ac3f
Sergey Ryabov <mre@mail.ru>
2024-05-21 21:27:22 +0800

    HashRepository

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,21 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.model.Hash;
+import feign.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends JpaRepository<Hash, Long> {
+    @Query(nativeQuery = true, value = "DELETE FROM hash WHERE hash IN (SELECT hash FROM hash LIMIT :batch) RETURNING *")
+    List<Hash> getHashBatch(@Param("batch") int batch);
+
+    @Modifying
+    @Query(nativeQuery = true, value = "SELECT nextval('unique_number_seq') FROM generate_series(1, :batchsize)")
+    List<Long> getUniqueNumbers(@Param("batchSize") int batchSize);
+
+}
