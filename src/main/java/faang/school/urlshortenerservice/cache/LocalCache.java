99f218a317f12a8de57025f0e2180547c48bc213
DadyaVolodya <pl4stmass@yandex.ru>
2024-05-24 23:44:14 +0300

    local

--- src/main/java/faang/school/urlshortenerservice/cache/LocalCache.java
+++ src/main/java/faang/school/urlshortenerservice/cache/LocalCache.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -25,19 +23,20 @@ public class LocalCache {
 
     private final AtomicBoolean filling = new AtomicBoolean(false);
 
-    private final Queue<String> hashes = new ArrayBlockingQueue<>(capacity);
+    private Queue<String> hashes;
 
     @PostConstruct
     public void init() {
+        hashes = new ArrayBlockingQueue<>(capacity);
         hashes.addAll(hashGenerator.getHashes(capacity));
     }
 
     public String getHash() {
-        if(hashes.size() / (capacity / 100.0) < percent){
-            if (filling.compareAndSet(false,true)){
+        if (hashes.size() / (capacity / 100.0) < percent) {
+            if (filling.compareAndSet(false, true)) {
                 hashGenerator.getHashAsync(capacity)
                         .thenAccept(hashes::addAll)
-                        .thenRun(()->filling.set(false));
+                        .thenRun(() -> filling.set(false));
             }
         }
         return hashes.poll();
