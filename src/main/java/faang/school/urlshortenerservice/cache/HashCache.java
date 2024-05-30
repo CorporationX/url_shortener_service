2d91d0335ca84b71914335354e3ea5445f7298cd
Sam Migachev <sammigachev@gmail.com>
2024-05-30 18:57:49 +0300

    рефакторинг

--- src/main/java/faang/school/urlshortenerservice/cache/HashCache.java
+++ src/main/java/faang/school/urlshortenerservice/cache/HashCache.java
@@ -3,6 +3,7 @@ package faang.school.urlshortenerservice.cache;
 import faang.school.urlshortenerservice.hash.HashGenerator;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class HashCache {
     @Value("${cache.capacity}")
     private int sizeQueue;
@@ -25,23 +27,28 @@ public class HashCache {
 
     @PostConstruct
     public void init() {
+        log.info("Начинается заполнение кэша");
         cache = new ArrayBlockingQueue<>(sizeQueue);
         cache.addAll(hashGenerator.getHashes(sizeQueue));
+        log.info("Кэш заполнен хэшами");
     }
 
     public String getHash() {
+        log.info("Запрос на получение хэша из кэша");
         updateCache();
-        return cache.poll();
+        String hash = cache.poll();
+        log.info("Хэш получен {}", hash);
+        return hash;
     }
     @Async("executorService")
     public void updateCache() {
         double percent = (double) cache.size() / 100;
-        if (percent < fullness) {
-            if (generateCacheHashes.compareAndSet(false, true)) {
-                hashGenerator.generatedBatchAsync(sizeQueue)
-                        .thenAccept(hashes -> cache.addAll(hashes))
-                        .thenRun(() -> generateCacheHashes.set(false));
-            }
+        if (percent < fullness && generateCacheHashes.compareAndSet(false, true)) {
+            log.info("Запуск наполнения кэша данными");
+            hashGenerator.generatedBatchAsync(sizeQueue)
+                    .thenAccept(hashes -> cache.addAll(hashes))
+                    .thenRun(() -> generateCacheHashes.set(false));
+            log.info("Наполнение кэша прошло успешно");
         }
     }
 }
--- src/main/java/faang/school/urlshortenerservice/service/UrlService.java
+++ src/main/java/faang/school/urlshortenerservice/service/UrlService.java
@@ -9,12 +9,14 @@ import faang.school.urlshortenerservice.repository.UrlCacheRepository;
 import faang.school.urlshortenerservice.repository.UrlRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UrlService {
     private final HashCache hashCache;
     private final UrlRepository urlRepository;
@@ -23,20 +25,27 @@ public class UrlService {
 
     @Transactional
     public UrlDto createShortUrl(UrlDto urlDto) {
+        log.info("Создания short url для {}", urlDto.getUrl());
         String  hash = hashCache.getHash();
+        log.info("Получили хэш из кэша {}", hash);
         urlCacheRepository.putUrl(hash, urlDto.getUrl());
+        log.info("Сохранили url и хэш в urlCacheRepository");
+        log.info("Начинаем формировать сущность Url");
         Url url = new Url();
         url.setHash(hash);
         url.setUrl(urlDto.getUrl());
         Url saveUrl = urlRepository.save(url);
+        log.info("Сущность {} успешно сохранена в БД", saveUrl);
         return urlMapper.toDto(saveUrl);
     }
 
     public RedirectView getRedirectView(String hash) {
+        log.info("Старт поиска Url адреса на хэшу");
         String url = urlCacheRepository.getUrl(hash)
                 .orElseGet(() -> urlRepository.findById(hash)
                         .orElseThrow(() -> new EntityNotFoundException(String.format("Post with id %s not found", hash)))
                         .getUrl());
+        log.info("Url {} получен и переноправил юзера" ,url);
         return new RedirectView(url);
     }
 
