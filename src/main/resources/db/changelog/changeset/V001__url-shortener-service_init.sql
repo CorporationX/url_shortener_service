4ba6e6a1dd91c1bd60db4e9d729f5887aed2d099
nadir <den4ik.strelok@gmail.com>
2024-07-07 22:27:40 +0300

    Настроил инициализацию БД

---
+++ src/main/resources/db/changelog/changeset/V001__url-shortener-service_init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS url
+(
+    hash             VARCHAR(6) PRIMARY KEY,
+    url              VARCHAR(512) NOT NULL,
+    created_at       TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_received_at TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS hash
+(
+    hash varchar(6) PRIMARY KEY
+);
+
+CREATE SEQUENCE unique_number_seq start 1 increment 1;
