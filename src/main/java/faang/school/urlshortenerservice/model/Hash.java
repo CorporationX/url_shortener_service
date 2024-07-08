e2b95e69cf7c6685c7239c6deef06ac911cdfba7
nadir <den4ik.strelok@gmail.com>
2024-07-08 15:09:07 +0300

    Реализовал енкодер

--- src/main/java/faang/school/urlshortenerservice/model/Hash.java
+++ src/main/java/faang/school/urlshortenerservice/model/Hash.java
@@ -2,10 +2,14 @@ package faang.school.urlshortenerservice.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity(name = "hash")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Hash {
     @Id
     private String hash;
