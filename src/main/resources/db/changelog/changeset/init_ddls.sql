b103cf26de36949fa81f57384fc8bb6264fe108e
mihanchik94 <mihanchik94@gmail.com>
2024-07-09 22:50:01 +0300

    Создал HashRepository для работы с базой

--- src/main/resources/db/changelog/changeset/init_ddls.sql
+++ src/main/resources/db/changelog/changeset/init_ddls.sql
@@ -3,3 +3,12 @@ create table url (
     varchar(2048) url not null, unique,
     created_at timestamp default current_timestamp not null
 )
+
+create sequence unique_number_seq (
+    start 1
+    increment 1
+)
+
+create table hashes (
+    varchar(6) hash primary key,
+)
