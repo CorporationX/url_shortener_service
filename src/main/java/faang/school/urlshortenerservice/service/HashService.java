305d12db42b04210740b33d2d75deaca8afdf07e
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-07-09 10:39:47 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-17301

--- src/main/java/faang/school/urlshortenerservice/service/HashService.java
+++ src/main/java/faang/school/urlshortenerservice/service/HashService.java
@@ -1,6 +1,13 @@
 package faang.school.urlshortenerservice.service;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public interface HashService {
 
     void generateBatch();
+
+    List<String> getHashes(Long amount);
+
+    CompletableFuture<List<String>> getHashesAsync(Long amount);
 }
