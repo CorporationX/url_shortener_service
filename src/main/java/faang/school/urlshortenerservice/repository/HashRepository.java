14b6674de38ad129678e8e766e3222b029ae53b7
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-07-10 23:04:32 +0400

    done

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,27 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends JpaRepository<Hash, Long> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT nextval('unique_number_seq') AS generated_value
+            FROM generate_series(1, :range);
+            """)
+    List<Long> getUniqueNumbers(int range);
+
+    @Modifying
+    @Query(nativeQuery = true, value = """
+            DELETE FROM hash h WHERE hash IN (
+            SELECT hash FROM h LIMIT :batchSize
+            ) RETURNING *
+            """)
+    List<Hash> getHashBatch(int batchSize);
+}
