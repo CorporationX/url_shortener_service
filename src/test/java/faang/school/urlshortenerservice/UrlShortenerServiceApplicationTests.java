371b51033969dc9146b66c0238aa5c8b4275be08
nadir <den4ik.strelok@gmail.com>
2024-07-10 23:29:25 +0300

    Добавил тудушку написать тесты после ревью

--- src/test/java/faang/school/urlshortenerservice/UrlShortenerServiceApplicationTests.java
+++ src/test/java/faang/school/urlshortenerservice/UrlShortenerServiceApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UrlShortenerServiceApplicationTests {
+    //TODO: написать тесты
     @Test
     void contextLoads() {
         Assertions.assertThat(40 + 2).isEqualTo(42);
