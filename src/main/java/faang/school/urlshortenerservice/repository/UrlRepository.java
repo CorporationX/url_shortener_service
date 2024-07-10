ffdec90867b7991fb3795d78b562dbdc4b443263
mihanchik94 <mihanchik94@gmail.com>
2024-07-10 23:08:59 +0300

    Добавил джобу для освобождения неиспользуемых хэшей

---
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
@@ -0,0 +1,13 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface UrlRepository extends JpaRepository<Url, String> {
+    @Query(value = "delete from url u where u.created_at < :period returning *", nativeQuery = true)
+    List<Url> deleteOldUrl(LocalDateTime period);
+}
