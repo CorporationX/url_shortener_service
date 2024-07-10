c0b610f0160c186e374d6ffefcb7269e60a33ed7
Tem <tipokruto014@gmail.com>
2024-07-10 18:03:15 +0300

    initial

---
+++ src/main/java/faang/school/urlshortenerservice/handler/UrlExceptionHandler.java
@@ -0,0 +1,62 @@
+package faang.school.urlshortenerservice.handler;
+
+import faang.school.urlshortenerservice.exception.NotFoundException;
+import faang.school.urlshortenerservice.exception.ValidationException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+public class UrlExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(
+                        error -> ((FieldError) error).getField(),
+                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), "")
+                ));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(NotFoundException e, HttpServletRequest request) {
+        log.error("Not found: {}", e.getMessage());
+        return buildErrorResponse(e, request, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleDataValidationException(ValidationException e, HttpServletRequest request) {
+        log.error("Data validation error: {}", e.getMessage());
+        return buildErrorResponse(e, request, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleRuntimeException(Exception e, HttpServletRequest request) {
+        log.error("Server error: {}", e.getMessage(), e);
+        return buildErrorResponse(e, request, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ErrorResponse buildErrorResponse(Exception e, HttpServletRequest request, HttpStatus status) {
+        return ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .url(request.getRequestURI())
+                .status(status)
+                .message(e.getMessage())
+                .build();
+    }
+}
