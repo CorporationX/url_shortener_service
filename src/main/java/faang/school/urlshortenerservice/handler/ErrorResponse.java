ad81ca28aaf2ae0ae8fbc6b6a3520171330b60c8
ekoregin <evgkoregin@gmail.com>
2024-07-12 15:45:53 +0300

    BJS2-17161: Exception handler

---
+++ src/main/java/faang/school/urlshortenerservice/handler/ErrorResponse.java
@@ -0,0 +1,11 @@
+package faang.school.urlshortenerservice.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ErrorResponse {
+
+    private String message;
+}
---
+++ src/main/java/faang/school/urlshortenerservice/handler/UrlExceptionHandler.java
@@ -0,0 +1,42 @@
+package faang.school.urlshortenerservice.handler;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class UrlExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse defaultExceptionHandler(RuntimeException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundEntityException(EntityNotFoundException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+}
