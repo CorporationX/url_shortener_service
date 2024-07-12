4bd8a76bff82c251235cfb4f9a04b3e488fac5d1
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-07-12 23:34:25 +0400

    commit

---
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlCacheRepository.java
@@ -0,0 +1,22 @@
+package faang.school.urlshortenerservice.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UrlCacheRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void save(String hash, String url ) {
+        redisTemplate.opsForValue().set(hash, url);
+    }
+
+    public Optional<String> findById(String hash) {
+        return Optional.ofNullable(redisTemplate.opsForValue().get(hash));
+    }
+}
--- src/main/java/faang/school/urlshortenerservice/service/UrlService.java
+++ src/main/java/faang/school/urlshortenerservice/service/UrlService.java
@@ -1,12 +1,32 @@
 package faang.school.urlshortenerservice.service;
 
 import faang.school.urlshortenerservice.cache.HashCache;
+import faang.school.urlshortenerservice.dto.UrlDto;
+import faang.school.urlshortenerservice.entity.Url;
+import faang.school.urlshortenerservice.repository.UrlCacheRepository;
+import faang.school.urlshortenerservice.repository.UrlRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UrlService {
 
     private final HashCache hashCache;
+    private final UrlCacheRepository urlCacheRepository;
+    private final UrlRepository urlRepository;
+
+    @Transactional
+    public UrlDto getShortUrl(UrlDto urlDto){
+        String hash = hashCache.getHash();
+        Url url = Url.builder().
+                url(urlDto.getUrl())
+                .hash(hash)
+                .build();
+
+        urlRepository.save(url);
+        urlCacheRepository.save(hash, urlDto.getUrl());
+        return null;
+    }
 }
