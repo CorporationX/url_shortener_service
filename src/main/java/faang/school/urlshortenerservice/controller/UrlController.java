04ae8d008464a82e98b78cb7afc681a9824b479f
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-07-13 21:13:47 +0300

    Логика.

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -1,7 +1,9 @@
 package faang.school.urlshortenerservice.controller;
 
 import faang.school.urlshortenerservice.dto.UrlDto;
+import faang.school.urlshortenerservice.service.UrlService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/url")
+@RequiredArgsConstructor
 public class UrlController {
+
+    private final UrlService urlService;
 
     @PostMapping
     public UrlDto createShortUrl(@Valid @RequestBody UrlDto urlDto) {
-
+        return urlService.createShortUrl(urlDto);
     }
-
-
 }
--- src/main/java/faang/school/urlshortenerservice/service/UrlService.java
+++ src/main/java/faang/school/urlshortenerservice/service/UrlService.java
@@ -1,13 +1,36 @@
 package faang.school.urlshortenerservice.service;
 
+import faang.school.urlshortenerservice.dto.UrlDto;
+import faang.school.urlshortenerservice.entity.Url;
+import faang.school.urlshortenerservice.mapper.UrlMapper;
+import faang.school.urlshortenerservice.repositoy.UrlCacheRepository;
+import faang.school.urlshortenerservice.repositoy.UrlRepository;
+import faang.school.urlshortenerservice.service.cache.HashCache;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UrlService {
 
     private final UrlRepository urlRepository;
 
     private final UrlCacheRepository urlCacheRepository;
 
+    private final HashCache hashCache;
 
+    private final UrlMapper urlMapper;
+
+    public UrlDto createShortUrl(UrlDto urlDto) {
+        Url url = new Url();
+        String link = urlDto.getUrl();
+        String hash = hashCache.getHash().getHash();
+
+        url.setUrl(link);
+        url.setHash(hash);
+
+        urlRepository.save(url);
+        urlCacheRepository.putUrl(link, hash);
+        return urlMapper.toDto(url);
+    }
 }
