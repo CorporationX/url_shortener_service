4cd9d5d7d7ea0d6076799252b1e8fb5ed6ad01e9
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-07-13 22:21:47 +0300

    Логика.

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -4,10 +4,9 @@ import faang.school.urlshortenerservice.dto.UrlDto;
 import faang.school.urlshortenerservice.service.UrlService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/url")
@@ -19,5 +18,12 @@ public class UrlController {
     @PostMapping
     public UrlDto createShortUrl(@Valid @RequestBody UrlDto urlDto) {
         return urlService.createShortUrl(urlDto);
+    }
+
+    @GetMapping("/{hash}")
+    public RedirectView getRedirectView(@PathVariable("hash") String hash) {
+        RedirectView redirectView = urlService.getRedirectView(hash);
+        redirectView.setStatusCode(HttpStatus.FOUND);
+        return redirectView;
     }
 }
--- src/main/java/faang/school/urlshortenerservice/service/UrlService.java
+++ src/main/java/faang/school/urlshortenerservice/service/UrlService.java
@@ -6,8 +6,10 @@ import faang.school.urlshortenerservice.mapper.UrlMapper;
 import faang.school.urlshortenerservice.repositoy.UrlCacheRepository;
 import faang.school.urlshortenerservice.repositoy.UrlRepository;
 import faang.school.urlshortenerservice.service.cache.HashCache;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Service
 @RequiredArgsConstructor
@@ -32,5 +34,13 @@ public class UrlService {
         urlRepository.save(url);
         urlCacheRepository.putUrl(link, hash);
         return urlMapper.toDto(url);
+    }
+
+    public RedirectView getRedirectView(String hash) {
+        String url = urlCacheRepository.getUrl(hash)
+                .orElseGet(() -> urlRepository.findById(hash)
+                        .orElseThrow(() -> new EntityNotFoundException(String.format("Post with id %s not found", hash)))
+                        .getUrl());
+        return new RedirectView(url);
     }
 }
