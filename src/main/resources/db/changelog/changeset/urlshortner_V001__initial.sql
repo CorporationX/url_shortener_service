19be9e1d52cc0378c41c6d46e7c37ec039ab46c6
Th0rn-dev <solnek33@gmail.com>
2024-07-14 20:16:49 +0300

    added structure db

---
+++ src/main/resources/db/changelog/changeset/urlshortner_V001__initial.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS hash(
+    hash       varchar(6) PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    url        varchar(256),
+    created_at timestamptz DEFAULT current_timestamp
+);
+
+CREATE SEQUENCE IF NOT EXISTS unique_number_seq;
