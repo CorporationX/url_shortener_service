6ce231c88cc69583ad1bc3c18d917309e8c30fa3
Th0rn-dev <solnek33@gmail.com>
2024-07-14 20:28:24 +0300

    fix tables

--- src/main/resources/db/changelog/changeset/urlshortner_V001__initial.sql
+++ src/main/resources/db/changelog/changeset/urlshortner_V001__initial.sql
@@ -1,7 +1,11 @@
-CREATE TABLE IF NOT EXISTS hash(
+CREATE TABLE IF NOT EXISTS url(
     hash       varchar(6) PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
     url        varchar(256),
     created_at timestamptz DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS hash (
+    hash varchar(6) PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE
 );
 
 CREATE SEQUENCE IF NOT EXISTS unique_number_seq;
