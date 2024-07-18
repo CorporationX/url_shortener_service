709007d95b966a86d0e46ffc31ed1fd395afcd20
Th0rn-dev <solnek33@gmail.com>
2024-07-18 17:58:39 +0300

    refactoring create tables

--- src/main/resources/db/changelog/changeset/urlshortner_V001__initial.sql
+++ src/main/resources/db/changelog/changeset/urlshortner_V001__initial.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS url(
-    hash       varchar(6) PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
-    url        varchar(256),
+    hash       varchar(6) PRIMARY KEY,
+    url        varchar(256) NOT NULL UNIQUE,
     created_at timestamptz DEFAULT current_timestamp
 );
 
 CREATE TABLE IF NOT EXISTS hash (
-    hash varchar(6) PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE
+    hash varchar(6) PRIMARY KEY
 );
 
-CREATE SEQUENCE IF NOT EXISTS unique_number_seq;
+CREATE SEQUENCE IF NOT EXISTS unique_number_seq AS INT START 1;
