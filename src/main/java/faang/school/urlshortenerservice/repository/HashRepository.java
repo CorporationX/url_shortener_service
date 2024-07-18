2bc4d34b0380f989e0e1df564842dec7247e8b46
Th0rn-dev <solnek33@gmail.com>
2024-07-18 21:26:54 +0300

    created hash repository with batch save entity

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,24 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.model.Hash;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends JpaRepository<Hash, Long> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT nextval('unique_numbers_seq') from generate_series(1, :n)
+            """)
+    List<Long> getUniqueNumbers(Long n);
+
+    @Modifying
+    @Query(nativeQuery = true, value = """
+            DELETE from hash WHERE hash in (SELECT h.* FROM hash h LIMIT :n) returning *
+             """)
+    List<Hash> getHashBatch(Long n);
+}
