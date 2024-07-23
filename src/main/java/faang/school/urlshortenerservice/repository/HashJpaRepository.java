b216b4bd16c9c4e401fcb5b9da3f0d0799e8336d
AlexHor230 <apsekenesss@gmail.com>
2024-07-23 23:03:22 +0300

    Fixed HashJpa

--- src/main/java/faang/school/urlshortenerservice/repository/HashJpaRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashJpaRepository.java
@@ -2,6 +2,7 @@ package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.model.Hash;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,7 @@ public interface HashJpaRepository extends JpaRepository<Hash, String> {
     @Query(nativeQuery = true, value = "SELECT NEXTVAL('unique_number_seq')  FROM GENERATE_SERIES(1,:n)")
     List<Long> getNUniqueNumbers(long n);
 
+    @Modifying
     @Query(nativeQuery = true, value = """
             DELETE FROM hash
             WHERE hash IN
