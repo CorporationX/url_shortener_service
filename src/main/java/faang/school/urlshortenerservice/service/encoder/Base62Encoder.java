f07d5b7c5038c14eceabf9869150500345fdeaf5
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-07-31 21:29:58 +0300

    Сделал так, что бы ссылка была на 6 символов, для красоты.

--- src/main/java/faang/school/urlshortenerservice/service/encoder/Base62Encoder.java
+++ src/main/java/faang/school/urlshortenerservice/service/encoder/Base62Encoder.java
@@ -4,11 +4,14 @@ import faang.school.urlshortenerservice.entity.Hash;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Random;
 
 @Component
 public class Base62Encoder {
 
     private static final int BASE62 = 62;
+
+    private static final int LENGTH = 6;
 
     private static final String CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
@@ -20,10 +23,17 @@ public class Base62Encoder {
 
     private Hash encodeEach(Long number) {
         StringBuilder stringBuilder = new StringBuilder(1);
+
         do {
             stringBuilder.insert(0, CHARACTERS.charAt((int) (number % BASE62)));
             number /= BASE62;
         } while (number > 0);
+
+        Random random = new Random();
+
+        while (stringBuilder.length() < LENGTH) {
+            stringBuilder.insert(0, CHARACTERS.charAt(random.nextInt(BASE62)));
+        }
 
         Hash hash = new Hash();
         hash.setHash(stringBuilder.toString());
