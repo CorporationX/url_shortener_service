5e9e10cef4b4a528c83b64a067a468b7b2db1055
Daniel Krasnov <danielpyld.work@gmail.com>
2024-08-01 11:37:31 +0300

    Fix after cross review

--- src/main/java/faang/school/urlshortenerservice/dto/Request.java
+++ src/main/java/faang/school/urlshortenerservice/dto/Request.java
@@ -1,5 +1,7 @@
 package faang.school.urlshortenerservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.validator.constraints.URL;
 
@@ -10,6 +12,8 @@ import org.hibernate.validator.constraints.URL;
 @NoArgsConstructor
 public class Request {
 
+    @NotNull(message = "URL cannot be null")
+    @NotBlank(message = "URL cannot be blank")
     @URL(message = "Invalid URL")
     private String url;
 }
