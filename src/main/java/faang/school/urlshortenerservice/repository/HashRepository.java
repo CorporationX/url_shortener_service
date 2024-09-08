299f7a2478b621c394b3669e116720d0304845cd
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-09-08 18:27:46 +0500

    написал HashRepository

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -1,9 +1,31 @@
 package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.entity.Hash;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface HashRepository extends CrudRepository<Hash, Long> {
+public interface HashRepository extends JpaRepository<Hash, Long> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT nextval('unique_number_seq')
+            FROM generate_series(1, ?1)
+            """)
+    List<Long> getUniqueNumbers(long count);
+
+    @Modifying
+    @Query(nativeQuery = true, value = """
+            DELETE FROM hash
+            WHERE id IN (
+                SELECT id FROM hash
+                ORDER BY random()
+                LIMIT ?1
+                )
+            RETURNING hash
+            """)
+    List<String> getHashBatch(int batchSize);
 }
