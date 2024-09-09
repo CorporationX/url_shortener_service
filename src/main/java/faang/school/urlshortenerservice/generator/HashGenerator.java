da3da1528234ba43fbcf23b588491150ce36034f
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-09-09 08:58:33 +0500

    решил задание - Генерация хэша

---
+++ src/main/java/faang/school/urlshortenerservice/generator/HashGenerator.java
@@ -0,0 +1,29 @@
+package faang.school.urlshortenerservice.generator;
+
+import faang.school.urlshortenerservice.encoder.Base62Encoder;
+import faang.school.urlshortenerservice.entity.Hash;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class HashGenerator {
+
+    private final HashRepository hashRepository;
+    private final Base62Encoder base62Encoder;
+
+    @Value("${generator.hash.count}")
+    private long countUniqueNumbers;
+
+    @Async("asyncExecutor")
+    public void generateBatch(){
+        List<Long> uniqueNumbers = hashRepository.getUniqueNumbers(countUniqueNumbers);
+        List<Hash> hashes = base62Encoder.encode(uniqueNumbers).stream().map(Hash::new).toList();
+        hashRepository.saveAll(hashes);
+    }
+}
