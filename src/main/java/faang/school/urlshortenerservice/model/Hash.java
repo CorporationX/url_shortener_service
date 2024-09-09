efb7e60a2302d27c6cef83e2c9e0dbc8121c0e42
Nikita Volzhin <vlzh.nik@mail.ru>
2024-09-09 23:08:36 +0500

    BJS2-28086 - added HashRepository

--- src/main/java/faang/school/urlshortenerservice/model/Hash.java
+++ src/main/java/faang/school/urlshortenerservice/model/Hash.java
@@ -3,11 +3,15 @@ package faang.school.urlshortenerservice.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "hash")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Hash {
     @Id
     private String hash;
