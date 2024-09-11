ebfb682359fce7ee060c8eaa0abe374ee2d5e080
Caveri95 <ura22051995@gmail.com>
2024-09-11 20:05:26 +0300

    BJS2-27888
    - Разнес миграции по разным файлам

--- src/main/resources/db/changelog/changeset/V001_urlshortener_service_url.sql
+++ src/main/resources/db/changelog/changeset/V001_urlshortener_service_url.sql
@@ -4,10 +4,3 @@ CREATE TABLE url
     url        varchar(2048) NOT NULL UNIQUE,
     created_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE hash
-(
-    hash varchar(6) NOT NULL PRIMARY KEY
-);
-
-CREATE SEQUENCE unique_number_seq START WITH 1 INCREMENT BY 1;
