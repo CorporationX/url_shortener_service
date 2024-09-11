e3f17e555beb6e4b106add737cd5ebad28336a79
Nikita Volzhin <vlzh.nik@mail.ru>
2024-09-11 22:55:16 +0500

    BJS2-28081 - added UrlExceptionHandler

---
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlExceptionHandler.java
@@ -0,0 +1,58 @@
+package faang.school.urlshortenerservice.controller;
+
+import faang.school.urlshortenerservice.exception.ErrorResponse;
+import faang.school.urlshortenerservice.exception.HashCacheException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class UrlExceptionHandler {
+
+    @ExceptionHandler(HashCacheException.class)
+    public ResponseEntity<Object> hashCacheException(HashCacheException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .details(e.getMessage())
+                .message("Failed to retrieve hash")
+                .build();
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        FieldError fieldError = (FieldError) ex.getBindingResult().getAllErrors().get(0);
+        String fieldName = fieldError.getField();
+        String errorMessage = fieldError.getDefaultMessage();
+
+        return new ResponseEntity<>(Map.of(fieldName, errorMessage), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        String message = "Duplicate entry: the value you are trying to insert already exists";
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .details(e.getMostSpecificCause().getMessage())
+                .message(message)
+                .build();
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGlobalException(Exception e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .details(e.getMessage())
+                .message("Internal Server Error")
+                .build();
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
