ee40b2abcb78762fc421ded518247498beaf4610
Kirill Maltsev <opp.2211@gmail.com>
2024-09-12 04:01:16 +0500

    BJS2-28017: Работа с базой(HashRepository)

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Hash.java
@@ -0,0 +1,19 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Hash {
+
+    @Id
+    private String hash;
+}
---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,25 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends JpaRepository<Hash, String> { //todo: 6. Не нужно мапить эти данные на какие-либо hibernate сущности.
+
+    @Query(nativeQuery = true, value = """
+            SELECT nextval('unique_number_seq') FROM generate_series(1, :n);
+            """)
+    List<Long> getUniqueNumbers(long n);
+
+    @Query(nativeQuery = true, value = """
+            DELETE FROM hash h
+                   WHERE h.hash IN
+                         (SELECT hash from hash LIMIT 2)
+            returning h.hash;
+            """)
+    List<String> getHashBatch(long n);
+}
