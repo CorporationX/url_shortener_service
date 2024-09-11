a92c9b741be32f99530017816857c21684d73e4f
Nurlan Turemuratov <nurlant2003@gmail.com>
2024-09-11 15:10:03 +0500

    Работа с базой(HashRepository)

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,26 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends CrudRepository<Hash, Long> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT nextval('unique_number_seq') FROM generate_series(1, :n)
+            """)
+    List<Long> getUniqueNumbers(long n);
+
+    @Transactional
+    @Query(nativeQuery = true, value = """
+            DELETE FROM hash WHERE hash IN (
+                SELECT hash FROM hash LIMIT :n
+            ) RETURNING *
+            """)
+    List<String> getHashBatch(int n);
+}
