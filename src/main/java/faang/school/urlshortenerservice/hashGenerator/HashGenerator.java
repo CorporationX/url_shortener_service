ad69dd241863ec77a148945c1952e11fee1214b4
Caveri95 <ura22051995@gmail.com>
2024-09-11 23:04:19 +0300

    BJS2-27887
    - Создан класс, который кодирует число в строку по алгоритму base62
    - Создан класс HashGenerator для созранения уникальных хешей в таблицу hash

---
+++ src/main/java/faang/school/urlshortenerservice/hashGenerator/HashGenerator.java
@@ -0,0 +1,30 @@
+package faang.school.urlshortenerservice.hashGenerator;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class HashGenerator {
+
+    @Value("${hash.generate_batch_size}")
+    private int generateBatchSize;
+
+    private final HashRepository hashRepository;
+    private final Base62Encoder base62Encoder;
+
+    @Async("executorService")
+    public void generateHash() {
+        List<Long> uniqueNumbers = hashRepository.getUniqueNumbers(generateBatchSize);
+
+        List<Hash> hashes = uniqueNumbers.stream().map(base62Encoder::encode).map(Hash::new).toList();
+
+        hashRepository.saveAll(hashes);
+    }
+}
