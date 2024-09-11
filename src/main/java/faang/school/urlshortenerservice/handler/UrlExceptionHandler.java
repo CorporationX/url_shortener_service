dcc784b7e262839cd45d557656f484e9bc71f9fb
OlgaK888 <olgakurilovi4@gmail.com>
2024-09-11 18:55:27 +0300

    Updated UrlExceptionHandler

--- src/main/java/faang/school/urlshortenerservice/handler/UrlExceptionHandler.java
+++ src/main/java/faang/school/urlshortenerservice/handler/UrlExceptionHandler.java
@@ -11,7 +11,7 @@ import java.net.MalformedURLException;
 
 @RestControllerAdvice
 @Slf4j
-public class GlobalExceptionHandler {
+public class UrlExceptionHandler {
 
     @ExceptionHandler(MalformedURLException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
