1a9e53b5c86b39d7d2b9fdaa66a6b4632ea25a32
V1nGeri|Artem Radkevich <artemka20040613@gmail.com>
2024-09-12 17:42:02 +0300

    HashRepository

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Hash.java
@@ -0,0 +1,20 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "hash")
+public class Hash {
+    @Id
+    private String hash;
+}
