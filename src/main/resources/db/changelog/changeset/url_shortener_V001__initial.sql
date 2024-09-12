70d344c23a81e67f9a9376c48adf45a4a8576139
Sergey Shkaev <shkaev.ss@mail.ru>
2024-09-12 12:12:04 +0300

    BJS2-28272 - done

--- src/main/resources/db/changelog/changeset/url_shortener_V001__initial.sql
+++ src/main/resources/db/changelog/changeset/url_shortener_V001__initial.sql
@@ -2,12 +2,16 @@ CREATE SEQUENCE unique_hash_number_seq
     START WITH 1
     INCREMENT BY 1;
 
-CREATE TABLE url (
-    hash VARCHAR(6) PRIMARY KEY UNIQUE,
-    url VARCHAR(128) NOT NULL UNIQUE,
+CREATE TABLE url
+(
+    id         BIGSERIAL PRIMARY KEY,
+    hash       VARCHAR(6) UNIQUE,
+    url        VARCHAR(128) NOT NULL,
     created_at timestamptz DEFAULT current_timestamp
 );
 
-CREATE TABLE hash (
-    hash VARCHAR(6) PRIMARY KEY UNIQUE
+CREATE TABLE hash
+(
+    id   BIGSERIAL PRIMARY KEY,
+    hash VARCHAR(6) NOT NULL UNIQUE
 );
