547d97d32be572500aee79307c8e58099b89bc3d
romax <maksimovskii.roman@yandex.ru>
2024-09-12 21:23:13 +0300

    BJS2-28288 Created HashRepository and Hash

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Hash.java
@@ -0,0 +1,16 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "hash")
+@Data
+public class Hash {
+    @Id
+    @Column(name = "hash")
+    private String hash;
+}
