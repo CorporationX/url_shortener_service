d23d2668ea5048f509dedc17b68cb9ca0bf3c8eb
Kirill Maltsev <opp.2211@gmail.com>
2024-09-13 03:27:58 +0500

    BJS2-28017: jpa profile

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -1,12 +1,14 @@
 package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.entity.Hash;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Profile(value = "jpa_profile")
 @Repository
 public interface HashRepository extends JpaRepository<Hash, String> { //todo: 6. Не нужно мапить эти данные на какие-либо hibernate сущности.
 
