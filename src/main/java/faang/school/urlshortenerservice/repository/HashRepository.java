aa93b93bd0fa47657fd4dc19bd4efd3e64b1d941
Kostantyn Huzil <huzil.konstantyn@gmail.com>
2024-09-13 15:41:55 +0200

    BJS2-28104 - Работа с базой(HashRepository)
    - Added batch operations for hash retrieval and deletion

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -2,6 +2,7 @@ package faang.school.urlshortenerservice.repository;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,16 +14,30 @@ import java.util.stream.Collectors;
 public class HashRepository {
     private final JdbcTemplate jdbcTemplate;
 
+    @Value("${hash.batch.size}")
+    private int hashBatchSize;
+
     public List<Long> getUniqueNumbers(int n) {
-        return jdbcTemplate.queryForList("SELECT nextval('unique_number_seq') FROM generate_series(1, ?)", Long.class, n);
+        return jdbcTemplate.queryForList(
+            "SELECT nextval('unique_number_seq') FROM generate_series(1, ?)",
+            Long.class,
+            n
+        );
     }
 
     public void save(List<String> hashes) {
-        jdbcTemplate.batchUpdate("INSERT INTO (hash) VALUES (?)", hashes, hashes.size(), (ps, hash) -> ps.setString(1, hash));
+        jdbcTemplate.batchUpdate(
+            "INSERT INTO (hash) VALUES (?)",
+            hashes, hashes.size(), (ps, hash) -> ps.setString(1, hash)
+        );
     }
 
-    public List<String> getHashBatch(int n) {
-        List<String> hashes = jdbcTemplate.queryForList("SELECT hash FROM hash ORDER BY RANDOM() LIMIT ?", String.class, n);
+    public List<String> getHashBatch() {
+        List<String> hashes = jdbcTemplate.queryForList(
+            "SELECT hash FROM hash ORDER BY RANDOM() LIMIT ?",
+            String.class,
+            hashBatchSize
+        );
 
         if (!hashes.isEmpty()) {
             String placeholders = hashes.stream()
