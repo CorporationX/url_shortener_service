909e8334c8ed283d3f3e58663b35c86e4d08d643
Kostantyn Huzil <huzil.konstantyn@gmail.com>
2024-09-13 22:12:24 +0200

    BJS2-28104 - Работа с базой(HashRepository)
    - Reformated code

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -2,7 +2,6 @@ package faang.school.urlshortenerservice.repository;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -14,9 +13,6 @@ import java.util.stream.Collectors;
 public class HashRepository {
     private final JdbcTemplate jdbcTemplate;
 
-    @Value("${hash.batch.size}")
-    private int hashBatchSize;
-
     public List<Long> getUniqueNumbers(int n) {
         return jdbcTemplate.queryForList(
             "SELECT nextval('unique_number_seq') FROM generate_series(1, ?)",
@@ -27,16 +23,18 @@ public class HashRepository {
 
     public void save(List<String> hashes) {
         jdbcTemplate.batchUpdate(
-            "INSERT INTO (hash) VALUES (?)",
-            hashes, hashes.size(), (ps, hash) -> ps.setString(1, hash)
+            "INSERT INTO hash VALUES (?)",
+            hashes,
+            hashes.size(),
+            (ps, hash) -> ps.setString(1, hash)
         );
     }
 
-    public List<String> getHashBatch() {
+    public List<String> getHashBatch(int n) {
         List<String> hashes = jdbcTemplate.queryForList(
             "SELECT hash FROM hash ORDER BY RANDOM() LIMIT ?",
             String.class,
-            hashBatchSize
+            n
         );
 
         if (!hashes.isEmpty()) {
