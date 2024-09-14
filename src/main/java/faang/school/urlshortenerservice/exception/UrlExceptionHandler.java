ddf30d81a109c86b5fc97c3079035fac22786cde
Andrei Korshuk <a.v.korshuk@gmail.com>
2024-09-14 21:56:53 +0300

    Add Exception handler [BJS2-28307]

---
+++ src/main/java/faang/school/urlshortenerservice/exception/UrlExceptionHandler.java
@@ -0,0 +1,39 @@
+package faang.school.urlshortenerservice.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class UrlExceptionHandler {
+
+    @ExceptionHandler({DataValidationException.class, EntityNotFoundException.class, IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleDataValidationException(Exception exception) {
+        return exception.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(
+                        error -> ((FieldError) error).getField(),
+                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), "")
+                ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleGenericException(Exception exception) {
+        return "An unexpected error occurred: " + exception.getMessage();
+    }
+}
