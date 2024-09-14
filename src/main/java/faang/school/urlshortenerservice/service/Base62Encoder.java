4bdaa0dbd6d6343f8ee77cfb49d2a09f2c9406d1
grisatkac <grisatkac@mail.ru>
2024-09-15 00:42:40 +0300

    add base62 encoder

---
+++ src/main/java/faang/school/urlshortenerservice/service/Base62Encoder.java
@@ -0,0 +1,29 @@
+package faang.school.urlshortenerservice.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class Base62Encoder {
+
+    @Value("${hash.alphabet}")
+    private String base62Alphabet;
+
+    public List<String> encode(List<Long> numbers) {
+        return numbers.stream()
+                .map(this::encodeNumber)
+                .toList();
+    }
+
+    private String encodeNumber(Long number) {
+        StringBuilder base62 = new StringBuilder();
+        while (number > 0) {
+            base62.append(base62Alphabet.charAt((int) (number % base62Alphabet.length())));
+            number /= base62Alphabet.length();
+        }
+
+        return base62.toString();
+    }
+}
