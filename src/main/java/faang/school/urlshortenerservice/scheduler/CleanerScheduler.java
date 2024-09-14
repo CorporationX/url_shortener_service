494430789baaa2908043432914502ca88b86dcd0
dobrevd <dobrev@ukr.net>
2024-09-14 16:26:38 +0200

    feature-BJS2-27791

---
+++ src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
@@ -0,0 +1,32 @@
+package faang.school.urlshortenerservice.scheduler;
+
+import faang.school.urlshortenerservice.repository.HashRepository;
+import faang.school.urlshortenerservice.repository.UrlRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class CleanerScheduler {
+    @Value("${app.scheduling.removal.months_before_expiration}")
+    private int monthsBeforeExpiration;
+
+    private final UrlRepository urlRepository;
+    private final HashRepository hashRepository;
+
+    @Scheduled(cron = "${app.scheduling.removal.cron}")
+    @Transactional
+    public void removeExpiredUrls(){
+        var expirationTime = LocalDateTime.now().minusMonths(monthsBeforeExpiration);
+        var deletedHashes = urlRepository.deleteUrlsOlderThan(expirationTime);
+
+        if (!deletedHashes.isEmpty()) {
+            hashRepository.saveAll(deletedHashes);
+        }
+    }
+}
