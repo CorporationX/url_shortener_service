c970411b205de86321913a19b80e780c3c0cb9bc
OlgaK888 <olgakurilovi4@gmail.com>
2024-09-14 11:08:36 +0300

    Caching free hashes

---
+++ src/main/java/faang/school/urlshortenerservice/cache/AsyncExecutorForHashCash.java
@@ -0,0 +1,38 @@
+package faang.school.urlshortenerservice.cache;
+
+import faang.school.urlshortenerservice.generotor.HashGenerator;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Component
+@RequiredArgsConstructor
+public class AsyncExecutorForHashCash {
+
+    private final HashRepository hashRepository;
+    private final HashGenerator hashGenerator;
+    private final Lock lock = new ReentrantLock();
+
+    @Async("executor")
+    public void exclusiveTransferHashBatch(int batchSize, ConcurrentLinkedQueue<String> queue) {
+        if (lock.tryLock()) {
+            try {
+                List<String> hashBatch = hashRepository.getHashBatch(batchSize);
+                queue.addAll(hashBatch);
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    @Async("executor")
+    public void asyncGenerateBatch() {
+        hashGenerator.generateBatch();
+    }
+}
---
+++ src/main/java/faang/school/urlshortenerservice/generotor/HashGenerator.java
@@ -0,0 +1,10 @@
+package faang.school.urlshortenerservice.generotor;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HashGenerator {
+
+    public void generateBatch() {
+    }
+}
