dd4d44ec792701096596719823640fc526d4b09a
Andrei Kuzmin <Andreytko@mail.ru>
2024-09-15 17:11:49 +0300

    writing exceptionHandler

---
+++ src/main/java/faang/school/urlshortenerservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package faang.school.urlshortenerservice.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
