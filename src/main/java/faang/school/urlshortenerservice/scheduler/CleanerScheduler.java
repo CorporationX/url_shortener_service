56f68d98cdf0e7761298a846070120e78a153048
Mikhail Fokin <fokinms@gmail.com>
2024-09-16 02:16:58 +0400

    Create CleanerScheduler

---
+++ src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
@@ -0,0 +1,18 @@
+package faang.school.urlshortenerservice.scheduler;
+
+import faang.school.urlshortenerservice.service.UrlService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CleanerScheduler {
+
+    private final UrlService urlService;
+
+    @Scheduled(cron = "${spring.url.scheduling-cron}")
+    public void cleanOldEntriesAndRetrieveUnusedHashes() {
+        urlService.deleteOldEntriesAndRetrieveUnusedHashes();
+    }
+}
