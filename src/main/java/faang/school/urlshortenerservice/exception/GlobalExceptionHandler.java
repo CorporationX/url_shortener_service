24b2dded6f4ff4149900a307fa1ac78b81c78650
Andrei Kuzmin <Andreytko@mail.ru>
2024-09-15 17:33:05 +0300

    done GlobalExceptionHandler

--- src/main/java/faang/school/urlshortenerservice/exception/GlobalExceptionHandler.java
+++ src/main/java/faang/school/urlshortenerservice/exception/GlobalExceptionHandler.java
@@ -1,10 +1,33 @@
 package faang.school.urlshortenerservice.exception;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleDataValidationException(Exception exception) {
+        return exception.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getAllErrors().stream()
+                .collect(Collectors.toMap(
+                        error -> ((FieldError) error).getField(),
+                        error -> Objects.requireNonNullElse(error.getDefaultMessage(), ""))
+                );
+    }
 
 }
