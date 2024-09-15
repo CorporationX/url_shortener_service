40eea464282532951b8ecec4aa415887dd02dece
malkov34 <malk.2011@yandex.ru>
2024-09-15 19:19:35 +0300

    added urlShortner service

--- src/main/java/faang/school/urlshortenerservice/model/Url.java
+++ src/main/java/faang/school/urlshortenerservice/model/Url.java
@@ -24,7 +24,7 @@ public class Url {
     @Column(name = "hash", length = 6)
     private String hash;
 
-    @Column(name = "url", length = 128, nullable = false)
+    @Column(name = "url", length = 2048, nullable = false)
     private String url;
 
     @CreationTimestamp
