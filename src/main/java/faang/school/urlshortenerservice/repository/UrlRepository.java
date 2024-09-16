b5f891b4d35b46d6bd4de0a7b01d559db1e4fbbe
Kirill Maltsev <opp.2211@gmail.com>
2024-09-17 01:35:57 +0500

    BJS2-28020: Джоб для освобождения неиспользуемых хэшей

--- src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
@@ -2,10 +2,20 @@ package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UrlRepository extends JpaRepository<Url, String> {
 
     Optional<Url> findByUrl(String url);
+
+    @Query(nativeQuery = true, value = """
+            DELETE FROM url u
+                   WHERE u.created_at <= :before
+            returning u.hash;
+            """)
+    List<String> pollBefore(LocalDateTime before);
 }
---
+++ src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
@@ -0,0 +1,31 @@
+package faang.school.urlshortenerservice.scheduler;
+
+import faang.school.urlshortenerservice.repository.HashRepository;
+import faang.school.urlshortenerservice.repository.UrlRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CleanerScheduler {
+
+    private final UrlRepository urlRepository;
+    private final HashRepository hashRepository;
+
+    @Scheduled(cron = "${app.cleaner-scheduler.cron:0 0 0 * * *}")
+    @Transactional
+    public void releaseHashes() {
+        log.info("Start scheduled task to release hashes older than 1 year");
+        LocalDateTime nowMinusYear = LocalDateTime.now().minusYears(1);
+        List<String> hashes = urlRepository.pollBefore(nowMinusYear);
+        hashRepository.saveBatch(hashes);
+        log.info("{} hashes was released", hashes.size());
+    }
+}
