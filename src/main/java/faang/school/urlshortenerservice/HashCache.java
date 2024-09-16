5e7d78d57f70ac06abe27b2da7372e154eb1ff06
romax <maksimovskii.roman@yandex.ru>
2024-09-16 17:26:42 +0300

    Added loggers Refactoring

--- src/main/java/faang/school/urlshortenerservice/HashCache.java
+++ src/main/java/faang/school/urlshortenerservice/HashCache.java
@@ -3,6 +3,7 @@ package faang.school.urlshortenerservice;
 import faang.school.urlshortenerservice.entity.Hash;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class HashCache {
@@ -18,29 +20,31 @@ public class HashCache {
     private int capacity;
     @Value("${hash.cache.min_percentage_filling}")
     private int minPercentageFilling;
-    private Queue<Hash> hashes;
+    private Queue<Hash> cache;
     private final AtomicBoolean isFilling = new AtomicBoolean(false);
     private final HashGenerator hashGenerator;
 
     @PostConstruct
     public void init() {
-        hashes = new ArrayBlockingQueue<>(capacity);
+        cache = new ArrayBlockingQueue<>(capacity);
 
-        hashes.addAll(hashGenerator.getHashes(capacity));
+        log.info("Initializing hash cache");
+        cache.addAll(hashGenerator.getHashes(capacity));
     }
 
     public Hash getHash() {
         if (percentageFilling() < minPercentageFilling) {
             if (isFilling.compareAndSet(false, true)) {
-                hashGenerator.getHashesAsync(capacity - hashes.size())
-                        .thenAccept(hashes::addAll)
+                log.info("Filling hash cache");
+                hashGenerator.getHashesAsync(capacity - cache.size())
+                        .thenAccept(cache::addAll)
                         .thenRun(() -> isFilling.set(false));
             }
         }
-        return hashes.poll();
+        return cache.poll();
     }
 
     private int percentageFilling() {
-        return hashes.size() * 100 / capacity;
+        return cache.size() * 100 / capacity;
     }
 }
--- src/main/java/faang/school/urlshortenerservice/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/HashGenerator.java
@@ -4,6 +4,7 @@ import faang.school.urlshortenerservice.entity.Hash;
 import faang.school.urlshortenerservice.repository.HashRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class HashGenerator {
@@ -22,6 +24,7 @@ public class HashGenerator {
     @Async
     @Transactional
     public void generateBatch() {
+        log.info("Generating {} hashes", batchSize);
         List<Long> numbers = hashRepository.getUniqueNumbers(batchSize);
         List<Hash> hashes = encoder.encode(numbers).stream()
                 .map(Hash::new)
@@ -32,10 +35,12 @@ public class HashGenerator {
     @Transactional
     public List<Hash> getHashes(int amount) {
         List<Hash> hashes = hashRepository.getHashBatch(amount);
+        log.info("Got {} hashes from DB", hashes.size());
         if (hashes.size() < amount) {
             generateBatch();
+            log.info("Get {} hashes from DB", amount - hashes.size());
+            hashes.addAll(hashRepository.getHashBatch(amount - hashes.size()));
         }
-        hashes.addAll(hashRepository.getHashBatch(amount - hashes.size()));
         return hashes;
     }
 
