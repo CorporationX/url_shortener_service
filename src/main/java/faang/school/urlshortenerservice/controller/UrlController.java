efd9c383ab23cc5ebf032407dfa9155d653bd2cc
Gevorg <a0964593@gmail.com>
2024-09-16 21:04:47 +0300

    in progress

---
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -0,0 +1,10 @@
+package faang.school.urlshortenerservice.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UrlController {
+    @PostMapping("/url")
+    public
+}
