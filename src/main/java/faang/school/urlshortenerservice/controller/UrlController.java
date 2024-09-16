471f07516478e5f267e4b6587d2d035890446b7d
Gevorg <a0964593@gmail.com>
2024-09-16 21:18:16 +0300

    in progress

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -1,10 +1,15 @@
 package faang.school.urlshortenerservice.controller;
 
+import faang.school.urlshortenerservice.dto.UrlDto;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UrlController {
     @PostMapping("/url")
-    public
+    public String createShortUrl(@RequestBody @Valid UrlDto urlDto) {
+        return urlService.createShortUrl(urlDto);
+    }
 }
