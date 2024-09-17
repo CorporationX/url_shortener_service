d98ab858f26b1f40aa3381e33d9b0f36a1a553fe
Rauan Kaztaev <rtishtyk@gmail.com>
2024-09-17 23:08:00 +0500

    minor fix

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/shortener")
 public class UrlController {
     private final UrlService urlService;
 
--- src/main/java/faang/school/urlshortenerservice/repository/UrlCacheRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlCacheRepository.java
@@ -2,19 +2,32 @@ package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.dto.UrlDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class UrlCacheRepository {
     private final RedisTemplate<String, String> redisTemplate;
 
+    @Value("${spring.cache.redis.time-to-live}")
+    private int ttl;
+
     public void save(UrlDto urlDto) {
-        redisTemplate.opsForValue().set(urlDto.getUrl(), urlDto.getHash());
+        redisTemplate.opsForValue().set(urlDto.getUrl(), urlDto.getHash(), ttl);
+        redisTemplate.opsForValue().set(urlDto.getHash(), urlDto.getUrl(), ttl);
+        log.info("Put into cache: {}", urlDto);
     }
 
     public String getHashByUrl(String url) {
         return redisTemplate.opsForValue().get(url);
+    }
+
+
+    public String getUrlByHash(String hash) {
+        return redisTemplate.opsForValue().get(hash);
     }
 }
