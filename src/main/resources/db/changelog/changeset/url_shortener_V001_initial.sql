71d08f8b6f7b80ee5fb2f94e83efd180e9a9946f
DonMaxim <donmaxim1990@gmail.com>
2024-09-16 23:23:10 -0300

    Add tables hash, url and sequence

---
+++ src/main/resources/db/changelog/changeset/url_shortener_V001_initial.sql
@@ -0,0 +1,13 @@
+CREATE TABLE url (
+    hash VARCHAR(6) PRIMARY KEY,
+    url TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE hash (
+    hash VARCHAR(6) PRIMARY KEY
+);
+
+CREATE SEQUENCE unique_number_seq
+    START WITH 1
+    INCREMENT BY 1;
