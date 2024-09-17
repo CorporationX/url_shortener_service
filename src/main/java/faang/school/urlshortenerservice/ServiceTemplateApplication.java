edf9c72a73ef2a3320a015a963ab2d940c886a24
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-09-17 09:53:06 +0500

    Revert "Fix 4"

--- src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
+++ src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
@@ -3,7 +3,6 @@ package faang.school.urlshortenerservice;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
