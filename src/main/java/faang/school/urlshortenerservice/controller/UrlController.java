feeec766506da44d836868de79f95521e29fb181
Gevorg <a0964593@gmail.com>
2024-09-18 12:42:17 +0300

    done

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -1,14 +1,27 @@
 package faang.school.urlshortenerservice.controller;
 
 import faang.school.urlshortenerservice.dto.UrlDto;
+import faang.school.urlshortenerservice.service.UrlService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 @RestController
+@RequestMapping("/url")
+@RequiredArgsConstructor
 public class UrlController {
-    @PostMapping("/url")
+    private final UrlService urlService;
+    @GetMapping("/{hash}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public RedirectView redirectToLongUrl(@PathVariable String hash) {
+        val url = urlService.getLongUrlByHash(hash);
+        return new RedirectView(url);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String createShortUrl(@RequestBody @Valid UrlDto urlDto) {
         return urlService.createShortUrl(urlDto);
     }
