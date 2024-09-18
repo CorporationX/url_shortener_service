104c3e259b92e15cb598faba8168eac9a4d04035
Nikita <nikita.tolstv@gmail.com>
2024-09-18 20:31:56 +0300

    Added Bean for Executor and fixed LocalCache

--- src/main/java/faang/school/urlshortenerservice/generator/LocalCache.java
+++ src/main/java/faang/school/urlshortenerservice/generator/LocalCache.java
@@ -1,15 +1,15 @@
 package faang.school.urlshortenerservice.generator;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
@@ -23,9 +23,8 @@ public class LocalCache {
     @Value("${hash.cache.fill.percent}")
     private int fillPercent;
 
-    @Value("${hash.thread.pool.core}")
-    private int corePoolSize;
-
+    @Resource(name = "taskExecutor")
+    private final ThreadPoolTaskExecutor executor;
     private final Queue<String> hashes = new ConcurrentLinkedQueue<>();
     private final HashGenerator hashGenerator;
     private final AtomicBoolean filling = new AtomicBoolean(false);
@@ -44,14 +43,21 @@ public class LocalCache {
     public String getHash() {
         if (hashes.size() / capacity * 100 < fillPercent &&
                 filling.compareAndSet(false, true)) {
-            hashGenerator.getHashesAsync(capacity)
-                    .thenAccept(hashes::addAll)
-                    .exceptionally(ex -> {
-                        log.error("Error initializing LocalCache ", ex);
-                        return null;
-                    })
-                    .thenRun(() -> filling.set(false));
+            log.info("Local cache less than 20 percent");
+            executor.execute(this::fillHandler);
+            fillHandler();
         }
         return hashes.poll();
+    }
+
+    public void fillHandler() {
+        hashGenerator.getHashesAsync(capacity)
+                .thenAccept(hashes::addAll)
+                .exceptionally(ex -> {
+                    log.error("Error initializing LocalCache ", ex);
+                    return null;
+                })
+                .thenRun(() -> filling.set(false));
+
     }
 }
