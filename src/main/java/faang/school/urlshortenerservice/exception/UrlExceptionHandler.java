cdfc2a156456c6f976fcdb878171dfd8a2483d11
DonMaxim <donmaxim1990@gmail.com>
2024-09-19 02:26:17 -0300

    Add UrlExceptionHandler

--- src/main/java/faang/school/urlshortenerservice/exception/UrlExceptionHandler.java
+++ src/main/java/faang/school/urlshortenerservice/exception/UrlExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
