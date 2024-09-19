78b34bad7100b31b988337cb777718b6564e64b7
pazhik <untmum01@gmail.com>
2024-09-19 15:18:29 +0300

    Кэширование свободных хэшей

---
+++ src/main/java/faang/school/urlshortenerservice/service/HashCache.java
@@ -0,0 +1,40 @@
+package faang.school.urlshortenerservice.service;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Component
+@RequiredArgsConstructor
+public class HashCache {
+    private final HashGenerator hashGenerator;
+    private final HashRepository hashRepository;
+    @Value("${hash-cache.low-limit-percent:20}")
+    private int lowLimitPercent;
+    @Value("${hash-cache.capacity:1000}")
+    private int capacity;
+    private AtomicBoolean areFilling;
+    private final ArrayBlockingQueue<Hash> queue = new ArrayBlockingQueue<>(capacity);
+
+    public Hash getHash() {
+        if (queue.size() / capacity * 100 < lowLimitPercent) {
+            if (areFilling.compareAndSet(false, true)) {
+                fill();
+            }
+        }
+        return queue.poll();
+    }
+
+    @Async(value = "hashCacheTaskExecutor")
+    private void fill() {
+        queue.addAll(hashRepository.getHashBatch(capacity - queue.size()));
+        hashGenerator.generateBatch();
+        areFilling.set(false);
+    }
+}
