14dcc6fa596d5925e6bfaa021f920b0b5863c1ff
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-21 02:51:27 +0300

    feat: add `RedisUrlCache`

---
+++ src/main/java/faang/school/urlshortenerservice/config/RedisCacheConfig.java
@@ -0,0 +1,79 @@
+package faang.school.urlshortenerservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisCacheConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+    @Value("${spring.data.redis.connection_factory}")
+    private String factoryType;
+    @Value("${spring.cache.url-cache.ttl}")
+    private int ttl;
+
+    @Bean
+    @Lazy
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration configuration =
+                new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    @Lazy
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    @Primary
+    public RedisConnectionFactory connectionFactory() {
+        return switch (factoryType) {
+            case "jedis" -> jedisConnectionFactory();
+            case "lettuce" -> lettuceConnectionFactory();
+            default -> throw new IllegalStateException("Unexpected value: " + factoryType);
+        };
+    }
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(ttl))
+                .disableCachingNullValues()
+                .serializeValuesWith(
+                        RedisSerializationContext
+                                .SerializationPair
+                                .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                );
+    }
+
+    @Bean
+    RedisTemplate<String, String> redisTemplate(
+            RedisConnectionFactory connectionFactory
+    ) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
