66d3bccc51cec78efed4408b5378be615a8458ac
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-21 02:47:03 +0300

    fix: add annotation for batch insertion

--- src/main/java/faang/school/urlshortenerservice/entity/UrlHash.java
+++ src/main/java/faang/school/urlshortenerservice/entity/UrlHash.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "hash")
@@ -14,7 +15,13 @@ import lombok.NoArgsConstructor;
 @Builder(toBuilder = true)
 public class UrlHash {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "hash_id_generator")
+    @GenericGenerator(name = "hash_id_generator", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "hash_id_seq"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1")
+            })
     private long id;
 
     @Column(name = "hash", nullable = false, length = 8)
