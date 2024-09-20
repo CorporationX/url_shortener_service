ea1e6675edd9379207765f9cf12831fcd4961afb
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-21 02:39:06 +0300

    feat: change init script

--- src/main/resources/db/changelog/changeset/init.sql
+++ src/main/resources/db/changelog/changeset/init.sql
@@ -2,18 +2,22 @@ CREATE SEQUENCE unique_hash_number_seq
     START WITH 1
     INCREMENT BY 1;
 
+CREATE SEQUENCE hash_id_seq
+    START WITH 1
+    INCREMENT BY 1;
+
 create table if not exists url
 (
     id           bigint primary key generated always as IDENTITY UNIQUE,
     short_url    varchar(8),
     url          text,
-    created_at   timestamp default now(),
-    updated_at   timestamp default now(),
+    created_at   timestamp default CURRENT_TIMESTAMP,
+    updated_at   timestamp default CURRENT_TIMESTAMP,
     requested_at timestamp
 );
 
 create table if not exists hash
 (
-    id   bigint primary key generated always as IDENTITY UNIQUE,
+    id   bigint primary key default nextval('hash_id_seq'),
     hash varchar(8)
-)
+);
