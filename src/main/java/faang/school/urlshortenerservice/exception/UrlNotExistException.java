bafa9b162039fe25958e70a9911986978cf01131
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-22 15:28:01 +0300

    fix: change modifier of `UrlNotExistException.notExistentUlrId` to public

--- src/main/java/faang/school/urlshortenerservice/exception/UrlNotExistException.java
+++ src/main/java/faang/school/urlshortenerservice/exception/UrlNotExistException.java
@@ -2,7 +2,7 @@ package faang.school.urlshortenerservice.exception;
 
 public class UrlNotExistException extends RuntimeException {
 
-    final Long notExistentUlrId;
+    public final Long notExistentUlrId;
 
     public UrlNotExistException(long id) {
         super(String.format("Url with id %s does not exist", id));
