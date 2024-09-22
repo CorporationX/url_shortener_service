63ee0d6d2ad58967043f88d3ff8d095626caf287
grisatkac <grisatkac@mail.ru>
2024-09-23 01:43:03 +0300

    add locking

--- src/main/java/faang/school/urlshortenerservice/service/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/service/HashGenerator.java
@@ -3,18 +3,23 @@ package faang.school.urlshortenerservice.service;
 import faang.school.urlshortenerservice.model.Hash;
 import faang.school.urlshortenerservice.repository.HashRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class HashGenerator {
     private final HashRepository hashRepository;
     private final Base62Encoder base62Encoder;
+    private final Lock lock = new ReentrantLock();
 
     @Value("${unique-numbers.amount}")
     private int uniqueNumbersAmount;
@@ -22,10 +27,18 @@ public class HashGenerator {
     @Async(value = "hashTaskExecutor")
     @Transactional
     public void generateBatch() {
-        List<Long> uniqueNumbersToEncode = hashRepository.getUniqueNumbers(uniqueNumbersAmount);
-        List<String> hashesOfUniqueNumbers = base62Encoder.encode(uniqueNumbersToEncode);
+        if (lock.tryLock()) {
+            try {
+                List<Long> uniqueNumbersToEncode = hashRepository.getUniqueNumbers(uniqueNumbersAmount);
+                List<String> hashesOfUniqueNumbers = base62Encoder.encode(uniqueNumbersToEncode);
 
-        saveHashes(hashesOfUniqueNumbers);
+                saveHashes(hashesOfUniqueNumbers);
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            log.info("Hash generation are already in use");
+        }
     }
 
     @Transactional
