0241e924b415a108335abe2c6604115dc73d5351
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-22 15:20:13 +0300

    feat: add method `deleteOldULRs`

--- src/main/java/faang/school/urlshortenerservice/service/UrlShorterService.java
+++ src/main/java/faang/school/urlshortenerservice/service/UrlShorterService.java
@@ -4,15 +4,19 @@ import faang.school.urlshortenerservice.cache.HashCache;
 import faang.school.urlshortenerservice.cache.UrlCache;
 import faang.school.urlshortenerservice.dto.ShortUrlDto;
 import faang.school.urlshortenerservice.entity.Url;
+import faang.school.urlshortenerservice.entity.UrlHash;
 import faang.school.urlshortenerservice.exception.UrlNotExistException;
 import faang.school.urlshortenerservice.exception.UrlNotFoundException;
+import faang.school.urlshortenerservice.repository.UrlHashRepository;
 import faang.school.urlshortenerservice.repository.UrlRepository;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +24,7 @@ public class UrlShorterService {
     private final HashCache urlHashCache;
     private final UrlCache urlCache;
     private final UrlRepository urlRepository;
+    private final UrlHashRepository urlHashRepository;
 
     @Transactional
     public ShortUrlDto shortenUrl(String longUrl) {
@@ -48,5 +53,15 @@ public class UrlShorterService {
         u.setUrl(url);
         u.setUpdatedAt(LocalDateTime.now());
         urlCache.saveUrl(u.getShortUrl(), u.getUrl());
+    }
+
+    @Transactional
+    public void deleteOldULRs(LocalDate obsolescenceDate) {
+        List<Url> outdatedUrls = urlRepository.popAllOldUrl(obsolescenceDate);
+        urlHashRepository.saveAll(
+                outdatedUrls.stream()
+                        .map(u -> new UrlHash(u.getShortUrl()))
+                        .toList()
+        );
     }
 }
