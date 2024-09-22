eda978d16462d6621ebee9f29f02d62f42bcb420
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-09-22 15:39:22 +0300

    feat: `UniqueRangeRepository` has been separated to isolated interface

--- src/main/java/faang/school/urlshortenerservice/repository/UniqueRangeRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/UniqueRangeRepository.java
@@ -1,19 +1,9 @@
 package faang.school.urlshortenerservice.repository;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class UniqueRangeRepository {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    public List<Long> getNextUniqueRange(int size) {
-        String q = "SELECT nextval('unique_hash_number_seq') FROM generate_series(1, ?)";
-        return jdbcTemplate.queryForList(q, Long.class, size);
-    }
+public interface UniqueRangeRepository {
+    List<Long> getNextUniqueRange(int size);
 }
