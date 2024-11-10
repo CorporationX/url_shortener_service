a9aabbad4bbc7f3cbf8e0003629fa1115d8f2941
Andrew Romanov <r0m4n0v4v@gmail.com>
2024-11-10 22:05:26 +0300

    added db scheme

---
+++ src/main/resources/db/changelog/changeset/V001_init.sql
@@ -0,0 +1,13 @@
+create table if not exists url
+(
+    hash       varchar(6) primary key,
+    url        varchar(2048) not null,
+    created_at timestamp     not null default current_timestamp
+);
+
+create table if not exists hash
+(
+    hash varchar(6) primary key
+);
+
+create sequence unique_number_seq increment 1 start 1;
