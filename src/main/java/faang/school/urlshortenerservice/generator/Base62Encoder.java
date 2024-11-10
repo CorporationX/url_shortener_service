485d4150ab8bbd9495ed9c319b0d52d674902938
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-11-11 01:10:15 +0300

    Генерация списка хэшей (Base62Encoder)

---
+++ src/main/java/faang/school/urlshortenerservice/generator/Base62Encoder.java
@@ -0,0 +1,32 @@
+package faang.school.urlshortenerservice.generator;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Base62Encoder {
+
+    private final static String BASE62_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private final static int BASE62_LENGTH = BASE62_CHARS.length();
+
+    public List<String> encode(List<Long> numbers) {
+        List<String> hashes = new ArrayList<>();
+        StringBuilder encoded = new StringBuilder();
+
+        for (Long num : numbers) {
+            while (num > 0 && encoded.length() < 6) {
+                int remainder = (int) (num % BASE62_LENGTH);
+                encoded.append(BASE62_CHARS.charAt(remainder));
+                num /= BASE62_LENGTH;
+            }
+            encoded.reverse().toString();
+            hashes.add(String.valueOf(encoded));
+            encoded.setLength(0);
+        }
+
+        return hashes;
+    }
+
+}
