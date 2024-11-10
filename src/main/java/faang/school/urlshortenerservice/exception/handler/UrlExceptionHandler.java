5209948346a933212c6f2eb13570b6caafd128c9
Robert Oguzov <robertoguzov2@gmail.com>
2024-11-10 14:01:41 +0300

    Написал хендлер

---
+++ src/main/java/faang/school/urlshortenerservice/exception/handler/UrlExceptionHandler.java
@@ -0,0 +1,9 @@
+package faang.school.urlshortenerservice.exception.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UrlExceptionHandler {
+
+
+}
