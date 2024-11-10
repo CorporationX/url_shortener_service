0d8a900da7469a55df1460a5bf9d9a82f79756ea
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-11-10 20:41:25 +0300

    created DB structure

--- src/main/java/faang/school/urlshortenerservice/UrlShortenerServiceApplication.java
+++ src/main/java/faang/school/urlshortenerservice/UrlShortenerServiceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableFeignClients("school.faang.servicetemplate.client")
-public class ServiceTemplateApplication {
+@EnableFeignClients("faang.school.urlshortenerservice.client")
+public class UrlShortenerServiceApplication  {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ServiceTemplateApplication.class)
+        new SpringApplicationBuilder(UrlShortenerServiceApplication.class)
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
     }
