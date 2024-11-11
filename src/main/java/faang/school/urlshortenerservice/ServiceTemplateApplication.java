dc495863b15b0ab01471d803daf739f681fa0223
ifef228 <ifef228@gmail.com>
2024-11-11 09:07:27 +0300

    delete DataBaseProperties.java, update JdbcConfig.java

--- src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
+++ src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
@@ -1,17 +1,14 @@
 package faang.school.urlshortenerservice;
 
-import faang.school.urlshortenerservice.config.database.DataBaseProperties;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients("school.faang.servicetemplate.client")
-@EnableConfigurationProperties(DataBaseProperties.class)
 public class ServiceTemplateApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ServiceTemplateApplication.class)
