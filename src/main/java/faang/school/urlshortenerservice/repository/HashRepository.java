416282b3ab01ec4973a034d08cd6711559feb3d5
cemeh666 <semen-5555@mail.ru>
2024-11-11 13:04:08 +0300

    BJS2-43077 Работа с базой(HashRepository)

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,24 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends JpaRepository<Hash, String> {
+    @Query(nativeQuery = true, value = """
+            SELECT nextval('unique_hash_number_seq') FROM generate_series(1, :maxRange)
+            """)
+    List<Long> getUniqueNumbers(@Param("maxRange") int maxRange);
+
+    @Query(nativeQuery = true, value = """
+            DELETE FROM hash WHERE id IN (
+                SELECT id FROM hash LIMIT :batchSize
+            ) RETURNING *
+            """)
+    List<Hash> getHashBatch(@Param("batchSize") int batchSize);
+}
