4ab801c8aec73c9c48bdb5202e93a0f3ed8a5da3
Evgen <fragmatik.Oo@yandex.ru>
2024-11-11 10:37:55 +0300

    Создать структуру БД - BJS2-42856

---
+++ src/main/resources/db/changelog/changeset/V0001_init.sql
@@ -0,0 +1,10 @@
+CREATE SEQUENCE IF NOT EXISTS hash_sequence_number
+    START WITH 1
+    INCREMENT BY 1;
+
+CREATE TABLE IF NOT EXISTS url
+(
+    hash       VARCHAR(6) PRIMARY KEY,
+    url        varchar(2048) NOT NULL,
+    created_at TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
