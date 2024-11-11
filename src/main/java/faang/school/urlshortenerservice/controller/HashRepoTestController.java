de63dece3b5f1b3869e2efa1c9e4a24398ff8751
ifef228 <ifef228@gmail.com>
2024-11-11 10:53:16 +0300

    метод save с использованием батчей

--- src/main/java/faang/school/urlshortenerservice/controller/HashRepoTestController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/HashRepoTestController.java
@@ -4,6 +4,8 @@ import faang.school.urlshortenerservice.repository.HashRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,10 @@ public class HashRepoTestController {
     @GetMapping("/unique/{n}")
     public List<Integer> getUniqueNums(@PathVariable("n") int n) {
         return repository.getUniqueNumbers(n);
+    }
+
+    @PutMapping("/hashes")
+    public void putHashes(@RequestBody List<String> hashes) {
+        repository.save(hashes);
     }
 }
