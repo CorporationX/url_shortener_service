a47c2350e3b19f271ebec30f5e6c0b23ff33ca2e
Ilya Merkulov <imerkulov@fun.co>
2024-11-12 21:11:33 +0200

    BJS2-42636 Работа с базой(HashRepository)

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Url.java
@@ -0,0 +1,30 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "url")
+public class Url {
+
+    @Id
+    @NotNull
+    @Size(min = 1, max = 6)
+    private String hash;
+
+    @NotNull
+    @Size(min = 1, max = 4096)
+    private String url;
+
+    @NotNull
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
