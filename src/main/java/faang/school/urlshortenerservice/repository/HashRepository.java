1851650126e47e1df8d231d3284f2af37b17af9e
Aleh Biahun <stones2875@gmail.com>
2024-11-12 09:58:33 +0100

    feature-bjs2-42802 Generation of hash list

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++
@@ -1,7 +0,0 @@
-package faang.school.urlshortenerservice.repository;
-
-import faang.school.urlshortenerservice.model.entity.Hash;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HashRepository extends JpaRepository<Hash, Long> {
-}
