0cecc7ca5a7a9b71281239a82a0317d82b6d968e
Robert Oguzov <robertoguzov2@gmail.com>
2024-11-12 18:19:45 +0300

    Решение

--- src/main/java/faang/school/urlshortenerservice/service/HashGeneratorServiceImpl.java
+++ src/main/java/faang/school/urlshortenerservice/service/HashGeneratorServiceImpl.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
--- src/main/java/faang/school/urlshortenerservice/util/encoder/Base62Encoder.java
+++ src/main/java/faang/school/urlshortenerservice/util/encoder/Base62Encoder.java
@@ -1,6 +1,7 @@
 package faang.school.urlshortenerservice.util.encoder;
 
 import faang.school.urlshortenerservice.entity.Hash;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,9 @@ public class Base62Encoder extends Encoder<Long, Hash> {
 
     private static final String BASE_62_CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private static final int BASE_62_LENGTH = BASE_62_CHARACTERS.length();
+
+    @Value("${server.hash.generate.length}")
+    private int hashLength;
 
     @Override
     public Hash encode(Long number) {
@@ -19,6 +23,12 @@ public class Base62Encoder extends Encoder<Long, Hash> {
         while (number > 0) {
             builder.append(BASE_62_CHARACTERS.charAt((int) (number % BASE_62_LENGTH)));
             number /= BASE_62_LENGTH;
+        }
+
+        if (builder.length() < hashLength) {
+            int count = hashLength - builder.length();
+            String buffer = "0".repeat(count);
+            builder.append(buffer);
         }
 
         String hash = builder.reverse().toString();
