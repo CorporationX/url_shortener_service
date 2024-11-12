dccf7530054d8017d81af469026a3e3c77b98b7c
Robert Oguzov <robertoguzov2@gmail.com>
2024-11-12 18:19:50 +0300

    Тесты

--- src/test/java/faang/school/urlshortenerservice/service/UrlServiceImplTest.java
+++ src/test/java/faang/school/urlshortenerservice/service/UrlServiceImplTest.java
@@ -8,6 +8,8 @@ import faang.school.urlshortenerservice.repository.UrlRepository;
 import faang.school.urlshortenerservice.service.cache.CacheService;
 import faang.school.urlshortenerservice.service.cache.HashCacheService;
 import faang.school.urlshortenerservice.service.outbox.OutboxService;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,6 +24,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -47,6 +50,9 @@ public class UrlServiceImplTest {
 
     @Spy
     private UrlMapperImpl urlMapper;
+
+    @Mock
+    private EntityManager entityManager;
 
     @InjectMocks
     private UrlServiceImpl urlService;
@@ -157,12 +163,26 @@ public class UrlServiceImplTest {
 
     @Test
     void shortenUrl_shouldSaveUrlAndReturnUrl() {
+        when(urlRepository.existsByUrl(url)).thenReturn(false);
         when(hashCacheService.getHash()).thenReturn(hash);
 
         String shortenedUrl = urlService.shortenUrl(urlDto);
 
-        verify(urlRepository).save(entityUrl);
+        verify(entityManager).persist(entityUrl);
         verify(outboxService).saveOutbox(entityUrl);
         assertEquals(url, shortenedUrl);
+    }
+
+    @Test
+    void shortenUrl_entityExists() {
+        String correctMessage = "Url %s already exists".formatted(url);
+        when(urlRepository.existsByUrl(url)).thenReturn(true);
+
+        Exception exception = assertThrows(EntityExistsException.class,
+                () -> urlService.shortenUrl(urlDto));
+
+        verify(entityManager, never()).persist(any());
+        verify(outboxService, never()).saveOutbox(any(Url.class));
+        assertEquals(correctMessage, exception.getMessage());
     }
 }
