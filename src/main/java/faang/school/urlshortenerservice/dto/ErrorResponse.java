5a96b02ff24b85e645ec42a1d1978ee4f4020b64
beksultanramankul <ramankul.beksultan.ramankul@gmail.com>
2024-11-12 11:30:42 +0500

    second

--- src/main/java/faang/school/urlshortenerservice/dto/ErrorResponse.java
+++ src/main/java/faang/school/urlshortenerservice/dto/ErrorResponse.java
@@ -1,6 +1,5 @@
 package faang.school.urlshortenerservice.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/faang/school/urlshortenerservice/exeption/DataValidationException.java
+++ src/main/java/faang/school/urlshortenerservice/exeption/DataValidationException.java
@@ -7,6 +7,7 @@ import java.util.Map;
 @Getter
 public class DataValidationException extends RuntimeException {
     private final Map<String, String> errorDetails;
+
     public DataValidationException(String message, Map<String, String> errorDetails) {
         super(message);
         this.errorDetails = errorDetails;
