f6fa2e05804b299cecc7f6fa4453d4490f80d70a
Sergey Sklyar <sklyar1091@gmail.com>
2024-11-12 14:25:21 +0300

    - added Base62Encoder class - added encode method to encode numbers for Hashes - added simple Hash entity to use in test

---
+++ src/main/java/faang/school/urlshortenerservice/encoder/Base62Encoder.java
@@ -0,0 +1,29 @@
+package faang.school.urlshortenerservice.encoder;
+
+import faang.school.urlshortenerservice.model.hash.Hash;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class Base62Encoder {
+
+    private static final String BASE_62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public List<Hash> encode(List<Long> numbers) {
+        return numbers.stream()
+                .map(this::encodeNumber)
+                .map(Hash::new)
+                .collect(Collectors.toList());
+    }
+
+    private String encodeNumber(long number) {
+        StringBuilder sb = new StringBuilder();
+        while (number > 0) {
+            sb.append(BASE_62.charAt((int) (number % BASE_62.length())));
+            number /= BASE_62.length();
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/faang/school/urlshortenerservice/model/hash/Hash.java
@@ -0,0 +1,13 @@
+package faang.school.urlshortenerservice.model.hash;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Hash {
+
+    private String hash;
+}
