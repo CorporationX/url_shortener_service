c6194516dc0cd647c3693b46d9e843c271cb29c9
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-11-12 23:48:25 +0500

    Added HashRepository

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,28 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.model.entity.Hash;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HashRepository extends JpaRepository<Hash, String> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT nextval('unique_hash_number_seq') FROM generate_series(1, :n)
+            """)
+    List<Long> getUniqueNumbers(int n);
+
+    @Modifying
+    @Query(nativeQuery = true, value = """
+            DELETE FROM hash
+            WHERE hash IN (SELECT hash FROM hash LIMIT :batchSize)
+            RETURNING *
+            """)
+    List<String> getHashBatch(int batchSize);
+
+}
