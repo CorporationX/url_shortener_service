0221789e5c92ba8f5bd17ed4135a901797161933
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-11-13 16:18:54 +0500

    Added HashGenerator

--- src/main/java/faang/school/urlshortenerservice/generator/Base62Encoder.java
+++ src/main/java/faang/school/urlshortenerservice/generator/Base62Encoder.java
@@ -22,6 +22,8 @@ public class Base62Encoder {
             builder.append(BASE_62_CHARACTERS.charAt((int) (number % BASE_62_CHARACTERS.length())));
             number /= BASE_62_CHARACTERS.length();
         }
-        return new Hash(builder.toString());
+        return Hash.builder()
+                .hash(builder.toString())
+                .build();
     }
 }
