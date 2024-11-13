1d7deaa3654e145058b8934f64529b4240b955d0
Artem Voevoda <voevodacode@gmail.com>
2024-11-13 15:27:01 +0300

    Created a database structure

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Url.java
@@ -0,0 +1,33 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "url")
+public class Url {
+
+    @Id
+    @Column(name = "hash", length = 6)
+    private String hash;
+
+    @Column(name = "url", nullable = false, length = 4096)
+    private String url;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
