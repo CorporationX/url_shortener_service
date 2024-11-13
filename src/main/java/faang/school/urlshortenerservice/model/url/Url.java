608a3e26efda89ffab08e80d89040516e299f447
Alexander Milosh <milosh.alexander.by@gmail.com>
2024-11-13 18:40:44 +0100

    BJS2-43056 Create DB structure

---
+++ src/main/java/faang/school/urlshortenerservice/model/url/Url.java
@@ -0,0 +1,34 @@
+package faang.school.urlshortenerservice.model.url;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "url")
+@Entity
+public class Url {
+    @Id
+    @Column(name = "hash", length = 6, nullable = false)
+    private String hash;
+
+    @Column(name = "url", length = 128, nullable = false)
+    private String url;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
---
+++ src/main/resources/db/changelog/changeset/url_shortener_V001__initial.sql
@@ -0,0 +1,13 @@
+CREATE TABLE url (
+    hash       VARCHAR(6)            PRIMARY KEY,
+    url        VARCHAR(128) NOT NULL,
+    created_at TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE hash (
+    hash VARCHAR(6) PRIMARY KEY
+);
+
+CREATE SEQUENCE unique_number_seq
+    START WITH 1
+    INCREMENT BY 1;
