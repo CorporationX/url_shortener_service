ae11a644081c92a69d63fc504f7a7a830a8f80a8
SharkB38 <val.wolf.ne@gmail.com>
2024-11-14 12:49:28 +0900

    added Encoder for hash generation

---
+++ src/main/java/faang/school/urlshortenerservice/util/encoder/Base62Encoder.java
@@ -0,0 +1,41 @@
+package faang.school.urlshortenerservice.util.encoder;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+
+@Setter
+@Component
+@RequiredArgsConstructor
+public class Base62Encoder {
+    private final ExecutorService taskExecutor;
+
+    @Value("${hash.encoder.alphabet}")
+    private String alphabet;
+
+    @Value("${hash.encoder.hash-size}")
+    private int hashSize;
+
+    public List<String> encodeBatch(List<Long> sequence) {
+        List<CompletableFuture<String>> futures = sequence.stream()
+                .map(num -> CompletableFuture.supplyAsync(() -> encode(num), taskExecutor))
+                .toList();
+        return futures.stream()
+                .map(CompletableFuture::join)
+                .toList();
+    }
+
+    private String encode(Long number) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < hashSize; i++) {
+            result.append(alphabet.charAt((int) (number % alphabet.length())));
+            number /= alphabet.length();
+        }
+        return result.toString();
+    }
+}
