e5997a1edf21315fd3165e9e2fc2f7bd1fb644bf
Peregruzochka <peregruzochka48@mail.ru>
2024-11-14 13:35:28 +0300

    Реализовал возврат длинной ссылки

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -4,23 +4,33 @@ import faang.school.urlshortenerservice.dto.UrlDto;
 import faang.school.urlshortenerservice.service.UrlService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/url")
+@RequestMapping
 public class UrlController {
     private final UrlService urlService;
 
-    @PostMapping
+    @PostMapping("/url")
     @ResponseStatus(HttpStatus.CREATED)
     public UrlDto shorten(@RequestBody UrlDto url) {
         String longUrl = url.getUrl();
         String shortUrl = urlService.shorten(longUrl);
         return new UrlDto(shortUrl);
+    }
+
+    @GetMapping("/{hash}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public RedirectView getUrls(@PathVariable String hash) {
+        String longUrl = urlService.getUrl(hash);
+        return new RedirectView(longUrl);
     }
 }
--- src/main/java/faang/school/urlshortenerservice/service/UrlService.java
+++ src/main/java/faang/school/urlshortenerservice/service/UrlService.java
@@ -3,18 +3,16 @@ package faang.school.urlshortenerservice.service;
 import faang.school.urlshortenerservice.exception.IncorrectUrl;
 import faang.school.urlshortenerservice.hash.HashCache;
 import faang.school.urlshortenerservice.model.UrlEntity;
-import faang.school.urlshortenerservice.properties.RedisProperties;
 import faang.school.urlshortenerservice.properties.UrlShortenerProperties;
+import faang.school.urlshortenerservice.repository.URLCacheRepository;
 import faang.school.urlshortenerservice.repository.UrlRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import static java.util.concurrent.TimeUnit.DAYS;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -22,9 +20,7 @@ public class UrlService {
     private final HashCache hashCache;
     private final UrlRepository urlRepository;
     private final UrlShortenerProperties urlShortenerProperties;
-    private final RedisProperties redisProperties;
-    private final RedisTemplate<String, String> redisTemplate;
-
+    private final URLCacheRepository urlCacheRepository;
 
     @Transactional
     public String shorten(String longUrl) {
@@ -37,9 +33,19 @@ public class UrlService {
         entity.setHashValue(hash);
         urlRepository.save(entity);
 
-        redisTemplate.opsForValue().set(hash, longUrl, redisProperties.getTtlDays(), DAYS);
+        urlCacheRepository.put(hash, longUrl);
 
         return createShortUrl(hash);
+    }
+
+    @Transactional(readOnly = true)
+    public String getUrl(String hash) {
+       String longUrl = urlCacheRepository.get(hash);
+       if (Objects.nonNull(longUrl)) {
+           return longUrl;
+       }
+       UrlEntity currentUrl = urlRepository.findById(hash).orElseThrow();
+       return currentUrl.getUrlValue();
     }
 
     private void validateUrl(String longUrl) {
