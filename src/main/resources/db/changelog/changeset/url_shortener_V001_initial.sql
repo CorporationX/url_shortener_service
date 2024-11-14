87a108f6391e7a466d339e013ebe0b6d1df20674
SharkB38 <val.wolf.ne@gmail.com>
2024-11-14 13:35:32 +0900

    fix filenames and timestamp type

--- src/main/resources/db/changelog/changeset/url_shortener_V001_initial.sql
+++ src/main/resources/db/changelog/changeset/url_shortener_V001_initial.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS url(
     hash char(6) PRIMARY KEY,
     url varchar(2048) NOT NULL,
-    created_at timestampz DEFAULT current_timestamp
+    created_at timestamptz DEFAULT current_timestamp
 );
 
 CREATE TABLE IF NOT EXISTS hash(
