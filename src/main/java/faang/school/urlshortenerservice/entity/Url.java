6b3c572ac3d2d2115574a861937a1ebe472dfd0f
beksultanramankul <ramankul.beksultan.ramankul@gmail.com>
2024-11-14 13:47:59 +0500

    обработал кросс ревю

--- src/main/java/faang/school/urlshortenerservice/entity/Url.java
+++ src/main/java/faang/school/urlshortenerservice/entity/Url.java
@@ -3,7 +3,6 @@ package faang.school.urlshortenerservice.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +12,10 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 
 @Data
-@Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "url")
+@Entity(name = "url")
 public class Url {
     @Id
     @Column(name = "hash", length = 6)
