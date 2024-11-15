7f60e933495813c20a3c73b695f85d960d03591f
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-11-16 00:47:31 +0300

    Джоб для освобождения неиспользуемых хэшей

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -2,8 +2,10 @@ package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.entity.Hash;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,8 @@ public interface HashRepository extends JpaRepository<Hash, String> {
             value = "SELECT nextval('unique_number_seq') AS num FROM generate_series(1,:n)")
     List<Long> getUniqueNumbers(long n);
 
+    @Modifying
+    @Transactional
     @Query(nativeQuery = true, value = """
             DELETE FROM hash
             WHERE hash IN (SELECT hash from hash ORDER BY random() LIMIT :n)
--- src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
@@ -2,8 +2,22 @@ package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, String> {
+
+    @Query(nativeQuery = true, value = "SELECT hash FROM url WHERE created_at < NOW() - INTERVAL '1 year'")
+    List<String> findHashesToDelete();
+
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = "DELETE FROM url WHERE created_at < NOW() - INTERVAL '1 year'")
+    void deleteExpiredUrls();
+
 }
