0e5148c7861298e1ccd455ae2284ea285457527f
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-11-16 17:20:30 +0300

    feature-BJS2-42653: Джоб для освобождения неиспользуемых хэшей

---
+++ src/main/java/faang/school/urlshortenerservice/repository/url/UrlRepository.java
@@ -0,0 +1,26 @@
+package faang.school.urlshortenerservice.repository.url;
+
+import faang.school.urlshortenerservice.entity.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, String> {
+
+    Optional<Url> findUrlByHash(String hash);
+
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value = """
+            DELETE FROM urls
+            WHERE created_at < NOW() - INTERVAL '1 year'
+            RETURNING hash
+            """)
+    List<String> getHashesAndDeleteOldUrls();
+}
