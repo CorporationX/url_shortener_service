a18046f9b303104d3c863a6a1e17cfe0266cced1
Arstan Subanov <arstansubanov@gmail.com>
2024-11-16 14:22:24 +0600

    feature: create UrlExceptionHandler

---
+++ src/main/java/faang/school/urlshortenerservice/model/Url.java
@@ -0,0 +1,43 @@
+package faang.school.urlshortenerservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "url")
+public class Url {
+
+    @Id
+    @Column(name = "hash", length = 6)
+    private String hash;
+
+    @Column(name = "url", length = 2048)
+    private String url;
+
+    @Column(name = "cache_date")
+    @Temporal(TemporalType.DATE)
+    private LocalDate cacheDate;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+}
