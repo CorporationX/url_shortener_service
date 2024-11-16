d9e63e598c22d1ee6d91602d75034763187bb148
Sergey Sklyar <sklyar1091@gmail.com>
2024-11-16 14:04:47 +0300

    - unused imports

--- src/test/java/faang/school/urlshortenerservice/cache/hash/HashCacheIntegrationTest.java
+++ src/test/java/faang/school/urlshortenerservice/cache/hash/HashCacheIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.test.context.ActiveProfiles;
