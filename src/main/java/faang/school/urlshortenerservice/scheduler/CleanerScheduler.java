8ad85ad932f93736ad983ca75a19f073daf61ab8
ifef228 <ifef228@gmail.com>
2024-11-17 12:06:12 +0300

    transactional

--- src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
+++ src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
@@ -5,6 +5,7 @@ import faang.school.urlshortenerservice.repository.UrlRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class CleanerScheduler {
     private final UrlRepository urlRepository;
     private final HashRepository hashRepository;
     @Scheduled(cron = "${schedulers.crons.cron-for-clean-urls}")
+    @Transactional
     public void clean() {
         List<String> hashes = urlRepository.getAllForClean();
         if (hashes != null && !hashes.isEmpty()) {
