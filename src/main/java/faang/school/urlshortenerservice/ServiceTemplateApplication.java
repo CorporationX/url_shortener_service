492ea0d8a5dea46e8a4ddbf8626c7179b2b7e8e5
Denys Dovgan <131907373+DenysDovgan@users.noreply.github.com>
2024-11-17 17:45:07 +0100

    Revert "URL shortener feature"
    This reverts commit 44da0f974517290b3f022f6dbb2749ca8a0593f2.

--- src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
+++ src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableFeignClients("faang.school.urlshortenerservice.client")
+@EnableFeignClients("school.faang.servicetemplate.client")
 public class ServiceTemplateApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ServiceTemplateApplication.class)
