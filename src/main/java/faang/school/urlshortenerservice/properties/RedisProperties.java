ec0d2cc08ac9494c8c801525673f27e6cc1195f3
PLSGOTOASS <koteykaarisha@mail.ru>
2024-11-17 20:26:47 +0300

    sdelal

--- src/main/java/faang/school/urlshortenerservice/properties/RedisProperties.java
+++ src/main/java/faang/school/urlshortenerservice/properties/RedisProperties.java
@@ -2,7 +2,6 @@ package faang.school.urlshortenerservice.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
 
