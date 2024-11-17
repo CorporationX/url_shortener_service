10d4667d00a184ac971eb2c83d55acc644635cf0
ifef228 <ifef228@gmail.com>
2024-11-17 11:37:35 +0300

    refactor

--- src/main/java/faang/school/urlshortenerservice/exception/OriginalUrlNotFoundException.java
+++ src/main/java/faang/school/urlshortenerservice/exception/OriginalUrlNotFoundException.java
@@ -1,9 +1,6 @@
 package faang.school.urlshortenerservice.exception;
 
 public class OriginalUrlNotFoundException extends RuntimeException {
-    public OriginalUrlNotFoundException() {
-    }
-
     public OriginalUrlNotFoundException(String message) {
         super(message);
     }
