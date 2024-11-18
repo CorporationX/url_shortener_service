18d06b362c833e1a711fd7daa005dbe75837ffcd
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-11-18 06:21:09 +0300

    feature-BJS2-42668: Создание короткой ссылки

--- src/main/java/faang/school/urlshortenerservice/entity/Url.java
+++ src/main/java/faang/school/urlshortenerservice/entity/Url.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.time.LocalDateTime;
 
@@ -18,6 +19,7 @@ import java.time.LocalDateTime;
 @Entity
 @Builder
 @ToString
+@RedisHash("Url")
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "urls")
