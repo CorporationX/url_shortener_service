6c46869e0d8c437ae0d212934cd4fc2cf97738d8
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-11-18 20:45:46 +0300

    feature-BJS2-42668: Создание короткой ссылки после CR

--- src/main/java/faang/school/urlshortenerservice/repository/url/UrlCacheRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/url/UrlCacheRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UrlCacheRepository extends CrudRepository<Url, String> {
+
+    @Override
+    <S extends Url> S save(S entity);
 }
