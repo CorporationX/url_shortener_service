6a0dbe4c010ec61c5257d76aae2de41add768217
Artem Voevoda <voevodacode@gmail.com>
2024-11-18 17:05:37 +0300

    fix

--- src/main/java/faang/school/urlshortenerservice/util/cache/HashCacheProperty.java
+++ src/main/java/faang/school/urlshortenerservice/util/cache/HashCacheProperty.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@ConfigurationProperties("spring.hash-cache")
+@ConfigurationProperties("spring.hash.cache")
 public class HashCacheProperty {
 
     private int maxQueueSize;
