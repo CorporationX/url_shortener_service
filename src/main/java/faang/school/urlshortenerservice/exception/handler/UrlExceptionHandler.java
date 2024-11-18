1f4211b726ba98455529c07d3fd1fde08dc6a5c7
ifef228 <ifef228@gmail.com>
2024-11-18 07:52:51 +0300

    exception handler

--- src/main/java/faang/school/urlshortenerservice/exception/handler/UrlExceptionHandler.java
+++ src/main/java/faang/school/urlshortenerservice/exception/handler/UrlExceptionHandler.java
@@ -1,29 +1,32 @@
 package faang.school.urlshortenerservice.exception.handler;
 
+import faang.school.urlshortenerservice.dto.ExceptionDto;
 import faang.school.urlshortenerservice.exception.OriginalUrlNotFoundException;
 import faang.school.urlshortenerservice.exception.UrlNotRecognizedException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ControllerAdvice
 public class UrlExceptionHandler {
     @Value("${server.exceptions.default-message}")
     private String defaultMessage;
-    @ExceptionHandler(value = UrlNotRecognizedException.class)
-    protected ResponseEntity<String> urlNotRecognisedExceptionHandler(UrlNotRecognizedException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-    }
 
-    @ExceptionHandler(value = OriginalUrlNotFoundException.class)
-    protected ResponseEntity<String> originalUrlNotFoundExceptionHandler(OriginalUrlNotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(value = {UrlNotRecognizedException.class, OriginalUrlNotFoundException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ExceptionDto urlNotRecognisedExceptionHandler(UrlNotRecognizedException e) {
+        log.error(e.getMessage(), e);
+        return new ExceptionDto(e.getMessage(), 1);
     }
 
     @ExceptionHandler(value = Exception.class)
-    protected ResponseEntity<String> exceptionHandler() {
-        return new ResponseEntity<>(defaultMessage, HttpStatus.valueOf(500));
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected ExceptionDto exceptionHandler(Exception e) {
+        log.error(e.getMessage(), e);
+        return new ExceptionDto(defaultMessage, 2);
     }
 }
