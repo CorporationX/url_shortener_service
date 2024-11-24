3674be73e3dd20e96e6e64719ce31bbebdd29476
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-11-24 12:26:26 +0100

    BJS2-43071 - Генерация списка хэшей

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -3,15 +3,16 @@ package faang.school.urlshortenerservice.repository;
 import faang.school.urlshortenerservice.entity.Hash;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface HashRepository extends JpaRepository<Hash, Long> {
+public interface HashRepository extends JpaRepository<Hash, String> {
 
     @Query(nativeQuery = true, value = """
-            SELECT nextval('unique_number_seq') FROM generate_series(1, 10000)
+            SELECT nextval('unique_number_seq') FROM generate_series(1, :count)
             """)
-    List<Long> getNextRange();
+    List<Long> getNextRange(@Param("offset") int count);
 }
