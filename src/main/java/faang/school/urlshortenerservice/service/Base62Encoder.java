cf069f0527078dcf4677c0af8786904ffc4ab983
VovaBuzivskyi <vov4ikvseti@gmail.com>
2025-01-12 20:09:59 +0700

    added: Base62Encoder

---
+++ src/main/java/faang/school/urlshortenerservice/service/Base62Encoder.java
@@ -0,0 +1,34 @@
+package faang.school.urlshortenerservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class Base62Encoder {
+
+    private static final String BASE62_ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final int BASE = BASE62_ALPHABET.length();
+
+    public List<String> encode(List<Long> numbers) {
+        log.info("Start encoding : {} numbers", numbers.size());
+        //if will be a lot of numbers maybe should do parallel stream but should check is it necessary
+        List<String> result = new ArrayList<>();
+        numbers.forEach(number -> {
+            StringBuilder encoded = new StringBuilder();
+            while (number > 0) {
+                int remainder = (int) (number % BASE);
+                encoded.append(BASE62_ALPHABET.charAt(remainder));
+                number /= BASE;
+            }
+            result.add(encoded.reverse().toString());
+        });
+        log.info("End encoding : {} hashes", result.size());
+        return result;
+    }
+}
