8544c9a5e4f41499ef2b27c4a3de7331580f11c5
Aleksey <aleksei.zuk86@gmail.com>
2025-01-13 21:33:26 +0300

    init2

--- src/main/java/faang/school/urlshortenerservice/hash/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/hash/HashGenerator.java
@@ -1,13 +1,29 @@
 package faang.school.urlshortenerservice.hash;
 
+import faang.school.urlshortenerservice.config.asyncExecutor.AsyncExecutor;
+import faang.school.urlshortenerservice.config.encoder.Base62Encoder;
+import faang.school.urlshortenerservice.entity.Hash;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@RequiredArgsConstructor
 public class HashGenerator {
+    @Value("${batchSize}")
+    private long batchSize;
 
-    @Value("${alphabet}")
-    private String alphabet;
+    private final HashRepository hashRepository;
+    private final Base62Encoder base62Encoder;
+    private final AsyncExecutor asyncExecutor;
 
+    public void generateBatch() {
+        List<Long> forHashGenerate = hashRepository.getUniqueNumbers(batchSize);
+        List<Hash> hashes = base62Encoder.encode(forHashGenerate);
+        hashRepository.saveAll(hashes);
+    }
 
 }
