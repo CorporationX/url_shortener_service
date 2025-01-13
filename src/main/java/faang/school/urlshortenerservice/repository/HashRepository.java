962ad2642e18d19b216273e83dd1de5a2ed63550
Aleksey <aleksei.zuk86@gmail.com>
2025-01-13 20:38:58 +0300

    init2

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -2,6 +2,7 @@ package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.entity.Hash;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,8 @@ import java.util.List;
 @Repository
 public interface HashRepository extends JpaRepository<Hash, String> {
 
-    public List<> getUniqueNumbers(n);
+    @Query(nativeQuery = true, value = """
+    select 
+""")
+    public List<Long> getUniqueNumbers(long n);
 }
