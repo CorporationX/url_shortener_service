494e918fbcb512f46bf57233b34c5f66be235d14
Aleksey <aleksei.zuk86@gmail.com>
2025-01-13 21:48:58 +0300

    init2

---
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlCacheRepository.java
@@ -0,0 +1,17 @@
+package faang.school.urlshortenerservice.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class UrlCacheRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public String searchUrl(){
+
+    }
+}
---
+++ src/main/java/faang/school/urlshortenerservice/service/UrlService.java
@@ -0,0 +1,13 @@
+package faang.school.urlshortenerservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UrlService {
+
+    private final
+
+    private final
+}
