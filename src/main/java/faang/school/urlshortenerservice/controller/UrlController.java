0bb8674e6e1c88d155348c77eb763ac3ada381c0
Aleksi Basilashvili <lucheidobra@mail.ru>
2025-01-13 17:28:05 +0300

    Project Runs!

---
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -0,0 +1,31 @@
+package faang.school.urlshortenerservice.controller;
+
+import faang.school.urlshortenerservice.dto.UrlDto;
+import faang.school.urlshortenerservice.service.UrlService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/url")
+@RestController
+@RequiredArgsConstructor
+@Validated
+public class UrlController {
+
+    private final UrlService urlService;
+
+    @GetMapping
+    public String getRealUrl(@Valid @RequestBody UrlDto urlDto) {
+        return "Hello from URL Shortener Service! Please check the documentation for more information.";
+    }
+
+    @PostMapping
+    public String getShortUrl(@Valid @RequestBody UrlDto urlDto) {
+        return "";
+    }
+}
