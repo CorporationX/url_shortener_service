64261da4d3a6c53513c716518a8b629af6070930
AlekseyB86 <avbelov86@mail.ru>
2025-01-14 22:48:35 +0300

    правки в коде

--- src/main/java/faang/school/urlshortenerservice/service/HashCache.java
+++ src/main/java/faang/school/urlshortenerservice/service/HashCache.java
@@ -3,41 +3,50 @@ package faang.school.urlshortenerservice.service;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
 @RequiredArgsConstructor
-public class LocalCache {
-    private final HashGenerator hashGenerator;
-
+public class HashCache {
     @Value("${hash.cache.capacity:1000}")
     private int capacity;
-
-    private final Queue<String> hashes = new ArrayBlockingQueue<>(capacity);
 
     @Value("${hash.cache.fill-percent:20}")
     private int fillPercent;
 
-    private AtomicBoolean filling;
+    private final AtomicBoolean filling = new AtomicBoolean(false);
+
+    private Queue<String> hashes;
+
+    private final HashGenerator hashGenerator;
 
     @PostConstruct
-    public void init(){
+    public void init() {
+        hashes = new ArrayBlockingQueue<>(capacity);
         hashes.addAll(hashGenerator.getHashes(capacity));
     }
 
     public String getHash() {
         if (getCurPercent() < fillPercent) {
-            if(filling.compareAndSet(false, true)) {
-                hashGenerator.getHashesAsync(capacity)
+            if (filling.compareAndSet(false, true)) {
+                getHashesAsync(capacity)
                         .thenAccept(hashes::addAll)
                         .thenRun(() -> filling.set(false));
             }
         }
         return hashes.poll();
+    }
+
+    @Async("hashGeneratorExecutor")
+    public CompletableFuture<List<String>> getHashesAsync(long amount) {
+        return CompletableFuture.completedFuture(hashGenerator.getHashes(amount));
     }
 
     private int getCurPercent() {
--- src/main/java/faang/school/urlshortenerservice/service/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/service/HashGenerator.java
@@ -3,6 +3,7 @@ package faang.school.urlshortenerservice.service;
 import faang.school.urlshortenerservice.repository.HashRepository;
 import faang.school.urlshortenerservice.util.BaseEncoder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,8 +11,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class HashGenerator {
@@ -21,27 +22,26 @@ public class HashGenerator {
     @Value("${hash.generator.batch-size:1000}")
     private long batchSize;
 
-    @Transactional
+    @Async("hashGeneratorExecutor")
     @Scheduled(cron = "${hash.generator.cron:0*****}")
     public void generateBatch(){
+        log.info("Generating hashes");
         List<Long> uniqueNumbers = hashRepository.getUniqueNumbers(batchSize);
         List<String> hashes = baseEncoder.encode(uniqueNumbers);
         hashRepository.saveAll(hashes);
+        log.info("Saved {} hashes", hashes.size());
     }
 
     @Transactional
     public List<String> getHashes(long amount) {
+        log.info("Getting {} hashes", amount);
         List<String> hashes = hashRepository.getHashBatchAndDelete(amount);
         if (hashes.size() < amount) {
+            log.info("Not enough hashes, generating more");
             generateBatch();
             hashes.addAll(hashRepository.getHashBatchAndDelete(amount- hashes.size()));
+            log.info("Generated {} hashes", hashes.size());
         }
         return hashes;
-    }
-
-    @Async("hashGeneratorExecutor")
-    public CompletableFuture<List<String>> getHashesAsync(long amount) {
-        return CompletableFuture.completedFuture(getHashes(amount));
-
     }
 }
