57834bb9f00f11ee14382ec0157217c9d48acf49
Alexander Litovkin <litovkinalexan@gmail.com>
2025-01-14 18:11:54 +0700

    implement global exception handler

---
+++ src/main/java/faang/school/urlshortenerservice/dto/ExceptionDto.java
@@ -0,0 +1,14 @@
+package faang.school.urlshortenerservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionDto {
+
+    private String message;
+}
---
+++ src/main/java/faang/school/urlshortenerservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package faang.school.urlshortenerservice.exception;
+
+import faang.school.urlshortenerservice.dto.ExceptionDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDto handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.info("Handle exception", ex);
+        String exceptionMessage = ex.getBindingResult().getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining("; "));
+
+        return new ExceptionDto(exceptionMessage);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionDto handleException(Exception ex) {
+        log.error("Handle exception", ex);
+
+        return new ExceptionDto(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+    }
+}
