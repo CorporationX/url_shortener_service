cf8bd0e520987d7f1ec9a46c9bce45f6834d4789
abzalkhan00 <abzalkhanondakanov@gmail.com>
2025-01-14 16:37:16 +0500

    solved hash generation task but without tests

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -16,11 +16,4 @@ public interface HashRepository extends JpaRepository<Hash, String> {
             """)
     List<Long> getNextRange(int maxRange);
 
-    @Modifying
-    @Query(nativeQuery = true, value = """
-                        DELETE FROM hash WHERE id IN (SELECT id FROM hash ORDER BY id ASC LIMIT :amount)
-                        RETURNING *
-            """ )
-    List<Hash> findAndDelete(long amount);
-
 }
