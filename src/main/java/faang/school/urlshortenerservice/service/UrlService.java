5d1a8d5a58eb240c095f04676c94d5ab6f044e3b
Alexander Litovkin <litovkinalexan@gmail.com>
2025-01-14 19:36:55 +0700

    add get method for controller

--- src/main/java/faang/school/urlshortenerservice/service/UrlService.java
+++ src/main/java/faang/school/urlshortenerservice/service/UrlService.java
@@ -9,4 +9,8 @@ public class UrlService {
     public UrlDto createShortUrl(UrlDto urlDto) {
         return new UrlDto("shortener/123456");
     }
+
+    public String getLongUrl(String hash) {
+        return "https://www.google.com/";
+    }
 }
