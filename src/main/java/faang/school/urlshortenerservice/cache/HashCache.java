4d44d7b6c792dcf2a7680ce9aa12e07e3e5ced7b
abzalkhan00 <abzalkhanondakanov@gmail.com>
2025-01-14 21:30:23 +0500

    solved  task related to Caching task into Queue

---
+++ src/main/java/faang/school/urlshortenerservice/cache/HashCache.java
@@ -0,0 +1,45 @@
+package faang.school.urlshortenerservice.cache;
+
+import faang.school.urlshortenerservice.generator.HashGenerator;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Component
+@RequiredArgsConstructor
+public class HashCache {
+
+    private final HashGenerator hashGenerator;
+
+    @Value("${hash.cache.capacity}")
+    private int capacity;
+
+    @Value("${hash.cache.fill-percentage}")
+    private int fillPercentage;
+
+    private AtomicBoolean filling = new AtomicBoolean(false);
+
+    private Queue<String> hashes;
+
+    @PostConstruct
+    public void init() {
+        this.hashes = new ArrayBlockingQueue<>(capacity);
+        hashes.addAll(hashGenerator.getHashes(capacity));
+    }
+
+    public String getHash() {
+        if (hashes.size() / (capacity / 100.0) < fillPercentage) {
+            if (filling.compareAndSet(false, true)) {
+                hashGenerator.getHashesAsync(capacity).thenAccept(hashes::addAll)
+                        .thenRun(() -> filling.set(false));
+            }
+        }
+        return hashes.poll();
+    }
+
+}
--- src/main/java/faang/school/urlshortenerservice/generator/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/generator/HashGenerator.java
@@ -3,7 +3,6 @@ package faang.school.urlshortenerservice.generator;
 import faang.school.urlshortenerservice.encoder.Base62Encoder;
 import faang.school.urlshortenerservice.entity.Hash;
 import faang.school.urlshortenerservice.repository.HashRepository;
-import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,14 +10,13 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 @RequiredArgsConstructor
 public class HashGenerator {
 
     private final HashRepository hashRepository;
-
-    private final EntityManager entityManager;
 
     private final Base62Encoder base62Encoder;
 
@@ -36,8 +34,22 @@ public class HashGenerator {
                 .map(Hash::new)
                 .toList();
 
-        hashRepository.saveAllAndFlush(hashes);
-        entityManager.clear();
+        hashRepository.saveAll(hashes);
+    }
+
+    @Transactional
+    public List<String> getHashes(long amount) {
+        List<Hash> hashes = hashRepository.getHashBatch(amount);
+        if (hashes.size() < amount) {
+            generateBatch();
+            hashes.addAll(hashRepository.getHashBatch(amount - hashes.size()));
+        }
+        return hashes.stream().map(Hash::getHash).toList();
+    }
+
+    @Async("hashGeneratorExecutor")
+    public CompletableFuture<List<String>> getHashesAsync(long amount) {
+        return CompletableFuture.completedFuture(getHashes(amount));
     }
 
 }
