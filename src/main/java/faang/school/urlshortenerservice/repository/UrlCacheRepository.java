e1a31b165a29570a98835599ca1050b77ccdc71a
Svyatoslav Kostrov <kost-2014@mail.ru>
2025-01-16 00:12:29 +0300

    logger for UrlCacheRepository

--- src/main/java/faang/school/urlshortenerservice/repository/UrlCacheRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlCacheRepository.java
@@ -3,6 +3,7 @@ package faang.school.urlshortenerservice.repository;
 import faang.school.urlshortenerservice.entity.Hash;
 import faang.school.urlshortenerservice.entity.Url;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -12,18 +13,22 @@ import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class UrlCacheRepository {
     private final RedisTemplate<String, URL> redisTemplate;
 
     public void saveAtRedis(Url url) {
+        log.info("add new key-value in redis with 1 day time to live");
         redisTemplate.opsForValue().set(url.getHash(), url.getUrl(), 1, TimeUnit.DAYS);
     }
 
     public URL getFromRedis(String hash) {
+        log.info("get value by key from redis");
         return redisTemplate.opsForValue().get(hash);
     }
 
     public void deleteFormRedis(String hash) {
+        log.info("remove value from redis by key");
         redisTemplate.delete(hash);
     }
 }
