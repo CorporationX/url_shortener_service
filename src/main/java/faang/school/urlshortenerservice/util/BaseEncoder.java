23a65d36b3bba6f46bfc02f1864223828726d845
Dmitrii Pazinich <pazinich.d@yandex.ru>
2025-01-15 13:03:43 +0300

    feature add BaseEncoder add tests

---
+++ src/main/java/faang/school/urlshortenerservice/util/BaseEncoder.java
@@ -0,0 +1,43 @@
+package faang.school.urlshortenerservice.util;
+
+import faang.school.urlshortenerservice.exception.DataValidationException;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class BaseEncoder {
+
+    @Value("${spring.values.base}")
+    private Long base;
+
+    @Value("${spring.values.characters}")
+    private String characters;
+
+    public List<String> encodeList(List<Long> numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            throw new DataValidationException("The list of numbers to encode must not be null or empty");
+        }
+
+        List<String> hashes = new ArrayList<>();
+        numbers.forEach(number -> hashes.add(encode(number)));
+        return hashes;
+    }
+
+    private String encode(long number) {
+        var sb = new StringBuilder();
+
+        while (number > 0) {
+            sb.append(characters.charAt((int) (number % base)));
+            number /= base;
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/faang/school/urlshortenerservice/util/BaseEncoderTest.java
@@ -0,0 +1,52 @@
+package faang.school.urlshortenerservice.util;
+
+import faang.school.urlshortenerservice.exception.DataValidationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BaseEncoderTest {
+    private Long base;
+    private String characters;
+    private List<Long> numbers;
+    private List<String> encodedValues;
+    private BaseEncoder baseEncoder;
+
+    @BeforeEach
+    void setUp() {
+        base = 62L;
+        characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        numbers = new ArrayList<>(List.of(1L, 2L, 3L));
+        encodedValues = new ArrayList<>(List.of("1", "2", "3"));
+        baseEncoder = new BaseEncoder(base, characters);
+    }
+
+    @Test
+    void testEncodeListSuccess() {
+        List<String> result = baseEncoder.encodeList(numbers);
+        assertThat(result).hasSize(numbers.size());
+
+        encodedValues.forEach(value -> assertThat(result).contains(value));
+    }
+
+    @Test
+    void testEncodeListShouldThrowException_WhenNumbersIsNull() {
+        numbers = null;
+        assertThatThrownBy(() -> baseEncoder.encodeList(numbers))
+                .isInstanceOf(DataValidationException.class)
+                .hasMessageContaining("The list of numbers to encode must not be null or empty");
+    }
+
+    @Test
+    void testEncodeListShouldThrowException_WhenNumbersIsEmpty() {
+        numbers = new ArrayList<>();
+        assertThatThrownBy(() -> baseEncoder.encodeList(numbers))
+                .isInstanceOf(DataValidationException.class)
+                .hasMessageContaining("The list of numbers to encode must not be null or empty");
+    }
+}
