951654cba93d827dcc5f67fdc2a6f40fdcbdee89
Aleksi Basilashvili <lucheidobra@mail.ru>
2025-01-16 16:09:49 +0300

    Task Seven -> HashGenerator

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -4,6 +4,7 @@ import faang.school.urlshortenerservice.dto.UrlDto;
 import faang.school.urlshortenerservice.service.UrlService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,17 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/url")
 @RequiredArgsConstructor
 @Validated
+@Slf4j
 public class UrlController {
 
     private final UrlService urlService;
 
     @PostMapping
     public String getShortUrl(@Valid @RequestBody UrlDto urlDto) {
+        log.info("Received a request to shorten URL: {}", urlDto.getUrl());
         return urlService.getShortUrl(urlDto);
     }
 
     @GetMapping("/{hash}")
     public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String hash) {
+        log.info("Received a request to redirect from url: https://localhost:8080/url/{}", hash);
         String originalUrl = urlService.redirectToRealUrl(hash);
         HttpHeaders headers = new HttpHeaders();
         headers.set(HttpHeaders.LOCATION, originalUrl);
