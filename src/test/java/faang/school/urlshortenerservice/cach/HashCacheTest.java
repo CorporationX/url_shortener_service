bad8a4de38bbe993ef0b855b0f2bffd3cabea19a
Aleksey <aleksei.zuk86@gmail.com>
2025-01-17 23:10:13 +0300

    немного тестиков.

--- src/test/java/faang/school/urlshortenerservice/cach/HashCacheTest.java
+++ src/test/java/faang/school/urlshortenerservice/cach/HashCacheTest.java
@@ -1,6 +1,5 @@
 package faang.school.urlshortenerservice.cach;
 
-import faang.school.urlshortenerservice.entity.Hash;
 import faang.school.urlshortenerservice.hash.HashGenerator;
 import faang.school.urlshortenerservice.repository.HashRepository;
 import org.junit.jupiter.api.Test;
@@ -8,14 +7,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class HashCacheTest {
