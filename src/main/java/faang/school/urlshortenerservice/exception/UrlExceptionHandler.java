ca7edbf60bbd38ee9ff8ceccec37723fc9d5c7f1
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2025-01-17 12:16:17 +0300

    url shortener Gadzhi

---
+++ src/main/java/faang/school/urlshortenerservice/exception/UrlExceptionHandler.java
@@ -0,0 +1,9 @@
+package faang.school.urlshortenerservice.exception;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UrlExceptionHandler {
+
+}
