019ddba99332313dba7f50c09187e74c96e8894e
Svyatoslav Kostrov <kost-2014@mail.ru>
2025-01-17 14:50:30 +0300

    added scheduler for remove an old url from db

--- src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
@@ -3,12 +3,21 @@ package faang.school.urlshortenerservice.repository;
 import faang.school.urlshortenerservice.entity.Url;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.net.URL;
+import java.util.List;
 import java.util.Optional;
 
 public interface UrlRepository extends JpaRepository<Url, Long> {
     Url findByHash(@Param("hash") String hash);
 
     Optional<Url> findByUrl(@Param("url") URL url);
+
+    @Query(nativeQuery = true, value = """
+             DELETE FROM url
+             WHERE created_at < NOW() - INTERVAL '1 year'
+             RETURNING * 
+            """)
+    List<Url> findAndRemoveAllOldEntity();
 }
---
+++ src/main/java/faang/school/urlshortenerservice/schedul/CleanerScheduler.java
@@ -0,0 +1,31 @@
+package faang.school.urlshortenerservice.schedul;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import faang.school.urlshortenerservice.entity.Url;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import faang.school.urlshortenerservice.repository.UrlRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class CleanerScheduler {
+    private final HashRepository hashRepository;
+    private final UrlRepository urlRepository;
+
+    @Transactional
+    @Scheduled(cron = "${remove_old_url_cron}")
+    public void removeOldUrl(){
+        List<Hash> hashes = urlRepository.findAndRemoveAllOldEntity().stream()
+                .map(Url::getHash)
+                .map(Hash::new)
+                .toList();
+
+        hashRepository.saveAll(hashes);
+    }
+}
