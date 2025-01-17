a4e97bae109109e1c19be13123bd9a0d985131dd
MunirIsmoilov <ismoilov.munir@gmail.com>
2025-01-17 16:35:44 +0300

    commit1: repository / database structure /job to release unused hashes

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -1,7 +1,7 @@
 package faang.school.urlshortenerservice.repository;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +13,7 @@ public class HashRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
-   // @Value("${batch.sizze}")
+    @Value("${hash.batch-size}")
     private int batchSize;
 
     public List<Long> getUniqueNumbers(int n) {
@@ -21,19 +21,24 @@ public class HashRepository {
         return jdbcTemplate.queryForList(query, Long.class, n);
     }
 
-    public void save(List<String> hashes){
+    public void save(List<String> hashes) {
         String query = "INSERT INTO hash (hash) VALUES (?)";
         jdbcTemplate.batchUpdate(query, hashes, batchSize, (ps, hash) -> ps.setString(1, hash));
     }
 
     public List<String> getHashBatch() {
-        String query = "WITH deleted AS ( " +
-                "DELETE FROM hash " +
-                "WHERE hash IN ( " +
-                "SELECT hash FROM hash " +
-                "ORDER BY random() LIMIT ?) " +
-                "RETURNING hash) " +
-                "SELECT hash FROM deleted";
+        String query = """ 
+                        WITH deleted AS (
+                        DELETE FROM hash
+                WHERE hash IN (
+                        SELECT hash
+                        FROM hash
+                        ORDER BY random()
+                        LIMIT ?
+                ) RETURNING hash
+                ) SELECT hash FROM deleted
+                """;
+
         return jdbcTemplate.queryForList(query, String.class, batchSize);
     }
 }
