74117f2f229b48b16b0a84e9cd0d60d10dcb6283
Daniyar Dostiev <daniyar.dst@gmail.com>
2025-01-18 16:42:15 +0700

    Исправление теста на поднятие контекста, изменение конфигурации приложения.

--- src/test/java/faang/school/urlshortenerservice/util/ApplicationContextTest.java
+++ src/test/java/faang/school/urlshortenerservice/util/ApplicationContextTest.java
@@ -1,12 +1,38 @@
 package faang.school.urlshortenerservice.util;
 
+import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 public class ApplicationContextTest {
+
+    @Container
+    public static PostgreSQLContainer<?> POSTGRESQL_CONTAINER =
+            new PostgreSQLContainer<>("postgres:13.6");
+    @Container
+    private static final RedisContainer REDIS_CONTAINER =
+            new RedisContainer(DockerImageName.parse("redis/redis-stack:latest"));
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
+
+        registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379));
+        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+    }
 
     @Test
     public void contextLoads() {
+
     }
 }
