edf593cc40055d0bafcc659824e8a68072ff2945
Алексей Беляев <belyaevayur@intech.rshb.ru>
2025-01-18 15:52:07 +0300

    BJS2-55443 Структура БД

--- src/main/resources/db/changelog/changeset/url_shortener_V001__initial.sql
+++ src/main/resources/db/changelog/changeset/url_shortener_V001__initial.sql
@@ -1,3 +1,13 @@
 CREATE SEQUENCE IF NOT EXISTS unique_hash_number_seq
     START WITH 1
     INCREMENT BY 1;
+
+CREATE TABLE IF NOT EXISTS url (
+    hash varchar(6) PRIMARY KEY,
+    url varchar(1024) NOT NULL,
+    created_at timestamptz DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS hash (
+    hash varchar(6) PRIMARY KEY
+);
