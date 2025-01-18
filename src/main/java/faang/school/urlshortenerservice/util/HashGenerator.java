2009062bb3b33582c4da76a28af599f801098525
Alexander Litovkin <litovkinalexan@gmail.com>
2025-01-18 13:09:32 +0700

    refactor hash generator

--- src/main/java/faang/school/urlshortenerservice/util/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/util/HashGenerator.java
@@ -6,9 +6,11 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 @RequiredArgsConstructor
@@ -18,11 +20,11 @@ public class HashGenerator {
     private final HashRepository hashRepository;
     private final Base62Encoder encoder;
 
-    @Value("${app.generation-batch-size:100}")
+    @Value("${app.hash.generator.batch-size:1000}")
     private int generationBatch;
 
     @Transactional
-    @Async("taskExecutor")
+    @Scheduled(cron = "${app.hash.generator.cron:0 0 0 * * *}")
     public void generateBatch() {
         List<Long> seeds = uniqueIdRepository.getNextRange(generationBatch);
         List<String> hashes = seeds.stream()
@@ -33,6 +35,18 @@ public class HashGenerator {
 
     @Transactional
     public List<String> getHashes(int amount) {
+        List<String> hashes = hashRepository.getHashes(amount);
+        if (hashes.size() < amount) {
+            generateBatch();
+            hashes.addAll(hashRepository.getHashes(amount - hashes.size()));
+        }
+
         return hashRepository.getHashes(amount);
+    }
+
+    @Transactional
+    @Async("taskExecutor")
+    public CompletableFuture<List<String>> getHashesAsync(int amount) {
+        return CompletableFuture.supplyAsync(() -> getHashes(amount));
     }
 }
