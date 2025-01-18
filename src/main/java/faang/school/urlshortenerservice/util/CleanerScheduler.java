524dedab34fd88fa5ae55bd53db28bf0bf41ea55
Alexander Litovkin <litovkinalexan@gmail.com>
2025-01-18 15:43:16 +0700

    add more threads for scheduler

--- src/main/java/faang/school/urlshortenerservice/util/CleanerScheduler.java
+++ src/main/java/faang/school/urlshortenerservice/util/CleanerScheduler.java
@@ -3,6 +3,7 @@ package faang.school.urlshortenerservice.util;
 import faang.school.urlshortenerservice.repository.HashRepository;
 import faang.school.urlshortenerservice.repository.UrlRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CleanerScheduler {
@@ -23,6 +25,7 @@ public class CleanerScheduler {
     @Transactional
     @Scheduled(cron = "${app.cleaner.cron:0 0 0 * * *}")
     public void removeOldUrls() {
+        log.info("Removing old urls");
         List<String> hashes = urlRepository.removeOldUrls(urlLifetime);
         hashRepository.saveHashes(hashes);
     }
