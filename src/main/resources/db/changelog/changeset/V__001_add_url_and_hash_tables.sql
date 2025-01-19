f5aabb54ff93690971d7af25d735f9dc171003c5
daurenberdibekov <berder08@gmail.com>
2025-01-19 15:37:49 +0100

    add liquibase and gradle deps

--- src/main/resources/db/changelog/changeset/V__001_add_url_and_hash_tables.sql
+++ src/main/resources/db/changelog/changeset/V__001_add_url_and_hash_tables.sql
@@ -1,10 +1,13 @@
-CREATE TABLE url (
-    hash VARCHAR(6) PRIMARY KEY,
-    url VARCHAR(255) UNIQUE ,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    expires_at TIMESTAMP
+CREATE TABLE url(
+    hash       VARCHAR(6) PRIMARY KEY,
+    url        TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE hash (
+CREATE TABLE hash(
     hash VARCHAR(6) PRIMARY KEY
-)
+);
+
+CREATE SEQUENCE unique_number_seq
+    INCREMENT BY 1
+    START WITH 1
