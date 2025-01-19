4dd2cafc097d69b14cf5f9df5a1204a2ba2f26b8
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2025-01-19 06:59:30 +0300

    Shortener Gadzhi

--- src/main/java/faang/school/urlshortenerservice/exception/UrlExceptionHandler.java
+++ src/main/java/faang/school/urlshortenerservice/exception/UrlExceptionHandler.java
@@ -1,9 +1,35 @@
 package faang.school.urlshortenerservice.exception;
 
 
+import faang.school.urlshortenerservice.dto.ErrorDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class UrlExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto handleIllegalArgumentException(IllegalArgumentException e) {
+        log.error("IllegalArgumentException", e);
+        return new ErrorDto(e.getMessage());
+    }
 
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto handleIllegalStateException(IllegalStateException e) {
+        log.error("IllegalStateException", e);
+        return new ErrorDto(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto handleRuntimeException(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return new ErrorDto(e.getMessage());
+    }
 }
+
