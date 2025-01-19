43005b8ab1694f128cb84fbc731c54a6a76b13e3
daurenberdibekov <berder08@gmail.com>
2025-01-19 15:39:34 +0100

    add encoder

--- src/main/java/faang/school/urlshortenerservice/encoder/Base62Encoder.java
+++ src/main/java/faang/school/urlshortenerservice/encoder/Base62Encoder.java
@@ -1,15 +1,25 @@
 package faang.school.urlshortenerservice.encoder;
 
-import faang.school.urlshortenerservice.entity.Hash;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class Base62Encoder {
+    private static final String BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final int BASE_LENGTH = BASE62.length();
 
-    public List<Hash> encode(List<Long> numbers) {
-        return null;
-
+    public List<String> encode(List<Long> numbers) {
+        List<String> hashList = new ArrayList<>(numbers.size());
+        for (Long number : numbers) {
+            StringBuilder builder = new StringBuilder();
+            do {
+                builder.append(BASE62.charAt((int) (number % BASE_LENGTH)));
+                number /= BASE_LENGTH;
+            } while (number > 0);
+            hashList.add(builder.reverse().toString());
+        }
+        return hashList;
     }
 }
