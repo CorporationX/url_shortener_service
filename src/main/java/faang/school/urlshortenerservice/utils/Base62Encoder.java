0f2659079ed7d52d77b454f0415c9c48e80aa69f
Алексей Беляев <belyaevayur@intech.rshb.ru>
2025-01-19 21:35:12 +0300

    BJS2-55446 Генерация хэша

--- src/main/java/faang/school/urlshortenerservice/utils/Base62Encoder.java
+++ src/main/java/faang/school/urlshortenerservice/utils/Base62Encoder.java
@@ -1,10 +1,8 @@
 package faang.school.urlshortenerservice.utils;
 
 import faang.school.urlshortenerservice.entity.Hash;
-import faang.school.urlshortenerservice.repository.HashRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,15 +10,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class Base62Encoder {
     private static final String BASE_62_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    private final HashRepository hashRepository;
 
-    @Transactional
-    public void encode(List<Long> numbers) {
-        List<Hash> hashes = numbers.stream()
+    public List<Hash> encode(List<Long> numbers) {
+         return numbers.stream()
                 .map(this::base62Encode)
                 .map(Hash::new)
                 .toList();
-        hashRepository.saveAll(hashes);
     }
 
     private String base62Encode(long number) {
