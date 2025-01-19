a9105130fd28d6245bb64c0ce9e812bfc92f33b3
daurenberdibekov <berder08@gmail.com>
2025-01-19 15:37:18 +0100

    add url service and repository

--- src/main/java/faang/school/urlshortenerservice/service/UrlService.java
+++ src/main/java/faang/school/urlshortenerservice/service/UrlService.java
@@ -2,41 +2,59 @@ package faang.school.urlshortenerservice.service;
 
 import faang.school.urlshortenerservice.dto.UrlDto;
 import faang.school.urlshortenerservice.entity.Url;
+import faang.school.urlshortenerservice.exception.UrlNotFoundException;
+import faang.school.urlshortenerservice.generator.HashCache;
 import faang.school.urlshortenerservice.mapper.UrlMapper;
+import faang.school.urlshortenerservice.repository.UrlCacheRepository;
 import faang.school.urlshortenerservice.repository.url.UrlRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UrlService {
+
+    private final UrlCacheRepository urlCacheRepository;
     private final UrlRepository urlRepository;
+    private final HashCache hashCache;
     private final UrlMapper urlMapper;
 
-    @CachePut(value = "urls", key = "#url.hash")
-    public Url createUrl(Url url) {
-        return urlRepository.save(url);
+    @Value("${spring.datasource.url}")
+    private String baseUrl;
+
+    public String getShortUrl(UrlDto urlDto) {
+        String url = urlDto.getOriginalUrl();
+        String hash = hashCache.getHashFromCache();
+
+        UrlDto newShortUrl = new UrlDto(url);
+
+        urlCacheRepository.save(hash, url);
+        urlRepository.save(urlMapper.toEntity(newShortUrl));
+
+        String shortUrl = baseUrl + "/url/" + hash;
+        log.info("Generated short URL: {}", shortUrl);
+        return shortUrl;
     }
 
-    @Cacheable(value = "urls", key = "#hash")
-    public Url getUrl(String hash) {
-
-        return urlRepository.findById(hash)
-                .orElseThrow(() -> new EntityNotFoundException("Url not found for hash: " + hash));
-    }
-
-    public Url buildUrl(UrlDto urlDto) {
-        Url url = urlMapper.toEntity(urlDto);
-        url.setHash("fvfd");
-        url.setCreatedAt(LocalDateTime.now());
-
+    public String redirectToRealUrl(String hash) {
+        String url = getUrlFromCacheOrDb(hash);
+        log.info("Redirecting to: {}", url);
         return url;
+    }
+
+    private String getUrlFromCacheOrDb(String hash) {
+        String url = urlCacheRepository.findByHashInRedis(hash);
+        if (url != null) {
+            log.info("Found in cache: {}", url);
+            return url;
+        }
+
+        log.info("Not found in cache, fetching from DB: {}", hash);
+        return urlRepository.findByHash(hash)
+                .map(Url::getUrl)
+                .orElseThrow(() -> new UrlNotFoundException("Url not found for hash " + hash));
     }
 }
