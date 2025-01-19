47db1bd09580cdbddc27682deaef7daaf2a8d074
Vakarash <max.vackarash@gmail.com>
2025-01-19 12:00:51 +0300

    initial commit for hash repo

---
+++ src/main/java/faang/school/urlshortenerservice/config/HashRepositoryProperties.java
@@ -0,0 +1,13 @@
+package faang.school.urlshortenerservice.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "hash-repository")
+public class HashRepositoryProperties {
+
+    private int randomBatchSize;
+}
