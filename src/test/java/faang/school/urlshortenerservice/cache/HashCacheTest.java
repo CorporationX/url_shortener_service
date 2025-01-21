bbdbaddf6f4e5de98f505b96a0c8d93c1d85a6f4
Egor Semenovskii <gaoqiao5217@gmail.com>
2025-01-21 17:11:42 +0300

    -

--- src/test/java/faang/school/urlshortenerservice/cache/HashCacheTest.java
+++ src/test/java/faang/school/urlshortenerservice/cache/HashCacheTest.java
@@ -1,7 +1,31 @@
 package faang.school.urlshortenerservice.cache;
 
-import static org.junit.jupiter.api.Assertions.*;
+import faang.school.urlshortenerservice.generator.HashGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@ExtendWith(MockitoExtension.class)
 class HashCacheTest {
+
+    @InjectMocks
+    private HashCache hashCache;
+
+    @Mock
+    private HashGenerator hashGenerator;
+
+    @Value("${hash.cache.capacity:1000}")
+    private int capacity;
+    @Value("${hash.cache.fill.percent:20}")
+    private volatile int fillPercent;
+
+
 
 }
