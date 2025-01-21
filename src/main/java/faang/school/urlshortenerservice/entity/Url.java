909460ded779d76f84525973050dc9e2e5259c46
SerUKalinin <serukalinin@gmail.com>
2025-01-21 18:30:11 +0300

    Url

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Url.java
@@ -0,0 +1,40 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "url")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Url {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hash", nullable = false, length = 6)
+    private String hash;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
