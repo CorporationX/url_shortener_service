8f98daf8edf37e781d87b429dfb893bc70f980d2
Amanyaz Bayramdurdyyev <amanyaz9090@gmail.com>
2025-01-21 16:59:05 -0600

    Скрипты для создания всех таблиц в базе данных, упомянутой в задаче BJS2-55416, написаны. Этот файл со скриптами добавлен в changelog Liquibase.

---
+++ src/main/resources/db/changelog/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE url (
+ hash varchar(6) PRIMARY KEY NOT NULL,
+ url varchar(1024) NOT NULL,
+ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE hash(
+    hash varchar(6) PRIMARY KEY NOT NULL
+);
+
+CREATE SEQUENCE unique_number-seq(
+    START WITH 1
+    INCREMENT BY 1;
+);
