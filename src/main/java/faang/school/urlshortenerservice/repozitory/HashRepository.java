7d6482474775b5d696453c938bad36966997ce83
lomuserg <sergeymu2015@yandex.ru>
2025-01-25 07:29:08 +0300

    BJS2-55309: Create HashRepository: saveAll, getHashBatch/ config batchSize

--- src/main/java/faang/school/urlshortenerservice/repozitory/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repozitory/HashRepository.java
@@ -3,6 +3,7 @@ package faang.school.urlshortenerservice.repozitory;
 import faang.school.urlshortenerservice.entity.Hash;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +14,16 @@ public interface HashRepository extends CrudRepository<Hash, Long> {
     @Query(nativeQuery = true, value = """
             SELECT nextval('unique_number_seq') FROM generate_series(1, :batchSize)
             """)
-    List<Long> getUniqueNumbers(long batchSize);
+    List<Long> getUniqueNumbers(@Param("batchSize") long batchSize);
+
+
+    List<Hash> saveAll(List<Hash> hashes);
+
+    @Query(nativeQuery = true, value = """
+            DELETE FROM hash WHERE id IN(
+                SELECT id FROM hash ORDER BY RANDOM() LIMIT :batchSize
+            )
+            RETURNING *;
+            """)
+    List<Hash> getHashBatch(@Param("batchSize") long batchSize);
 }
