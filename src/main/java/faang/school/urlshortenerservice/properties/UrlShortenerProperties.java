de8c51e015b7d245e3a5903337a0e9eadfd72423
Shakhzod Kamolov <shakhzod.kamolov99@gmail.com>
2025-03-13 23:11:27 +0500

    BJS2-67137 Add HashRepository with required methods - add hash-batch-size into application.yaml - add optimised ci/cd

---
+++ src/main/java/faang/school/urlshortenerservice/properties/UrlShortenerProperties.java
@@ -0,0 +1,14 @@
+package faang.school.urlshortenerservice.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "url-shortener-service")
+public class UrlShortenerProperties {
+  private int hashBatchSize;
+}
---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,34 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.properties.UrlShortenerProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class HashRepository {
+
+  private final JdbcTemplate jdbcTemplate;
+  private final UrlShortenerProperties properties;
+
+  public List<Long> getUniqueNumbers(int n) {
+    String sql = "SELECT nextval('unique_number_seq') FROM generate_series(1, ?)";
+    return jdbcTemplate.queryForList(sql, Long.class, n);
+  }
+
+  public void save(List<String> hashes) {
+    String sql = "INSERT INTO hash (hash) VALUES (?)";
+    jdbcTemplate.batchUpdate(sql, hashes, hashes.size(),
+        (ps, hash) -> ps.setString(1, hash));
+  }
+
+  public List<String> getHashBatch() {
+    String sql = "DELETE FROM hash WHERE hash IN (SELECT hash FROM hash ORDER BY random() LIMIT ?) RETURNING hash";
+    return jdbcTemplate.queryForList(sql, String.class, properties.getHashBatchSize());
+  }
+}
