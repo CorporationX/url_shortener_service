0a91a31cbb2c56d633c996d26d5e49e1df90e177
Dias Kamza <kamzadias31@gmail.com>
2025-03-16 13:15:04 +0500

    BJS2-66935 fixed migration and cache by comments

--- src/main/java/faang/school/urlshortenerservice/service/cache/UrlCache.java
+++ src/main/java/faang/school/urlshortenerservice/service/cache/UrlCache.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
 @RequiredArgsConstructor
 public class UrlCache {
@@ -11,7 +13,8 @@ public class UrlCache {
     private final StringRedisTemplate redisTemplate;
 
     public void saveUrlMapping(String hash, String longUrl) {
-        redisTemplate.opsForValue().set("url:" + hash, longUrl);
+        long ttlInSeconds = 365L * 24 * 60 * 60;
+        redisTemplate.opsForValue().set("url:" + hash, longUrl, ttlInSeconds, TimeUnit.SECONDS);
     }
 
     public String getLongUrl(String hash) {
