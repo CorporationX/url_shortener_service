3534eb55214555935ce0075729a00ea1c8081be0
Dias Kamza <kamzadias31@gmail.com>
2025-03-17 17:17:56 +0500

    BJS2-66935 made ttl as a property

--- src/main/java/faang/school/urlshortenerservice/service/cache/UrlCache.java
+++ src/main/java/faang/school/urlshortenerservice/service/cache/UrlCache.java
@@ -1,6 +1,7 @@
 package faang.school.urlshortenerservice.service.cache;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +13,10 @@ public class UrlCache {
 
     private final StringRedisTemplate redisTemplate;
 
+    @Value("${hash.cache.ttl-in-seconds:31536000}")
+    private long ttlInSeconds;
+
     public void saveUrlMapping(String hash, String longUrl) {
-        long ttlInSeconds = 365L * 24 * 60 * 60;
         redisTemplate.opsForValue().set("url:" + hash, longUrl, ttlInSeconds, TimeUnit.SECONDS);
     }
 
