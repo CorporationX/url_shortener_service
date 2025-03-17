2e1fdc0e4ca155e3fcb56d201873300c6ef0c9a6
Pavel Schelkin <rhanan1233@gmail.com>
2025-03-17 11:31:08 +0300

    Hash Generator

--- src/main/java/faang/school/urlshortenerservice/config/generator/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/config/generator/HashGenerator.java
@@ -3,12 +3,18 @@ package faang.school.urlshortenerservice.config.generator;
 import faang.school.urlshortenerservice.encoder.Base62Encoder;
 import faang.school.urlshortenerservice.repository.HashRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.ListUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -17,14 +23,32 @@ public class HashGenerator {
     @Value("${hash_repo.unique_max_size}")
     private int UNIQUE_MAX_SIZE;
 
+    @Value("${hash.partition_size}")
+    private int PARTITION_SIZE;
+
     private final HashRepository repository;
     private final Base62Encoder encoder;
+    @Autowired
+    @Qualifier("hashGeneratorThreadPool")
+    private ExecutorService pool;
 
     @Transactional
     @Async("hashGeneratorThreadPool")
-    public void generateBatch() {
+    public CompletableFuture<Void> generateBatch() {
         List<Long> uniqueNumbers = repository.getUniqueNumbers(UNIQUE_MAX_SIZE);
-        List<String> hashes = encoder.encode(uniqueNumbers);
+        List<CompletableFuture<List<String>>> futureList = ListUtils
+                .partition(uniqueNumbers, PARTITION_SIZE)
+                .stream()
+                .map((values) -> CompletableFuture.supplyAsync(() -> encoder.encode(values)))
+                .toList();
+        CompletableFuture.allOf(futureList.toArray(new CompletableFuture[0])).join();
+        List<String> hashes = futureList.stream()
+                .map(CompletableFuture::join)
+                .flatMap(List::stream)
+                .toList();
         repository.save(hashes);
+        return CompletableFuture.completedFuture(null);
+
+
     }
 }
