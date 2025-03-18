4b317b7716c30116ac6a447e6c5505afac3b70ce
romenskd <romenskyid@gmail.com>
2025-03-18 19:06:15 +0100

    добавлен HashRepository и сущности

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepositoryJDBCImpl.java
@@ -0,0 +1,37 @@
+package faang.school.urlshortenerservice.repository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class HashRepositoryJDBCImpl implements HashRepositoryJDBC {
+    @Value("${hash.batch.size}")
+    private int batchSize;
+
+    private final JdbcTemplate jdbcTemplate;
+
+
+    @Override
+    @Transactional
+    public void save(List<String> hashes){
+        String sql = "INSERT INTO hash (hash) VALUES (?)";
+        jdbcTemplate.update(sql, SqlParameterSourceUtils.createBatch(hashes));
+    }
+
+    @Override
+    @Transactional
+    public List<String> getHashBatch() {
+        String sql = "WITH deleted AS " +
+                "(DELETE FROM hash WHERE hash IN (SELECT hash FROM hash ORDER BY RANDOM() LIMIT :n) " +
+                "RETURNING hash) SELECT hash FROM deleted";
+
+        return jdbcTemplate.queryForList(sql, String.class, batchSize);
+    }
+}
