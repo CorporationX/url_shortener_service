138046d8cdae6f922c1f61dba38d68d48d1fbdc6
maks <fhbfds@fb>
2025-03-20 21:00:00 +0300

    BJS2-67280-Генерация хэша BJS2-67278-Генерация списка хэшей

---
+++ src/main/java/faang/school/urlshortenerservice/generator/HashGenerator.java
@@ -0,0 +1,35 @@
+package faang.school.urlshortenerservice.generator;
+
+import faang.school.urlshortenerservice.encoder.Base62Encoder;
+import faang.school.urlshortenerservice.entity.Hash;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class HashGenerator {
+
+    private final HashRepository hashRepository;
+    private final Base62Encoder base62Encoder;
+
+    @Value("${data.url.hash.maxRange}")
+    private int maxRange;
+
+    @Transactional
+    @Async("hashGeneratorExecutor")
+    public void generateBatch() {
+        List<Long> uniqueNumbers = hashRepository.getUniqueNumbers(maxRange);
+        List<String> hashes = base62Encoder.encode(uniqueNumbers);
+        List<Hash> hashEntities = hashes.stream()
+                .map(hash -> Hash.builder().hash(hash).build())
+                .collect(Collectors.toList());
+        hashRepository.saveAll(hashEntities);
+    }
+}
