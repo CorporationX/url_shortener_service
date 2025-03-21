479d8175bb3c983f8a58848fd753a246e847081b
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-03-21 11:55:33 +0500

    fix

--- src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
+++ src/main/java/faang/school/urlshortenerservice/controller/UrlController.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/url")
+@RestController
+@RequestMapping("/url")
 @Slf4j
 @RequiredArgsConstructor
 public class UrlController {
