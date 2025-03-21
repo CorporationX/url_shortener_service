fb98e0b2c7b81452543f05c3077cfe0ff4540d70
Kairat <snowmango.official@gmail.com>
2025-03-22 02:26:55 +0500

    add cleaner scheduler

---
+++ src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
@@ -0,0 +1,26 @@
+package faang.school.urlshortenerservice.scheduler;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import faang.school.urlshortenerservice.repository.UrlRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CleanerScheduler {
+
+    private final UrlRepository urlRepository;
+    private final HashRepository hashRepository;
+
+    @Scheduled(cron = "${scheduler.url-cron}")
+    @Transactional
+    public void urlCleanerScheduler() {
+        List<Hash> hashes = urlRepository.deleteUrlsAndReturnHashes();
+        hashRepository.saveAll(hashes);
+    }
+}
