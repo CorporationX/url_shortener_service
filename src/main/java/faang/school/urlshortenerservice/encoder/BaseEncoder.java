68e517e54dc865874f9e4697e6c3101b58f04eff
Aleksandr Kozlov <vismut-888@yandex.ru>
2025-03-23 00:41:12 +0300

    Добавил тест на BaseEncoder

--- src/main/java/faang/school/urlshortenerservice/encoder/BaseEncoder.java
+++ src/main/java/faang/school/urlshortenerservice/encoder/BaseEncoder.java
@@ -1,12 +1,14 @@
 package faang.school.urlshortenerservice.encoder;
 
 import faang.school.urlshortenerservice.entity.Hash;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Component
 public class BaseEncoder {
 
@@ -26,6 +28,10 @@ public class BaseEncoder {
     }
 
     private Hash encodeBase(long number) {
+        if (number <= 0) {
+            log.error("Число для кодировки хэша должно быть больше 0, переданное число: {}", number);
+            throw new NumberFormatException("Число для кодировки хэша должно быть больше 0");
+        }
         StringBuilder stringBuilder = new StringBuilder();
         while (number > 0) {
             stringBuilder.append(alphabet.charAt((int) (number % base)));
