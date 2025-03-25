d9ca1c9f4feeb2f4bf2437a324dd60d83275ed80
rauanzhanabergen <rauan.zhanabergen@guavapay.com>
2025-03-25 10:18:08 +0500

    done

--- src/main/java/faang/school/urlshortenerservice/handler/UrlExceptionHandler.java
+++ src/main/java/faang/school/urlshortenerservice/handler/UrlExceptionHandler.java
@@ -3,7 +3,6 @@ package faang.school.urlshortenerservice.handler;
 import faang.school.urlshortenerservice.exception.HashNotFoundException;
 import faang.school.urlshortenerservice.exception.UrlNotDetectedException;
 import faang.school.urlshortenerservice.exception.UrlNotFoundException;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,25 +14,25 @@ public class UrlExceptionHandler {
 
     @ExceptionHandler(HashNotFoundException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handle(@NotNull HashNotFoundException exception) {
+    public ErrorResponse handle(HashNotFoundException exception) {
         return new ErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handle(@NotNull IllegalArgumentException exception) {
+    public ErrorResponse handle(IllegalArgumentException exception) {
         return new ErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler(UrlNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handle(@NotNull UrlNotFoundException exception) {
+    public ErrorResponse handle(UrlNotFoundException exception) {
         return new ErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler(UrlNotDetectedException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handle(@NotNull UrlNotDetectedException exception) {
+    public ErrorResponse handle(UrlNotDetectedException exception) {
         return new ErrorResponse(exception.getMessage());
     }
 
