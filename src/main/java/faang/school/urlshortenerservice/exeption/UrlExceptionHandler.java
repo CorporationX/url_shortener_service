bb9ad10133c3fa651ba1661d5af70436de20b600
Garib_Aghayev <garibaghayev@gmail.com>
2025-03-25 03:40:55 +0300

    Задача: BJS2-67378/Обработчик исключений

---
+++ src/main/java/faang/school/urlshortenerservice/exeption/UrlExceptionHandler.java
@@ -0,0 +1,29 @@
+package faang.school.urlshortenerservice.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.xml.bind.ValidationException;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class UrlExceptionHandler {
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().
+                stream()
+                .map(x -> x.getField() + ": " + x.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Ошибка сервера " + e.getMessage());
+    }
+}
