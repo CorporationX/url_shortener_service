6c16775d24f765b54affb0b68c403f1390e18037
NiveshOne <Shishov.44@mail.ru>
2025-03-26 18:10:57 +0300

    Генерация хэша

--- src/main/java/faang/school/urlshortenerservice/event/Hash.java
+++ src/main/java/faang/school/urlshortenerservice/event/Hash.java
@@ -4,17 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Table(name = "hash")
 @Data
+@AllArgsConstructor
 public class Hash {
     @Id
     @Column(name = "hash", unique = true, nullable = false, length = 6)
     private String hash;
-
-    public Hash(String hash) {
-        this.hash = hash;
-    }
 }
---
+++ src/main/java/faang/school/urlshortenerservice/generator/HashGenerator.java
@@ -0,0 +1,43 @@
+package faang.school.urlshortenerservice.generator;
+
+import faang.school.urlshortenerservice.encoder.Base62Encoder;
+import faang.school.urlshortenerservice.event.Hash;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class HashGenerator {
+
+    private final HashRepository hashRepository;
+    private final Base62Encoder base62Encoder;
+
+    @Value("${hash.range:1000}")
+    private int maxRange;
+
+    @Async("customThreadPoolTaskExecutor")
+    @Transactional
+    public void generateBatch(){
+        try {
+            List<Long> numbers = hashRepository.getNextRange(maxRange);
+            List<String> hashes = base62Encoder.encode(numbers);
+
+            List<Hash> hashEntities = hashes.stream()
+                    .map(Hash::new)
+                    .toList();
+
+            hashRepository.saveAll(hashEntities);
+            log.info("Успешно сгенерированные и сохраненные хэши {}", hashEntities.size());
+        } catch (Exception e) {
+            log.error("Ошибка при генерации или сохранении хэшей: ", e);
+        }
+    }
+}
