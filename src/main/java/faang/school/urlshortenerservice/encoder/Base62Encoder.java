02ce8240129cd8572beedf228637dd8e3eafb734
Garib_Aghayev <garibaghayev@gmail.com>
2025-03-26 05:46:13 +0300

    Задача: BJS2-67427/Генерация списка хэшей

---
+++ src/main/java/faang/school/urlshortenerservice/encoder/Base62Encoder.java
@@ -0,0 +1,24 @@
+package faang.school.urlshortenerservice.encoder;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Base62Encoder {
+    private static final String BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public static List<String> encode(List<Long> value) {
+        List<String> result = new ArrayList<>();
+        for (Long l : value) {
+            StringBuilder sb = new StringBuilder();
+            while (l > 0) {
+                sb.append(BASE62.charAt((int) (l % 62)));
+                l /= 62;
+            }
+            result.add(sb.reverse().toString());
+        }
+        return result;
+    }
+}
