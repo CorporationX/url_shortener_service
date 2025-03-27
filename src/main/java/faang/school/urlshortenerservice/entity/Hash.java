779a33958c1ee52227417c517ce43284b84a562c
Garib_Aghayev <garibaghayev@gmail.com>
2025-03-27 13:08:33 +0300

    Задача: BJS2-67403/ Работа с базой(HashRepository)

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Hash.java
@@ -0,0 +1,21 @@
+package faang.school.urlshortenerservice.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "hash")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hash {
+    @Id
+    @Column(name = "hash", length = 6, nullable = false)
+    private String hash;
+}
---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,33 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends JpaRepository<Hash, String> {
+
+    @Query(nativeQuery = true,
+            value = """
+                     SELECT nextval('unique_number_seq')
+                     FROM generate_series(1, :batchSize)
+                     """)
+    List<Long> getUniqueNumbers(@Param("batch") long batch);
+
+    @Transactional
+    @Query(nativeQuery = true,
+            value = """
+                     DELETE FROM hash
+                     WHERE hash IN(
+                     SELECT hash
+                     FROM hash ORDER BY random()
+                     LIMIT :batchSize)
+                     RETURNING hash
+                     """)
+    List<Hash> getHashBatch(long batch);
+}
