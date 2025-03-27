9994652d61f881b64314f31ab4e175a9852c4102
NiveshOne <Shishov.44@mail.ru>
2025-03-27 20:52:56 +0300

    Обработчик исключений

--- src/main/java/faang/school/urlshortenerservice/service/HashCacheService.java
+++ src/main/java/faang/school/urlshortenerservice/service/HashCacheService.java
@@ -1,6 +1,5 @@
-package faang.school.urlshortenerservice.cache;
+package faang.school.urlshortenerservice.service;
 
-import faang.school.urlshortenerservice.service.HashService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +13,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class HashCache {
+public class HashCacheService {
 
     private final HashService hashService;
 
