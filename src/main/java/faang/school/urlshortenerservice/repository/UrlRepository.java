b7fe2b1b8cda93277ca4b1cba62753b697661e53
NiveshOne <Shishov.44@mail.ru>
2025-03-31 17:43:45 +0300

    Джоб для освобождения неиспользуемых хэшей

--- src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
@@ -2,11 +2,19 @@ package faang.school.urlshortenerservice.repository;
 
 import faang.school.urlshortenerservice.event.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
     Optional<Url> findByHash(String hash);
+
+    @Query(nativeQuery = true,
+            value = "DELETE FROM url WHERE " +
+                    "created_at < NOW() - INTERVAL '1 year' " +
+                    "RETURNING hash")
+    List<String> deleteOldUrlsAndReturnHashes();
 }
