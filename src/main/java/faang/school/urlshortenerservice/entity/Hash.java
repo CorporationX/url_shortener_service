7b62e11f05ab4e6be14539f5bf1b649dc8f0ca66
Viktor <vikt.ovod@outlook.com>
2025-04-10 00:58:14 +0300

    Работа с базой (HashRepository)

--- src/main/java/faang/school/urlshortenerservice/entity/Hash.java
+++ src/main/java/faang/school/urlshortenerservice/entity/Hash.java
@@ -4,9 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "hash")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Hash {
 
     @Id
