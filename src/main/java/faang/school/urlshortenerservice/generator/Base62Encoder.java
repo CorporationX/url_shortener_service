450a5466a4a03a2a8e43f86ccb3d785a78a5f622
Viktor <vikt.ovod@outlook.com>
2025-04-13 23:04:45 +0300

    Генерация списка хэшей

---
+++ src/main/java/faang/school/urlshortenerservice/generator/Base62Encoder.java
@@ -0,0 +1,34 @@
+package faang.school.urlshortenerservice.generator;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Base62Encoder {
+
+    private static final String BASE62_ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final int BASE = 62;
+
+    public List<Hash> encode(List<Long> numbers) {
+        return numbers.stream()
+                .map(this::encodeBase62)
+                .map(Hash::new)
+                .toList();
+    }
+
+    private String encodeBase62(long number) {
+        if (number == 0) {
+            return "0";
+        }
+
+        StringBuilder encoded = new StringBuilder();
+        while (number > 0) {
+            encoded.append(BASE62_ALPHABET.charAt((int) (number % BASE)));
+            number /= BASE;
+        }
+
+        return encoded.reverse().toString();
+    }
+}
