f351214e6769f6369af87c468e2eba45ed987282
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-05-06 18:46:14 +0300

    voldemarmv-BJS2-78489 Added annotations Param in HashRepository

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -4,6 +4,7 @@ import faang.school.urlshortenerservice.entity.Hash;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,12 +13,12 @@ import java.util.List;
 public interface HashRepository extends JpaRepository<Hash, String> {
 
     @Query(value = "SELECT nextval('unique_number_sec') FROM generate_series(1, :amount)", nativeQuery = true)
-    List<Long> getUniqueNumbers(long amount);
+    List<Long> getUniqueNumbers(@Param("amount")long amount);
 
     @Modifying
     @Query(value = "DELETE FROM hash WHERE hash IN (SELECT hash FROM hash LIMIT :batchSize) RETURNING hash",
             nativeQuery = true)
-    List<String> getHashBatch(int batchSize);
+    List<String> getHashBatch(@Param("batchSize") int batchSize);
 
     default void save(List<String> hashes) {
         List<Hash> hashEntities = hashes.stream()
