0dfa48de6a11106f45511e024ea78e5c8283483e
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-05-06 16:05:07 +0300

    voldemarmv-BJS2-78487 Added HashGenerator

---
+++ src/main/java/faang/school/urlshortenerservice/model/util/HashGenerator.java
@@ -0,0 +1,32 @@
+package faang.school.urlshortenerservice.model.util;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HashGenerator {
+    private final HashRepository hashRepository;
+    private final Base62Encoder base62Encoder;
+    @Value("${hash.sequence-amount}")
+    private int sequenceAmount;
+
+    @Async("fixedThreadPool")
+    public void generateBatch() {
+        try {
+            List<Long> sequencesForHash = hashRepository.getUniqueNumbers(sequenceAmount);
+            log.info("Sequence numbers to be hashed: {}", sequencesForHash);
+            List<String> encodeHashes = encoder.encode(sequencesForHash);
+            hashRepository.save(encodeHashes);
+        } catch (Exception e) {
+            log.error("Exception during generating hashcodes: ", e);
+            throw new RuntimeException();
+        }
+    }
+}
