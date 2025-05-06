fd53842934d5961f46e36a3fcebc553e2bde2f12
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-05-06 15:10:40 +0300

    voldemarmv-BJS2-78489 Added HashRepository. @Value for batch-size will be added in service package

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,28 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends JpaRepository<Hash, String> {
+
+    @Query(value = "SELECT nextval('unique_number_sec') FROM generate_series(1, :amount)", nativeQuery = true)
+    List<Long> getUniqueNumbers(long amount);
+
+    @Modifying
+    @Query(value = "DELETE FROM hash WHERE hash IN (SELECT hash FROM hash LIMIT :batchSize) RETURNING hash",
+            nativeQuery = true)
+    List<String> getHashBatch(int batchSize);
+
+    default void save(List<String> hashes) {
+        List<Hash> hashEntities = hashes.stream()
+                .map(Hash::new)
+                .toList();
+        saveAll(hashEntities);
+    }
+}
