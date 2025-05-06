aa9dcc934ea1129d74642cba84f2f66826cf5478
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-05-07 00:08:41 +0300

    voldemarmv-BJS2-78482 Created CleanerScheduler

---
+++ src/main/java/faang/school/urlshortenerservice/repository/UrlRepository.java
@@ -0,0 +1,15 @@
+package faang.school.urlshortenerservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, String> {
+    @Query(value = "DELETE FROM url WHERE created_at < NOW() - INTERVAL '1 year' RETURNING hash", nativeQuery = true)
+    @Modifying
+    List<String> removeOldLinks();
+}
