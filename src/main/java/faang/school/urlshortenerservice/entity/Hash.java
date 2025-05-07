d8e292777b9cd1d7cc9192767a0818c204b941ad
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-05-07 20:35:03 +0300

    Создал структуру БД

---
+++ src/main/java/faang/school/urlshortenerservice/entity/Hash.java
@@ -0,0 +1,29 @@
+package faang.school.urlshortenerservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "hashes")
+public class Hash {
+
+    @Id
+    @Column(name = "hash", length = 6, unique = true, nullable = false)
+    private String hash;
+}
