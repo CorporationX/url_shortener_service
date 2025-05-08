ac5751e25e55d5be49a73e4427f1e0faee075101
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-05-08 14:48:56 +0300

    voldemarmv-BJS2-78486 Added HashCache

---
+++ src/main/java/faang/school/urlshortenerservice/cache/HashCache.java
@@ -0,0 +1,60 @@
+package faang.school.urlshortenerservice.cache;
+
+import faang.school.urlshortenerservice.model.util.HashGenerator;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HashCache {
+    private final TaskExecutor queueTaskThreadPool;
+    private final HashRepository hashRepository;
+    private final HashGenerator hashGenerator;
+    private final AtomicBoolean isCaching = new AtomicBoolean(false);
+    private ArrayBlockingQueue<String> freeCaches;
+    @Value("${hash.queue.size}")
+    private int queueSize;
+    @Value("${hash.batch-size}")
+    private int batchSize;
+    @Value("${hash.queue.percentage-multiplier}")
+    private double percentageMultiplier;
+
+    @PostConstruct
+    private void init() {
+        freeCaches = new ArrayBlockingQueue<>(queueSize);
+        hashGenerator.generateBatch();
+        freeCaches.addAll(hashRepository.getHashBatch(batchSize));
+        log.info("HashCache initialized, queue is filled");
+    }
+
+    public String getHash() {
+        if (freeCaches.size() < queueSize * percentageMultiplier
+                && isCaching.compareAndSet(false, true)) {
+            queueTaskThreadPool.execute(() -> {
+                try {
+                    freeCaches.addAll(hashRepository.getHashBatch(batchSize));
+                    hashGenerator.generateBatch();
+                    log.info("Hash cache will be refilled by {}", Thread.currentThread().getName());
+
+                } finally {
+                    isCaching.set(false);
+                }
+            });
+        }
+
+        try {
+            return freeCaches.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
