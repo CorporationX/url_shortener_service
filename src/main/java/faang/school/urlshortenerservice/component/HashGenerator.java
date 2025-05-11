913c8c1b160d963fd0901f431378d8ec79b6df70
Artem Doronin <doronintema2018@yandex.ru>
2025-05-11 21:57:56 +0500

    commit 1

---
+++ src/main/java/faang/school/urlshortenerservice/component/HashGenerator.java
@@ -0,0 +1,40 @@
+package faang.school.urlshortenerservice.component;
+
+import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HashGenerator {
+
+    private final HashRepository hashRepository;
+    private final Base62Encoder base62Encoder;
+
+    @Value("${hash.batch-size}")
+    private int batchSize;
+
+    @Async("hashGenerationExecutor")
+    public CompletableFuture<Void> generateBatch() {
+        log.info("Generating batch of {} hashes", batchSize);
+
+        List<Long> numbers = hashRepository.getUniqueNumbers(batchSize);
+        log.info("Retrieved {} unique numbers", numbers.size());
+
+        List<String> hashes = base62Encoder.encode(numbers);
+        log.info("Generated {} hashes", hashes.size());
+
+        hashRepository.save(hashes);
+        log.info("Saved {} hashes to DB", hashes.size());
+
+        return CompletableFuture.completedFuture(null);
+    }
+}
+
