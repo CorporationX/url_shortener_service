71307c31dbc990c185822857afef724708bef4a1
Max Goloviznin <roxet59rus@mail.ru>
2025-05-13 03:49:02 +0500

    (BJS2-78552) created encoder, some fixes

--- src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
+++ src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 @ConfigurationPropertiesScan
 @EnableFeignClients("faang.school.urlshortenerservice.client")
 public class ServiceTemplateApplication {
--- src/main/java/faang/school/urlshortenerservice/component/Base62Encoder.java
+++ src/main/java/faang/school/urlshortenerservice/component/Base62Encoder.java
@@ -9,12 +9,22 @@ import java.util.List;
 public class Base62Encoder {
 
     private static final String base62Symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int BASE = base62Symbols.length();
 
     public List<String> encode(List<Long> numbers) {
         List<String> hashes = new ArrayList<>();
-        numbers.forEach(number -> {
-            hashes.add(number.toString());
-        });
+        numbers.forEach(number -> hashes.add(createHashString(number)));
         return hashes;
+    }
+
+    private String createHashString(long number) {
+        StringBuilder hashBuilder = new StringBuilder();
+
+        while (number > 0) {
+            int remainder = (int)(number % BASE);
+            hashBuilder.append(base62Symbols.charAt(remainder));
+            number /= BASE;
+        }
+        return hashBuilder.toString();
     }
 }
