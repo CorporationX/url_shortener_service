853eaba3e20d457d7387147fb93b5848e9627c3f
Max Goloviznin <roxet59rus@mail.ru>
2025-05-13 01:03:56 +0500

    (BJS2-78555) add hash generator

---
+++ src/main/java/faang/school/urlshortenerservice/component/HashGenerator.java
@@ -0,0 +1,13 @@
+package faang.school.urlshortenerservice.component;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HashGenerator {
+
+    public void generateBatch() {
+
+    }
+}
