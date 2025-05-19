3ad268bd8e460cdb89b0a0d56f518c66e4618650
saroyangor <dr.saroyan98@gmail.com>
2025-05-20 00:58:29 +0300

    BJS2-78846:feature/Hash Generation

---
+++ src/main/java/faang/school/urlshortenerservice/generator/HashGenerator.java
@@ -0,0 +1,41 @@
+package faang.school.urlshortenerservice.generator;
+
+import faang.school.urlshortenerservice.model.Hash;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HashGenerator {
+    private final Base62Encoder base62Encoder;
+    private final HashRepository hashRepository;
+
+    @Value("${url-shortener.hash-count}")
+    private int count;
+
+    @Transactional
+    @Async("hashGeneratorExecutor")
+    public void generateBatch() {
+        log.info("Starting generate {} hashes", count);
+
+        List<Long> uniqueNumbers = hashRepository.getUniqueNumbers(count);
+        List<String> uniqueStrings = base62Encoder.encode(uniqueNumbers);
+
+        List<Hash> hashes = uniqueStrings.stream()
+                .map(hash -> Hash.builder()
+                        .hash(hash)
+                        .build()
+                ).toList();
+        hashRepository.saveAll(hashes);
+
+        log.info("Finished generate {} hashes", count);
+    }
+}
--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -10,8 +10,7 @@ import java.util.List;
 
 @Repository
 public interface HashRepository extends JpaRepository<Hash, Long> {
-    @Query(
-            nativeQuery = true, value = """
+    @Query(nativeQuery = true, value = """
                 SELECT nextval('unique_number_seq')
                 FROM generate_series(1, :count)
             """)
