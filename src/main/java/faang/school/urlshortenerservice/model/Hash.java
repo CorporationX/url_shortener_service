8c45dd669d3b61e6f7322c7fcbdd295ca5117eb2
saroyangor <dr.saroyan98@gmail.com>
2025-05-19 18:10:24 +0300

    BJS2-78850:feature/Hash Repository

---
+++ src/main/java/faang/school/urlshortenerservice/model/Hash.java
@@ -0,0 +1,24 @@
+package faang.school.urlshortenerservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "hash")
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hash {
+    @Id
+    @Column(name = "hash", nullable = false, length = 6)
+    private String hash;
+}
---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,33 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.model.Hash;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends JpaRepository<Hash, Long> {
+    @Query(
+            nativeQuery = true, value = """
+                SELECT nextval('unique_number_seq')
+                FROM generate_series(1, :count)
+            """)
+    List<Long> getUniqueNumbers(int count);
+
+    @Modifying
+    @Query("""
+            INSERT INTO hash(hash) VALUES (:hash)
+            """)
+    void save(List<Hash> hashes);
+
+    @Modifying
+    @Query("""
+            DELETE FROM hash WHERE hash IN
+            (SELECT hash  FROM hash LIMIT :size FOR UPDATE SKIP LOCKED)
+            RETURNING hash
+            """)
+    List<Hash> getHashBatch(int size);
+}
