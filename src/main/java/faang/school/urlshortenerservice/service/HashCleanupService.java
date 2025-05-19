676db2c61b672079bd557c171714aa651ed79149
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-05-19 09:57:53 +0500

    https://faang-school.atlassian.net/browse/BJS2-78696 add cleaner

---
+++ src/main/java/faang/school/urlshortenerservice/service/HashCleanupService.java
@@ -0,0 +1,31 @@
+package faang.school.urlshortenerservice.service;
+
+import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class HashCleanupService {
+
+    private final HashRepository hashRepository;
+
+    // Запуск джоба с интервалом, например, 1 раз в час (настраивается)
+    @Scheduled(fixedDelayString = "${hash.cleanup.job.delay:3600000}")
+    @Transactional
+    public void cleanupUnusedHashes() {
+        List<String> unusedHashes = hashRepository.findUnusedHashes();
+        if (!unusedHashes.isEmpty()) {
+            hashRepository.deleteAllById(unusedHashes);
+            log.info("Deleted {} unused hashes", unusedHashes.size());
+        } else {
+            log.info("No unused hashes to delete");
+        }
+    }
+}
