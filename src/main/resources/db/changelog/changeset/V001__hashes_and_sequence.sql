d85d971e448c234c8f7af80dbbe3cfa9e024c45c
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-05-20 22:23:08 +0300

    feat: Added tables and entities for URL Shortener

---
+++ src/main/resources/db/changelog/changeset/V001__hashes_and_sequence.sql
@@ -0,0 +1,13 @@
+CREATE TABLE url (
+    hash VARCHAR(6) PRIMARY KEY,
+    url TEXT NOT NULL UNIQUE,
+    created_at timestamptz DEFAULT current_timestamp
+);
+
+CREATE TABLE hash (
+    hash VARCHAR(6) PRIMARY KEY
+);
+
+CREATE SEQUENCE unique_number_seq
+    START WITH 1
+    INCREMENT BY 1;
