4f241ee2db4d4621fc3efa39c3ad3b81668bd8cd
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-05-20 22:58:22 +0300

    feat: Added HashRepository for URL Shortener

---
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -0,0 +1,26 @@
+package faang.school.urlshortenerservice.repository;
+
+import faang.school.urlshortenerservice.entity.Hash;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HashRepository extends CrudRepository<Hash, String> {
+
+    @Query(nativeQuery = true, value = """
+            SELECT nextval('unique_number_seq')
+            FROM generate_series(1, :size);
+            """)
+    List<Long> getUniqueNumbers(int size);
+
+    @Query(value = """
+            DELETE FROM hash
+            WHERE hash IN (
+                SELECT hash FROM hash ORDER BY hash  LIMIT :amount
+            ) RETURNING *
+            """, nativeQuery = true)
+    List<Hash> getHashBatch(int amount);
+}
