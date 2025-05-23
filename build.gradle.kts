d2747d7c049d71a1448b6df9549a40037c448577
Vitaliy <vitaliy.zhilitskiy@gmail.com>
2025-05-23 22:28:15 +0300

    Добавлена интеграция с grafana

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,13 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.awaitility:awaitility:4.2.0")
+
+    /**
+     * Monitoring
+     */
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("com.github.loki4j:loki-logback-appender:1.5.0")
 }
 
 tasks.withType<Test> {
