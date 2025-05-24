34f627abcb6f03b7045c02d8e7b95e51d4c0d954
naught-wo <catrinesaxon@yandex.ru>
2025-05-24 18:56:28 +0300

    удалила закомментированные и неиспользуемые части

--- src/main/java/faang/school/urlshortenerservice/service/HashService.java
+++ src/main/java/faang/school/urlshortenerservice/service/HashService.java
@@ -6,7 +6,6 @@ import faang.school.urlshortenerservice.properties.CacheProperties;
 import faang.school.urlshortenerservice.repository.JdbcHashRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
