e06dc183eb227a85d7cafd12267ef5627d47b44f
Vladislav Kozlov <littlerock2877@mail.ru>
2025-07-16 22:16:39 +0300

    add: hash generator

---
+++ src/main/java/faang/school/urlshortenerservice/generator/HashGenerator.java
@@ -0,0 +1,33 @@
+package faang.school.urlshortenerservice.generator;
+
+import faang.school.urlshortenerservice.config.properties.HashConfig;
+import faang.school.urlshortenerservice.encoder.Encoder;
+import faang.school.urlshortenerservice.model.Hash;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class HashGenerator {
+    private final HashRepository hashRepository;
+    private final HashConfig hashConfig;
+    private final Encoder encoder;
+
+    @Async("fixedThreadPool")
+    public CompletableFuture<List<Hash>> generateBatch() {
+        log.debug("Start async generation of new hashes batch");
+        List<Long> numbers = hashRepository.getUniqueNumbers(hashConfig.getBatchSize());
+        List<Hash> hashes = encoder.encode(numbers).stream()
+                .map(Hash::new)
+                .toList();
+        log.info("Hashes {} was successfully generated", hashes);
+        return CompletableFuture.supplyAsync(() -> hashRepository.saveAll(hashes));
+    }
+}
