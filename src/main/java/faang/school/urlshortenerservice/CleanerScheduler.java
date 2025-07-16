d0f4c06eda18859cb4b085ef4c7f01412a43120a
Aleksandr <alexandr.sidorkin@gmail.com>
2025-07-16 17:48:05 +0200

    url shortener. Expired clean up

---
+++ src/main/java/faang/school/urlshortenerservice/CleanerScheduler.java
@@ -0,0 +1,44 @@
+package faang.school.urlshortenerservice;
+
+import faang.school.urlshortenerservice.config.ConstantsProperties;
+import faang.school.urlshortenerservice.repository.HashRepository;
+import faang.school.urlshortenerservice.repository.UrlRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CleanerScheduler {
+    private final HashRepository hashRepository;
+    private final UrlRepository urlRepository;
+    private final ConstantsProperties constantsProperties;
+
+    @Scheduled(cron = "${spring.scheduling.expired_hash_cleanup}")
+    @Async("taskExecutor")
+    public void cleanUpHashes() {
+        int cleaned;
+        do {
+            cleaned = getSelf().cleanUpBatch();
+        } while (cleaned == constantsProperties.getCleanUpBatchSize());
+    }
+
+    @Transactional
+    public int cleanUpBatch() {
+        List<String> removedHashes = urlRepository.getHashesAndDelete(
+                constantsProperties.getExpirationInterval(),
+                constantsProperties.getCleanUpBatchSize()
+        );
+        hashRepository.save(removedHashes);
+        return removedHashes.size();
+    }
+
+    private CleanerScheduler getSelf() {
+        return (CleanerScheduler) AopContext.currentProxy();
+    }
+}
