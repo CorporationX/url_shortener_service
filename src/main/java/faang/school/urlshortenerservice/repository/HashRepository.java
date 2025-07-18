ee7ea507bd3d24500ee30c37c258f1bfc0c5e70f
Vladimir Levshin <levshin@intervolga.ru>
2025-07-18 23:41:32 +0300

    BJS2-84688: Deleted wrong annotation.

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++ src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
@@ -1,6 +1,5 @@
 package faang.school.urlshortenerservice.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,9 +7,7 @@ import java.util.List;
 @Repository
 public interface HashRepository {
 
-    @Query(value = "select setval('unique_number_seq', (nextval('unique_number_seq') + abs(:count)))")
     List<Long> getUniqueNumbers(int count);
-
 
     void saveBatch(List<String> hashes);
 
