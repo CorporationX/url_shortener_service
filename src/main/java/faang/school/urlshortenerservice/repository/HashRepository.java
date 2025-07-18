1376d9b9562ad60656e3427beecbf66c47ea0756
firma78 <rap215@icloud.com>
2025-07-18 03:43:02 +0300

    Сделал

--- src/main/java/faang/school/urlshortenerservice/repository/HashRepository.java
+++
@@ -1,6 +0,0 @@
-package faang.school.urlshortenerservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HashRepository extends JpaRepository<Hash> {
-}
