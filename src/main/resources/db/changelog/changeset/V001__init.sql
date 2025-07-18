68ee51e9cc78303989ed1a35b964f1241ac287cf
firma78 <rap215@icloud.com>
2025-07-18 22:45:07 +0300

    Сделал

--- src/main/resources/db/changelog/changeset/V001__init.sql
+++ src/main/resources/db/changelog/changeset/V001__init.sql
@@ -1,15 +1,16 @@
-CREATE SEQUENCE unique_hash_seq
+CREATE SEQUENCE IF NOT EXISTS unique_hash_seq
+    AS BIGINT
     START WITH 1
     INCREMENT BY 1;
 
-    CREATE TABLE hashes (
-    hash VARCHAR(6) PRIMARY KEY)
+CREATE TABLE IF NOT EXISTS hashes (
+    hash VARCHAR(6) PRIMARY KEY not null unique);
 
-    CREATE TABLE urls (
-    hash VARCHAR(6) PRIMARY KEY
+CREATE TABLE IF NOT EXISTS urls (
+    hash VARCHAR(6) PRIMARY KEY not null unique
     url TEXT NOT NULL
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP DEFAULT current_timestamp
     CONSTRAINT fk_hash FOREIGN KEY(hash) REFERENCES hashes(hash)
-    )
+    );
 
-    CREATE INDEX idx_url_hash ON urls(hash);
+CREATE INDEX idx_url_hash ON urls(hash);
