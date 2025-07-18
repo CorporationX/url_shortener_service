190ebf84e1abcef3971f83749415a283982c98e4
Anatolii Izotov <izotov.anatoly.e@gmail.com>
2025-07-18 09:30:12 +0200

    test(BSS2-84706): Add unit and integration tests coverage for URL shortener service

---
+++ src/test/resources/faang/school/urlshortenerservice/controller/scripts/insert-hashes.sql
@@ -0,0 +1,5 @@
+INSERT INTO hash (hash) VALUES
+                            ('a1b'),
+                            ('e4f'),
+                            ('i7j'),
+                            ('m0n');
