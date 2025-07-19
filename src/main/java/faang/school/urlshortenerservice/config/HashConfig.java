7465fd41ecca5cb16b7590afddc4adf084f710cf
Gafurova Faribo <faribo.gafur@gmail.com>
2025-07-19 23:45:38 +0300

    BJS2-85007 Работа с базой(HashRepository)

---
+++ src/main/java/faang/school/urlshortenerservice/config/HashConfig.java
@@ -0,0 +1,12 @@
+package faang.school.urlshortenerservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "hash")
+public class HashConfig {
+    private int batchSize;
+}
