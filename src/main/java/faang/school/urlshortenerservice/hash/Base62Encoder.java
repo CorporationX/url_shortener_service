cbd86b4443b1fbd61fae637271dc5453275f9ee6
Aleksandr <alexandr.sidorkin@gmail.com>
2025-07-19 21:57:01 +0200

    ARR (After review refactoring) Base62Encoder logic changed. Squids lib - new hash provider. https://github.com/sqids/sqids-java DB migrations merged.

--- src/main/java/faang/school/urlshortenerservice/hash/Base62Encoder.java
+++ src/main/java/faang/school/urlshortenerservice/hash/Base62Encoder.java
@@ -2,39 +2,25 @@ package faang.school.urlshortenerservice.hash;
 
 import faang.school.urlshortenerservice.config.HashEncoderProperties;
 import org.springframework.stereotype.Component;
+import org.sqids.Sqids;
 
 import java.util.List;
 
 @Component
 public class Base62Encoder {
-    private final HashEncoderProperties properties;
-    private final int base;
+    private final Sqids sqids;
 
     public Base62Encoder(HashEncoderProperties properties) {
-        this.properties = properties;
-        this.base = properties.getAlphabet().length();
+        this.sqids = Sqids.builder()
+                .alphabet(properties.getAlphabet())
+                .minLength(properties.getHashLength())
+                .build();
     }
 
     public List<String> encode(List<Long> numbers) {
         return numbers.stream()
-                .map(this::shuffleNumber)
-                .map(this::encodeToBase62)
+                .map(List::of)
+                .map(sqids::encode)
                 .toList();
-    }
-
-    private long shuffleNumber(long number) {
-        return (number * properties.getShuffleKey()) % (properties.getMaxNumber() + 1);
-    }
-
-    private String encodeToBase62(long shuffledNumber) {
-        StringBuilder sb = new StringBuilder();
-        long argumentCopy = shuffledNumber;
-
-        for (int i = 0; i < properties.getHashLength(); i++) {
-            sb.insert(0, properties.getAlphabet().charAt((int) (argumentCopy % base)));
-            argumentCopy /= base;
-        }
-
-        return sb.toString();
     }
 }
