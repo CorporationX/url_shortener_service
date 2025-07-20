8b9d14e1d3160bfdc2cc98774cf60df806c8f3db
Vladislav Kozlov <littlerock2877@mail.ru>
2025-07-20 12:05:08 +0300

    add: HashCache

---
+++ src/main/java/faang/school/urlshortenerservice/config/properties/CacheConfig.java
@@ -0,0 +1,15 @@
+package faang.school.urlshortenerservice.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "app.cache")
+public class CacheConfig {
+    private int capacity;
+    private int fillPercentage;
+}
