bdc3e9a88ab5853a1d2fd27862d784c1a7846c07
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-07-20 23:02:42 +0300

    refactor url

--- src/main/java/faang/school/urlshortenerservice/dto/url/UrlRequestDto.java
+++ src/main/java/faang/school/urlshortenerservice/dto/url/UrlRequestDto.java
@@ -1,12 +1,9 @@
 package faang.school.urlshortenerservice.dto.url;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.URL;
 
 public record UrlRequestDto (@NotBlank(message = "URL is empty")
-                             @Pattern(
-                                     regexp = "^(http|https)://.*$",
-                                     message = "URL must start with http:// or https://"
-                             )
+                             @URL(message = "URL must start with http:// or https://")
                              String url) {
 }
