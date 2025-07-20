975dee73796ad0170a6a06e42f067ef5e727ef39
firma78 <rap215@icloud.com>
2025-07-20 03:14:21 +0300

    Base62 полностью готова

--- src/main/java/faang/school/urlshortenerservice/util/hash/HashGeneratorImpl.java
+++ src/main/java/faang/school/urlshortenerservice/util/hash/HashGeneratorImpl.java
@@ -4,9 +4,12 @@ import faang.school.urlshortenerservice.repository.hash.HashRepository;
 import faang.school.urlshortenerservice.util.Base62Encoder;
 import faang.school.urlshortenerservice.util.HashGenerator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class HashGeneratorImpl implements HashGenerator {
@@ -17,9 +20,13 @@ public class HashGeneratorImpl implements HashGenerator {
     private final int generateBatchSize;
 
     @Override
+    @Async
     public void generateBatch() {
+        log.info("Hashes Generation started");
         hashRepository.save(
                 base62Encoder.encode(hashRepository.getUniqueNumbers(generateBatchSize)));
-
+        log.info("Hashes was generated, from {} to {}",
+                hashRepository.getUniqueNumbers(generateBatchSize),
+                hashRepository.getUniqueNumbers(generateBatchSize).size() - 1);
     }
 }
