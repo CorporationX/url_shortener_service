87a4d378733ecb27c7fda34d8e5d23bfccbe1b06
Vladislav Kozlov <littlerock2877@mail.ru>
2025-07-20 13:55:03 +0300

    fix: urldto

--- src/main/java/faang/school/urlshortenerservice/dto/UrlDto.java
+++ src/main/java/faang/school/urlshortenerservice/dto/UrlDto.java
@@ -6,10 +6,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UrlDto {
+public class UrlDto implements Serializable {
     @NotBlank(message = "Url should not be blank")
     @URL(message = "Url should be correct")
     private String url;
