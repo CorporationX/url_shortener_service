8d8eb24679bf1e2c508b0f62d969c3ac1702b5fe
firma78 <rap215@icloud.com>
2025-07-21 20:21:16 +0300

    Промежуточный

--- src/main/java/faang/school/urlshortenerservice/service/url/UrlServiceImpl.java
+++ src/main/java/faang/school/urlshortenerservice/service/url/UrlServiceImpl.java
@@ -1,14 +1,27 @@
 package faang.school.urlshortenerservice.service.url;
 
+import faang.school.urlshortenerservice.cache.HashCache;
 import faang.school.urlshortenerservice.dto.ShortUrlDto;
 import faang.school.urlshortenerservice.dto.UrlDto;
+import faang.school.urlshortenerservice.entity.Url;
+import faang.school.urlshortenerservice.mapper.UrlMapper;
 import faang.school.urlshortenerservice.service.UrlService;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class UrlServiceImpl implements UrlService {
-    private final HashCache
+    private final HashCache hashCache;
+    private final UrlMapper urlMapper;
+    private final H
+
     @Override
     public ShortUrlDto createShortUrl(UrlDto urlDto) {
-        urlDto.
+        String hash = hashCache.getHash();
+        Url url = urlMapper.toEntity(urlDto);
+        url.setHash(hash);
+
+
+
     }
 
     @Override
