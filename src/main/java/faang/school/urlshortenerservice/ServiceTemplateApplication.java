4215acbd9fdf19049f281ad89f86337557426dd7
Anatolii Izotov <izotov.anatoly.e@gmail.com>
2025-07-22 12:49:36 +0200

    fix(BJS2-84706): address PR review comments, integrate ShedLock.

--- src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
+++ src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
@@ -1,5 +1,6 @@
 package faang.school.urlshortenerservice;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "${shedlock.defaults.lock-at-most-for}")
 @EnableFeignClients("faang.school.urlshortenerservice.client")
 @EnableAsync
 @EnableCaching
