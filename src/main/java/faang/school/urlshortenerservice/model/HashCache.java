efa2fc02ad77c2309fc530480c6054a7d0225734
v888.rus <v888.rus@gmail.com>
2025-07-23 09:29:49 +0000

    Part 2. Url Shortener. Async lock added.

--- src/main/java/faang/school/urlshortenerservice/model/HashCache.java
+++ src/main/java/faang/school/urlshortenerservice/model/HashCache.java
@@ -1,6 +1,8 @@
 package faang.school.urlshortenerservice.model;
 
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
@@ -8,18 +10,19 @@ import org.springframework.stereotype.Component;
 
 import faang.school.urlshortenerservice.config.MainConfig;
 import faang.school.urlshortenerservice.repository.HashRepository;
-import faang.school.urlshortenerservice.util.CustomTaskExecutor;
 import faang.school.urlshortenerservice.util.HashGenerator;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class HashCache {
-    private final CustomTaskExecutor customTaskExecutor;
     private final MainConfig mainConfig;
     private final HashRepository hashRepository;
     private final HashGenerator hashGenerator;
+    private final AtomicBoolean generationInProgress = new AtomicBoolean(false);
 
     @Value("${hashes.numberOfHashes}")
     private int numberOfHashes;
@@ -45,10 +48,22 @@ public class HashCache {
 
     @Async("customTaskExecutor")
     public void generateHashBatchAsync() {
-        int requestedHashesNumber = mainConfig.getNumberOfHashes() - hashQueue.size();
-        customTaskExecutor.execute(() -> {
-            hashGenerator.generateBatch();
-            hashQueue.addAll(hashRepository.getHashBatchOf(requestedHashesNumber));
-        });
+        if(!generationInProgress.compareAndSet(false, true)) {
+            log.warn("The thread is task is already running. Skipping");
+            return;
+        }
+
+        try {
+            int requestedHashesNumber = mainConfig.getNumberOfHashes() - hashQueue.size();
+            List<Hash> batch = hashGenerator.generateBatch();
+            hashQueue.addAll(
+                batch.stream()
+                    .map(Hash::getHash)
+                    .limit(requestedHashesNumber)
+                    .toList()
+            );
+        } finally {
+            generationInProgress.set(false);
+        }
     }
 }
