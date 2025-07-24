784d6d4bf9254d8f1bb140dde714b84d903deca0
v888.rus <v888.rus@gmail.com>
2025-07-24 12:53:24 +0000

    Part 3. Url Shortener. HashCleanerScheduler.

--- src/main/java/faang/school/urlshortenerservice/handler/GlobalExceptionHandler.java
+++ src/main/java/faang/school/urlshortenerservice/handler/GlobalExceptionHandler.java
@@ -10,7 +10,13 @@ import faang.school.urlshortenerservice.dto.ErrorResponseDto;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponseDto> handleInvalidUrl(RuntimeException e) {
+    public ResponseEntity<ErrorResponseDto> handleInternalErrors(RuntimeException e) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 500);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponseDto);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGenericException(Exception e) {
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getMessage(), 500);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponseDto);
     }
---
+++ src/main/java/faang/school/urlshortenerservice/model/UrlJPA.java
@@ -0,0 +1,36 @@
+package faang.school.urlshortenerservice.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "url")
+public class UrlJPA {
+    @Id
+    @Column(name = "hash", length = 6, nullable = false, unique = true)
+    private String hash;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
