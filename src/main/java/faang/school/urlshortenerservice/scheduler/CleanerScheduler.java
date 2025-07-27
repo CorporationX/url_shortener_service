c0c557dbcc06219aa8f8a67630caaa3245da2d52
Tanya <hummingbird_t@mail.ru>
2025-07-27 16:49:52 +0300

    FillInScheduler

--- src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
+++ src/main/java/faang/school/urlshortenerservice/scheduler/CleanerScheduler.java
@@ -1,22 +1,11 @@
 package faang.school.urlshortenerservice.scheduler;
 
-import faang.school.urlshortenerservice.entity.Hash;
-import faang.school.urlshortenerservice.repository.HashRepository;
-import faang.school.urlshortenerservice.repository.UrlRepository;
-import faang.school.urlshortenerservice.service.UrlCacheService;
 import faang.school.urlshortenerservice.service.UrlService;
-import faang.school.urlshortenerservice.service.UrlServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -24,7 +13,6 @@ import java.util.List;
 public class CleanerScheduler {
 
     private final UrlService urlService;
-    private final UrlCacheService urlCacheService;
 
     @Scheduled(cron = "${cleaner.cron.cleanTime:0 0 0 * * ?}")
     @SchedulerLock(name = "CleanerScheduler_cleanOldUrls",
--- src/main/java/faang/school/urlshortenerservice/scheduler/FillInScheduler.java
+++ src/main/java/faang/school/urlshortenerservice/scheduler/FillInScheduler.java
@@ -1,12 +1,29 @@
 package faang.school.urlshortenerservice.scheduler;
 
+import faang.school.urlshortenerservice.cache.hash.HashGenerator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class FillInScheduler {
+
+    private final HashGenerator hashGenerator;
 
     @Value("${cleaner.cron.fillInTime:0 0 0 * * ?}")
     private String fillInTime;
 
-    @Scheduled(cron = "${hash.schedule.cron:0 0/5 * * * ?}")
+    @Scheduled(cron = "${hash.schedule.fill-in.cron:0 0/5 * * * ?}")
+    @SchedulerLock(name = "FillInScheduler_addNewFreeHashes",
+            lockAtLeastFor = "PT2M", lockAtMostFor = "PT10M")
+    public void addNewFreeHashes(){
+        log.debug("Starting generating new free hashes...");
+        hashGenerator.generateHashesAsync();
+        log.info("New free hashes have been added to table hash");
+    }
 }
