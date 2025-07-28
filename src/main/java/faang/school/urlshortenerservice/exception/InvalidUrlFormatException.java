883d4e96fd312ec25f0be65fd791b2354455e896
Alexey Antipin <alexey@kittco.ru>
2025-07-28 21:08:29 +0300

    Revert "feat(BJS2-84706): Add initial implementation for URL shortener service"
    This reverts commit 00496176796b42873c63cd9e64fcd4cae9e51b14.

--- src/main/java/faang/school/urlshortenerservice/exception/InvalidUrlFormatException.java
+++
@@ -1,7 +0,0 @@
-package faang.school.urlshortenerservice.exception;
-
-public class InvalidUrlFormatException extends RuntimeException {
-    public InvalidUrlFormatException() {
-        super("Invalid URL format. Please provide a valid URL.");
-    }
-}
--- src/main/java/faang/school/urlshortenerservice/repository/UrlCacheRepository.java
+++
@@ -1,7 +0,0 @@
-package faang.school.urlshortenerservice.repository;
-
-import java.time.Duration;
-
-public interface UrlCacheRepository {
-    void save(String hash, String url, Duration ttl);
-}
