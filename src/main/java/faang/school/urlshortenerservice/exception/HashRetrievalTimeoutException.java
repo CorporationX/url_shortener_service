c29024703b8aeedb36f45f24450f84cec7e61a61
Alexey Antipin <alexey@kittco.ru>
2025-07-28 21:08:29 +0300

    Revert "feat(BJS2-84706): Add initial implementation for URL shortener service"
    This reverts commit 00496176796b42873c63cd9e64fcd4cae9e51b14.

--- src/main/java/faang/school/urlshortenerservice/exception/HashRetrievalTimeoutException.java
+++
@@ -1,7 +0,0 @@
-package faang.school.urlshortenerservice.exception;
-
-public class HashRetrievalTimeoutException extends RuntimeException {
-    public HashRetrievalTimeoutException() {
-        super("Failed to retrieve a hash within the specified timeout. The service may be temporarily overloaded.");
-    }
-}
--- src/main/java/faang/school/urlshortenerservice/exception/UrlNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package faang.school.urlshortenerservice.exception;
-
-public class UrlNotFoundException extends RuntimeException {
-    public UrlNotFoundException(String hash) {
-        super(String.format("URL with hash '%s' not found", hash));
-    }
-}
