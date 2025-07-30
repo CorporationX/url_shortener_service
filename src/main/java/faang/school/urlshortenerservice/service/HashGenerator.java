df9509ad850616bcbdcb9261f77c3671e1c248f8
tanufa 25 <tanufa25@gmail.com>
2025-07-31 01:55:45 +0300

    BJS2-85002 Unnecessary log deleted

--- src/main/java/faang/school/urlshortenerservice/service/HashGenerator.java
+++ src/main/java/faang/school/urlshortenerservice/service/HashGenerator.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.util.Collections;
 import java.util.List;
@@ -29,8 +28,6 @@ public class HashGenerator {
 
     @Transactional
     public void generateHashBatch(int amountOfGeneration) {
-        // todo check new Transaction
-        log.info("generateHashBatch, is transaction active: {}", TransactionSynchronizationManager.isActualTransactionActive());
         List<Long> sequenceValues = hashRepository.getUniqueNumbers(amountOfGeneration);
         Collections.shuffle(sequenceValues);
         List<String> generatedHashValues = base62Encoder.encodeBatch(sequenceValues);
