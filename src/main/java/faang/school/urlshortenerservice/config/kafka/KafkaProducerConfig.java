cc35d20a6d3eee1683002a005f4da20ddec100f5
Viktor Romanenko <romanenkoviktor080@gmail.com>
2025-08-05 18:52:16 +0300

    fix

--- src/main/java/faang/school/urlshortenerservice/config/kafka/KafkaProducerConfig.java
+++ src/main/java/faang/school/urlshortenerservice/config/kafka/KafkaProducerConfig.java
@@ -25,7 +25,6 @@ public class KafkaProducerConfig {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        configProps.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, true);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
