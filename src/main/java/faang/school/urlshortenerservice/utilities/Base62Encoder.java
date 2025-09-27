1c3703aea4ba9a8d8dda991d5fa45761e34a9668
Dmitry197652RT <134863049+Dmitry197652RT@users.noreply.github.com>
2025-09-27 20:32:41 +0300

    Delete src/main/java/faang/school/urlshortenerservice/utilities/Base62Encoder.java

--- src/main/java/faang/school/urlshortenerservice/utilities/Base62Encoder.java
+++
@@ -1,9 +0,0 @@
-package faang.school.urlshortenerservice.utilities;
-
-import org.springframework.stereotype.Component;
-import java.util.List;
-@Component
-public interface Base62Encoder {
-    List<String> encode(List<Long> numbers);
-    String coding (long incomingNumber);
-}
